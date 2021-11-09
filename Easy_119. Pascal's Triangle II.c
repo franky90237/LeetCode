d8c0f06cbc48255839c4c692e9890c96bbcaf9a6
franky90237 <30579359+franky90237@users.noreply.github.com>
2021-11-09 10:43:00 +0800

    Update Easy_119. Pascal's Triangle II.c
    right to left

--- Easy_119. Pascal's Triangle II.c
+++ Easy_119. Pascal's Triangle II.c
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //left to rigt
+    //space complexity : 2(rowIndex+1) = O(rowIndex)    
     vector<int> getRow(int rowIndex) 
     {
         vector<int> result[2];
@@ -19,4 +21,23 @@ public:
         
         return result[rowIndex%2];
     }
+    
+    //right to left
+    //space complexity : rowIndex+1 = O(rowIndex)
+    vector<int> getRow(int rowIndex) 
+    {
+        vector<int> result(rowIndex+1);        
+        
+        result[0]=1;
+        for(int i=1;i<=rowIndex;++i)
+        {
+            result[rowIndex]=1;            
+            for(int j=rowIndex-1;j>0;--j)
+            {
+                result[j]=result[j]+result[j-1];
+            }            
+        }
+        
+        return result;
+    }    
 };
