d176b3ee9ddb3a24bcb67dd3abf657b140a0d407
franky90237 <30579359+franky90237@users.noreply.github.com>
2021-12-01 11:26:21 +0800

    Update Medium_133. Clone Graph.c

--- Medium_133. Clone Graph.c
+++ Medium_133. Clone Graph.c
@@ -19,22 +19,26 @@ public:
 };
 */
 
+//clone graph with dfs
+//clone graph with bfs
 class Solution {
 public:
     Node* cloneGraph(Node* node) 
     {
         if(node==NULL) return node;
         
-        unordered_map<int,Node*> visited;
-        return dfs_clone(node,visited);
+        //unordered_map<Node*,Node*> visited;
+        //return dfs_clone(node,visited);
+        
+        return bfs_clone(node);
     }
     
-    Node* dfs_clone(Node* node, unordered_map<int,Node*>& visited)
+    Node* dfs_clone(Node* node, unordered_map<Node*,Node*>& visited)
     {
-        if(visited.find(node->val)!=visited.end()) return visited.find(node->val)->second;
+        if(visited.find(node)!=visited.end()) return visited.find(node)->second;
                              
         Node* n=new Node(node->val);        
-        visited.insert({node->val,n});
+        visited.insert({node,n});
         //print_node(*node);
         
         for(int i=0;i<node->neighbors.size();++i)
@@ -44,6 +48,51 @@ public:
         }
         
         return n;
+    }
+    
+    Node* bfs_clone(Node* node)
+    {
+        unordered_map<Node*,Node*> visited;
+        queue<Node*> q;
+        
+        q.push(node);
+        while(!q.empty())
+        {
+            Node* original_node=q.front();
+            q.pop();
+            //print_node(*original_node);
+            
+            Node* copied_node;
+            if(visited.find(original_node)==visited.end())
+            {
+                copied_node=new Node(original_node->val);
+                visited.insert({original_node,copied_node});
+            }
+            else
+            {
+                copied_node=visited[original_node];
+            }
+            
+            for(int i=0;i<original_node->neighbors.size();++i)
+            {
+                Node* copied_node_neighbor;
+                if(visited.find(original_node->neighbors[i])==visited.end())
+                {
+                    copied_node_neighbor=new Node(original_node->neighbors[i]->val);
+                    visited.insert({original_node->neighbors[i],copied_node_neighbor});
+                    
+                    q.push(original_node->neighbors[i]);
+                }
+                else
+                {
+                    copied_node_neighbor=visited[original_node->neighbors[i]];
+                }
+                
+                copied_node->neighbors.push_back(copied_node_neighbor);                
+            }            
+        }
+        
+        return visited[node];
     }
     
     void print_node(Node& node)
