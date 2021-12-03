f8d1c7b4348f31884d70771d55d4abe464b84a38
franky90237 <30579359+franky90237@users.noreply.github.com>
2021-12-03 19:28:49 +0800

    Update Medium_494. Target Sum.c

--- Medium_494. Target Sum.c
+++ Medium_494. Target Sum.c
@@ -1,3 +1,8 @@
+/*
+2021/12/02
+dfs (without memorization)
+bfs (Time Limit Exceeded)
+*/
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) 
@@ -64,4 +69,52 @@ public:
         }
         cout<<endl;
     }
+};
+
+/*
+2021/12/03
+dfs with memorization
+*/
+class Solution {
+private:
+    int **visited;    
+    int nums_sum;
+    
+public:        
+    int findTargetSumWays(vector<int>& nums, int target) 
+    {        
+        nums_sum=0;
+        for(int i=0;i<nums.size();++i) nums_sum+=nums[i];
+            
+        int row_size=nums.size()+1;
+        int col_size=2*nums_sum+1;
+        
+        visited=new int*[row_size];
+        for(int i=0;i<row_size;++i)
+        {
+            visited[i]=new int[col_size];
+            memset(visited[i],0,sizeof(int)*col_size);
+        }
+                
+        return dfs_memorization(nums,target,0,0);
+    }   
+    
+    int dfs_memorization(vector<int>& nums, int target, int sum, int step)
+    {
+        if(step==nums.size())
+        {
+            if(sum==target) return 1;
+            return 0;
+        }
+        
+        if(visited[step][sum+nums_sum])
+        {
+            //cout<<step<<" "<<sum+nums_sum<<endl;
+            return visited[step][sum+nums_sum];
+        }
+               
+        visited[step][sum+nums_sum]=\
+            dfs_memorization(nums,target,sum+nums[step],step+1)+dfs_memorization(nums,target,sum-nums[step],step+1);
+        return visited[step][sum+nums_sum];
+    }    
 };
