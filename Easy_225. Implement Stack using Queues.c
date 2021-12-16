c103d6c49f973ed15a15c8106b0f0fffd2f78450
franky90237 <30579359+franky90237@users.noreply.github.com>
2021-12-16 10:54:12 +0800

    Update Easy_225. Implement Stack using Queues.c

--- Easy_225. Implement Stack using Queues.c
+++ Easy_225. Implement Stack using Queues.c
@@ -95,17 +95,10 @@ public:
     }
 };
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
-
-
 //one queue
+//push  O(1)
+//pop   O(n)
+//top   O(n)
 class MyStack {
 private:
     queue<int> q;    
@@ -162,6 +155,48 @@ public:
     bool empty() 
     {
         return size==0;
+    }
+};
+
+//one queue
+//push  O(n)
+//pop   O(1)
+//top   O(1)
+class MyStack {
+private:
+    queue<int> q;
+    
+public:
+    MyStack() 
+    {
+    }
+    
+    void push(int x) 
+    {
+        int size=q.size();
+        
+        q.push(x);
+        for(int i=0;i<size;++i)
+        {
+            q.push(pop());
+        }                
+    }
+    
+    int pop() 
+    {        
+        int top=q.front();
+        q.pop();
+        return top;      
+    }
+    
+    int top() 
+    {        
+        return q.front();
+    }
+    
+    bool empty() 
+    {
+        return q.size()==0;
     }
 };
 
