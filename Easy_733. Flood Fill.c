bae85ffad813a7e13cd6814dc89b3aceab10c1fc
franky90237 <30579359+franky90237@users.noreply.github.com>
2021-12-29 09:19:55 +0800

    Update Easy_733. Flood Fill.c

--- Easy_733. Flood Fill.c
+++ Easy_733. Flood Fill.c
@@ -1,4 +1,5 @@
 //dfs recursive
+//space complexity O(row_size*column_size)
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)  
@@ -29,5 +30,40 @@ public:
         dfs(image,visited,sr-1,sc,sColor,newColor);
         dfs(image,visited,sr,sc+1,sColor,newColor);
         dfs(image,visited,sr,sc-1,sColor,newColor);                       
+    }
+};
+
+//dfs recursive
+//space complexity O(1)
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)  
+    {
+        if(image[sr][sc]!=newColor)        
+        {
+            dfs(image,sr,sc,image[sr][sc],newColor);
+        }
+        
+        return image;
+    }
+    
+    void dfs(vector<vector<int>>& image, int sr, int sc, int sColor, int newColor)
+    {
+        int r_size=image.size();
+        int c_size=image[0].size();
+        
+        //cout<<sr<<" "<<sc<<endl;
+        
+        if(r_size<=sr || sr<0 || c_size<=sc || sc<0 || image[sr][sc]!=sColor)
+        {
+            return;
+        }
+                
+        image[sr][sc]=newColor;
+        
+        dfs(image,sr+1,sc,sColor,newColor);
+        dfs(image,sr-1,sc,sColor,newColor);
+        dfs(image,sr,sc+1,sColor,newColor);
+        dfs(image,sr,sc-1,sColor,newColor);                       
     }
 };
