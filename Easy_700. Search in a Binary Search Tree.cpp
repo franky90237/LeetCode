7d981889562f2e1577a39f96ef555ccfc6aaf255
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-02-16 14:33:24 +0800

    Create Easy_700. Search in a Binary Search Tree.cpp

---
+++ Easy_700. Search in a Binary Search Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//iterative
+//time : O(log n)
+//space : O(1)
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) 
+    {
+        while(root)
+        {
+            if(root->val > val) root=root->left;
+            else if(root->val < val) root=root->right;
+            else return root;
+        }
+        
+        return NULL;
+    }
+};
