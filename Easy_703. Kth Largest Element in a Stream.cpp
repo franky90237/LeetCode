eff2cc67eab768316b8ab2820c295017aa79035b
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-02-22 16:53:23 +0800

    Update Easy_703. Kth Largest Element in a Stream.cpp

--- Easy_703. Kth Largest Element in a Stream.cpp
+++ Easy_703. Kth Largest Element in a Stream.cpp
@@ -37,3 +37,126 @@ public:
     }
 };
 
+typedef struct Treenode Treenode;
+struct Treenode 
+{
+    int val;
+    int cnt;
+    Treenode *left;
+    Treenode *right;
+    Treenode() : val(0), cnt(1), left(NULL), right(NULL) {}
+    Treenode(int x) : val(x), cnt(1), left(NULL), right(NULL) {}
+};
+
+//binary search tree
+class KthLargest 
+{
+private:
+    Treenode* root;
+    int k;
+    
+public:
+    KthLargest(int k, vector<int>& nums) 
+    {
+        root=NULL;
+        for(auto i:nums)
+        {
+            Treenode* n=new Treenode(i);
+            
+            if(!root) root=n;
+            else
+            {
+                Treenode* cur=root;
+                Treenode* pre=NULL;
+                
+                while(cur)
+                {
+                    ++cur->cnt;
+                    
+                    pre=cur;
+                    if(i>=cur->val) cur=cur->right;
+                    else cur=cur->left;
+                }
+                
+                if(i>=pre->val) pre->right=n;
+                else pre->left=n;
+            }
+        }
+        
+        this->k=k;
+    }
+    
+    int add(int val) 
+    {
+        Treenode* pre=NULL;
+        Treenode* cur=root;
+        Treenode* n=new Treenode(val);
+        
+        while(cur)
+        {
+            ++cur->cnt;
+                    
+            pre=cur;
+            if(val>=cur->val) cur=cur->right;
+            else cur=cur->left;
+        }
+        
+        if(!root) root=n;
+        else 
+        {
+            if(val>=pre->val) pre->right=n;
+            else pre->left=n;
+        }
+        
+        //BSTinorder(root);
+        
+        int kth=k;
+        cur=root;
+        while(cur)
+        {
+            //cout<<cur->cnt<<" ";
+            if(cur->left && cur->right)
+            {
+                //cout<<cur->right->cnt+1<<"-"<<k<<" ";
+                if(cur->right->cnt+1==kth) break;
+                if(cur->right->cnt+1 > kth)
+                {
+                    cur=cur->right;
+                }
+                else
+                {
+                    kth=kth-cur->right->cnt-1;
+                    cur=cur->left;
+                }
+            }
+            else if(cur->left)
+            {
+                if(kth==1) break;
+                
+                cur=cur->left;
+                --kth;
+            }
+            else if(cur->right)
+            {
+                if(cur->right->cnt+1==kth) break;
+                
+                cur=cur->right;
+            }
+            else
+            {
+                break;
+            }
+        }
+        //cout<<"---"<<cur->val<<endl;
+        return cur->val;
+    }
+    
+    void BSTinorder(Treenode* n)
+    {
+        if(!n) return;
+        
+        BSTinorder(n->left);
+        cout<<n->val<<"-"<<n->cnt<<" ";
+        BSTinorder(n->right);
+    }
+};
