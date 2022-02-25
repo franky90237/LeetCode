a004cafa31d598ffabc9003671e8c8ea79a1137d
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-02-25 11:14:18 +0800

    Update Medium_220. Contains Duplicate III.cpp

--- Medium_220. Contains Duplicate III.cpp
+++ Medium_220. Contains Duplicate III.cpp
@@ -14,3 +14,38 @@ public:
         return false;
     }
 };
+
+//binary search tree
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        if(k==0 || nums.size()<=1) return false;
+        
+        int n=nums.size();
+        multiset<int> bst;
+        
+        for(int i=0; i<n; ++i)
+        {
+            long min=(long)nums[i]-t<INT_MIN ? INT_MIN : nums[i]-t;
+            long max=(long)nums[i]+t>INT_MAX ? INT_MAX : nums[i]+t;
+                
+            multiset<int>::iterator l=bst.lower_bound(min);
+            multiset<int>::iterator h=bst.lower_bound(max);
+            
+            //for(auto i:bst) cout<<i<<" ";
+            //cout<<i<<"--"<<(long)*l<<" "<<(long)*h<<endl;
+                
+            if( (l!=bst.end() && (long)*l>=(long)nums[i]-t && (long)*l<=(long)nums[i]+t) || 
+               (h!=bst.end() && (long)*h>=(long)nums[i]-t && (long)*h<=(long)nums[i]+t) )
+            {
+                return true;
+            }
+            
+            if(bst.size()>=k) bst.erase(nums[i-k]);
+            
+            bst.insert(nums[i]);
+        }
+        
+        return false;
+    }
+};
