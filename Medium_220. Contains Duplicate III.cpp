11c2542a1baf6f015174b57373d0a8488aacd5ec
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-02-25 14:05:12 +0800

    Update Medium_220. Contains Duplicate III.cpp

--- Medium_220. Contains Duplicate III.cpp
+++ Medium_220. Contains Duplicate III.cpp
@@ -27,16 +27,13 @@ public:
         for(int i=0; i<n; ++i)
         {
             long min=(long)nums[i]-t<INT_MIN ? INT_MIN : nums[i]-t;
-            long max=(long)nums[i]+t>INT_MAX ? INT_MAX : nums[i]+t;
                 
             multiset<int>::iterator l=bst.lower_bound(min);
-            multiset<int>::iterator h=bst.lower_bound(max);
             
             //for(auto i:bst) cout<<i<<" ";
-            //cout<<i<<"--"<<(long)*l<<" "<<(long)*h<<endl;
+            //cout<<i<<"--"<<(long)*l<<" "<<endl;
                 
-            if( (l!=bst.end() && (long)*l>=(long)nums[i]-t && (long)*l<=(long)nums[i]+t) || 
-               (h!=bst.end() && (long)*h>=(long)nums[i]-t && (long)*h<=(long)nums[i]+t) )
+            if(l!=bst.end() && (long)*l>=(long)nums[i]-t && (long)*l<=(long)nums[i]+t)
             {
                 return true;
             }
