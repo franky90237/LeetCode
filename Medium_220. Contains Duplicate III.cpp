9e72d4e57bf3412551f15c4d32f75e9f27710d9e
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-02-25 16:43:00 +0800

    Update Medium_220. Contains Duplicate III.cpp

--- Medium_220. Contains Duplicate III.cpp
+++ Medium_220. Contains Duplicate III.cpp
@@ -46,3 +46,46 @@ public:
         return false;
     }
 };
+
+//bucket
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) 
+    {
+        if(k==0 || nums.size() <= 1) return false;
+        
+        int n=nums.size();
+        map<int,int> bucket;
+        
+        //cout<<-1/4<<" "<<-1%4<<endl;
+        //cout<<-4/4<<" "<<-4%4<<endl;
+        
+        for(int i=0; i<n; ++i)
+        {
+            int index=nums[i] / ((long)t+1);
+            if(nums[i] < 0) --index;
+                        
+            if(bucket.find(index)!=bucket.end() ||  
+              (bucket.find(index+1)!=bucket.end() && (long)bucket.find(index+1)->second-nums[i] <= t) ||
+              (bucket.find(index-1)!=bucket.end() && (long)nums[i]-bucket.find(index-1)->second <= t) )
+            {
+                return true;
+            }
+            
+            if(bucket.size()>=k)
+            {
+                int erased_index=nums[i-k] / ((long)t+1);
+                if(nums[i-k] < 0) --erased_index;
+                
+                bucket.erase(erased_index);
+            }
+            
+            bucket.insert(pair<int,int>(index,nums[i]));
+            
+            //for(auto j:bucket) cout<<j.second<<" ";
+            //cout<<"----"<<i<<endl;
+        }
+        
+        return false;
+    }
+};
