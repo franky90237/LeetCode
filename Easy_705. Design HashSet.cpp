55ed9800ee6b9e6e8ad9e7246e541f679e8059e5
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-03 17:02:31 +0800

    Update Easy_705. Design HashSet.cpp

--- Easy_705. Design HashSet.cpp
+++ Easy_705. Design HashSet.cpp
@@ -106,3 +106,68 @@ public:
         return false;
     }
 };
+
+//link list with loading_factor
+class MyHashSet 
+{
+private:
+    vector<list<int>> buckets;    
+    int capacity;
+    int count;
+    int loading_factor;
+    
+    int hash(int key)
+    {
+        return key % buckets.size();
+    }
+    
+    void rehash()
+    {
+        capacity*=2;
+        buckets.resize(capacity);        
+    }
+    
+public:
+    MyHashSet() : capacity(1000), count(0), loading_factor(0.75), buckets(1000) 
+    {
+        
+    }       
+    
+    void add(int key) 
+    {
+        if(contains(key)) return; 
+        
+        if(loading_factor*capacity>=count) rehash();
+        
+        int bucket=hash(key);
+        buckets[bucket].push_back(key);
+        ++count;
+    }
+    
+    void remove(int key) 
+    {
+        int bucket=hash(key);
+        
+        for(auto it=buckets[bucket].begin(); it!=buckets[bucket].end(); ++it)
+        {
+            if(*it==key) 
+            {
+                buckets[bucket].erase(it);
+                --count;
+                return;
+            }
+        }
+    }
+    
+    bool contains(int key)
+    {
+        int bucket=hash(key);
+                        
+        for(int i:buckets[bucket])
+        {
+            if(i==key) return true;
+        }
+        
+        return false;
+    }
+};
