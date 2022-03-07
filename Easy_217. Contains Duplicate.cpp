dfc17e80745d01f55354c7c952c6cd9615f7e944
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-07 10:00:58 +0800

    Update Easy_217. Contains Duplicate.cpp

--- Easy_217. Contains Duplicate.cpp
+++ Easy_217. Contains Duplicate.cpp
@@ -18,3 +18,22 @@ public:
     }
 };
 
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_set<int> s;
+        int n=nums.size();
+        
+        for(int i=0; i<n; ++i)
+        {
+            if(s.count(nums[i])==1) return true;
+            s.insert(nums[i]);
+        }
+        
+        return false;
+    }
+};
+
