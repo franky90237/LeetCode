6762e91aa130725fb938fa509ce57edacc11bf59
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-07 15:33:53 +0800

    Update Easy_349. Intersection of Two Arrays.cpp

--- Easy_349. Intersection of Two Arrays.cpp
+++ Easy_349. Intersection of Two Arrays.cpp
@@ -64,3 +64,35 @@ public:
         return res;
     }
 };
+
+//time. : O(nlogn) + O(mlogm)
+//space : O(1)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {        
+        vector<int> res;        
+        int n=nums1.size(), m=nums2.size();
+        
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        int i=0, j=0;
+        while(i<n && j<m)
+        {
+            //cout<<i<<"-"<<nums1[i]<<" "<<j<<"-"<<nums2[j]<<endl;
+            if(nums1[i] < nums2[j]) ++i;
+            else if(nums1[i] > nums2[j]) ++j;
+            else 
+            {
+                int size=res.size();
+                if(size==0 || res[size-1]!=nums1[i]) res.push_back(nums1[i]);
+                   
+                ++i; 
+                ++j;
+            }
+        }    
+        
+        return res;
+    }
+};
