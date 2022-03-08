0c0e82a79c833b52d10b72b53bc02aff3f0a0293
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-08 15:02:29 +0800

    Update Easy_1. Two Sum.cpp

--- Easy_1. Two Sum.cpp
+++ Easy_1. Two Sum.cpp
@@ -16,3 +16,32 @@ public:
         return {-1,-1};
     }
 };
+
+//time :  O(n)
+//space : O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> res;
+        unordered_multimap<int,int> m;
+        
+        int n=nums.size();
+        for(int i=0; i<n; ++i)
+        {
+            m.insert(make_pair(nums[i],i));
+        }
+        
+        for(auto i=m.begin(); i!=m.end();)
+        {
+            int dif=target-i->first;
+            int i_index=i->second;
+            i=m.erase(i);
+            
+            auto dif_find=m.find(dif);            
+            if(dif_find!=m.end()) return {i_index,dif_find->second};
+        }
+        
+        return {-1,-1};
+    }
+};
