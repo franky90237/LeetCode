777706dab9f950a5dc5a218ac348f14ed0073b80
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-10 14:45:34 +0800

    Update Easy_219. Contains Duplicate II.cpp

--- Easy_219. Contains Duplicate II.cpp
+++ Easy_219. Contains Duplicate II.cpp
@@ -1,3 +1,6 @@
+//hash map 
+//time  : O(n)
+//space : O(2n)
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) 
@@ -16,6 +19,27 @@ public:
             {
                 m[nums[i]]=i;
             }
+        }
+        
+        return false;
+    }
+};
+
+//simplified hash map 
+//time  : O(n)
+//space : O(2n)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) 
+    {
+        unordered_map<int,int> m;
+        
+        int n=nums.size();
+        for(int i=0; i<n; ++i)
+        {
+            if(m.find(nums[i])!=m.end() && i-m[nums[i]]<=k) return true;
+            
+            m[nums[i]]=i;
         }
         
         return false;
