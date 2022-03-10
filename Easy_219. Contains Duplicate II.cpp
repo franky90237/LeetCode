50cc0868c7f221468e8422cd913a203e237ef219
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-10 15:05:07 +0800

    Update Easy_219. Contains Duplicate II.cpp

--- Easy_219. Contains Duplicate II.cpp
+++ Easy_219. Contains Duplicate II.cpp
@@ -45,3 +45,24 @@ public:
         return false;
     }
 };
+
+//hash set 
+//time  : O(n)
+//space : O(k)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) 
+    {
+        unordered_set<int> s;
+        
+        int n=nums.size();
+        for(int i=0; i<n; ++i)
+        {
+            if(i>k) s.erase(nums[i-k-1]);
+            if(s.find(nums[i])!=s.end()) return true;
+            s.insert(nums[i]);
+        }
+        
+        return false;
+    }
+};
