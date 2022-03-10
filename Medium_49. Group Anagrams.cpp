a7d3fd2b75e5c63ac93b542284c021cd2e621ccd
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-10 17:16:20 +0800

    Update Medium_49. Group Anagrams.cpp

--- Medium_49. Group Anagrams.cpp
+++ Medium_49. Group Anagrams.cpp
@@ -1,3 +1,6 @@
+//hash of hash
+//time  : O(n*mlogm), n=strs.size() and m=strs[].size()
+//space : O(n)
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) 
@@ -25,6 +28,32 @@ public:
             {
                 res.push_back(j.second);
             }
+        }
+        
+        return res;
+    }
+};
+
+//hash map with sorted each strs[i] as a key 
+//time  : O(n*mlogm), n=strs.size() and m=strs[].size()
+//space : O(n)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        unordered_map<string,vector<string>> m;
+        
+        for(const auto& i:strs)
+        {
+            string s=i;
+            sort(s.begin(),s.end());                        
+            m[s].push_back(i);
+        }
+        
+        vector<vector<string>> res;
+        for(const auto& i:m)
+        {
+            res.push_back(i.second);
         }
         
         return res;
