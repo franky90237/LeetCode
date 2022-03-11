3004c0510f4087503683ee1c2ce01134c24150bf
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-11 11:16:25 +0800

    Update Medium_36. Valid Sudoku.cpp

--- Medium_36. Valid Sudoku.cpp
+++ Medium_36. Valid Sudoku.cpp
@@ -31,3 +31,29 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        char row[10][10]={0}, col[10][10]={0}, block[10][10]={0};
+        
+        for(int i=0; i<9; ++i)
+        {
+            for(int j=0; j<9; ++j)
+            {
+                if(board[i][j]!='.')
+                {
+                    int val=board[i][j]-'0';
+                    if(row[val][i]!=0 || col[val][j]!=0 || block[val][(i/3)*3+j/3]!=0) return false;
+                    
+                    row[val][i]=1;
+                    col[val][j]=1;
+                    block[val][(i/3)*3+j/3]=1;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
