f13488d9706148fd259155ac9fbd67695b2ad56e
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-14 10:26:08 +0800

    Update Medium_3. Longest Substring Without Repeating Characters.cpp

--- Medium_3. Longest Substring Without Repeating Characters.cpp
+++ Medium_3. Longest Substring Without Repeating Characters.cpp
@@ -26,3 +26,32 @@ public:
         return max(max_len,cur_len);
     }
 };
+
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        unordered_map<char,int> m;
+        
+        int cur_len=0;
+        int max_len=0;
+        int start=0;
+        for(int i=0; i<s.size(); ++i)
+        {
+            if(m.find(s[i])!=m.end() && m[s[i]]>=start)
+            {
+                cur_len=i-m[s[i]];
+                start=m[s[i]];
+            }
+            else ++cur_len;
+            //cout<<cur_len<<" ";
+            
+            m[s[i]]=i;
+            max_len=max(max_len,cur_len);
+        }
+        
+        return max_len;
+    }
+};
