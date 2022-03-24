872145ece5798184223ea34717e2143168221bdf
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-24 18:07:52 +0800

    Update Medium_77. Combinations.cpp

--- Medium_77. Combinations.cpp
+++ Medium_77. Combinations.cpp
@@ -28,3 +28,38 @@ public:
         //cout<<endl;
     }
 };
+
+/*class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) 
+    {
+        vector<vector<vector<int>>> cache[n+1];
+        
+        for(int i=1; i<=n; ++i) 
+        {
+            for(int j=1; j<=i; ++j)
+            {
+                vector<vector<int>> tmp;
+                cache[i].emplace_back(tmp);
+            }
+        }
+        
+        solve(n,k,cache);        
+        return cache[n][k];
+    }
+    
+    void solve(int n, int k, vector<vector<vector<int>>> cache[])
+    {
+        if(!cache[n][k].empty()) return;
+        
+        
+    }
+    
+    void print(int m, int p, int arr[][p])
+    {
+        int i, j;
+        for (i = 0; i < m; i++)
+          for (j = 0; j < p; j++)
+            printf("%d ", arr[i][j]);
+    }
+};*/
