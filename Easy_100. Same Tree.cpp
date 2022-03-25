8bb4a1a2813df256e99aaefa80aa2be89144fad0
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-25 10:33:18 +0800

    Update Easy_100. Same Tree.cpp

--- Easy_100. Same Tree.cpp
+++ Easy_100. Same Tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//recursive
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) 
@@ -18,5 +20,53 @@ public:
         if(p->val != q->val) return false;
         
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+
+//iterative
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
+        
+        stack<TreeNode*> sp;
+        stack<TreeNode*> sq;
+        TreeNode* curp=p;
+        TreeNode* curq=q;
+        
+        sp.push(curp);
+        sq.push(curq);
+        while(!sp.empty())
+        {
+            while(curp)
+            {
+                if(!check(curp->right,curq->right)) return false;
+                sp.push(curp->right);
+                sq.push(curq->right);
+                
+                curp=curp->left;
+                curq=curq->left;
+                
+                if(!check(curp,curq)) return false;
+            }
+            
+            curp=sp.top();
+            sp.pop();
+            curq=sq.top();
+            sq.pop();
+        }
+        
+        return true;
+    }
+    
+    bool check(TreeNode* p, TreeNode* q)
+    {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
+        return true;
     }
 };
