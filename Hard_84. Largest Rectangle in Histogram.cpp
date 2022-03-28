f97263d5714accfb61cf3b4cc72c772203fbd9e9
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-28 15:57:48 +0800

    Update Hard_84. Largest Rectangle in Histogram.cpp

--- Hard_84. Largest Rectangle in Histogram.cpp
+++ Hard_84. Largest Rectangle in Histogram.cpp
@@ -34,3 +34,41 @@ public:
         return max_size;
     }    
 };
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) 
+    {
+        int n=heights.size();
+        
+        int left_boundary[n];
+        left_boundary[0]=-1;
+        for(int i=1; i<n; ++i)
+        {
+            int pre=i-1;
+            while(pre>=0 && heights[i]<=heights[pre]) pre=left_boundary[pre];
+            
+            left_boundary[i]=pre;
+        }
+                
+        int right_boundary[n];
+        right_boundary[n-1]=n;
+        for(int i=n-2; i>=0; --i)
+        {
+            int pre=i+1;
+            while(pre<n && heights[i]<=heights[pre]) pre=right_boundary[pre];
+            
+            right_boundary[i]=pre;
+        }
+        
+        int max_size=0;
+        for(int i=0; i<n; ++i)
+        {
+            int height=heights[i];            
+            int width=right_boundary[i]-left_boundary[i]-1;
+            max_size=max(max_size,height*width);
+        }
+        
+        return max_size;
+    }    
+};
