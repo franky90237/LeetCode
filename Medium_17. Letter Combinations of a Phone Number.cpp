1dd706a4ca22b4f1a576bf08caad654f4ef594d6
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-29 14:28:52 +0800

    Update Medium_17. Letter Combinations of a Phone Number.cpp

--- Medium_17. Letter Combinations of a Phone Number.cpp
+++ Medium_17. Letter Combinations of a Phone Number.cpp
@@ -1,3 +1,4 @@
+//recursive : like DFS + backtracking
 class Solution 
 {
 private:
@@ -35,6 +36,52 @@ public:
             solve(res,tmp,digits,now+1);
             tmp.pop_back();
         }
+    }
+    
+    void config()
+    {
+        table[2]="abc";
+        table[3]="def";
+        table[4]="ghi";
+        table[5]="jkl";
+        table[6]="mno";
+        table[7]="pqrs";
+        table[8]="tuv";
+        table[9]="wxyz";
+    }
+};
+
+//iterative : like BFS
+class Solution 
+{
+private:
+    string table[10];
+    
+public:
+    vector<string> letterCombinations(string digits) 
+    {
+        if(digits.size()==0) return {};
+        
+        config();
+        
+        vector<string> res;
+        res.push_back("");
+        for(int i=0; i<digits.size(); ++i)
+        {
+            int num=digits[i]-'0';
+            vector<string> new_res;
+            for(auto& candidate:res)
+            {                
+                for(auto& s:table[num])
+                {
+                    new_res.push_back(candidate+s);
+                }
+            }
+            
+            swap(res,new_res);
+        }
+        
+        return res;
     }
     
     void config()
