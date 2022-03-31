1162eb14338311264c927757b845a46a5c05f361
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-03-31 17:39:18 +0800

    Update Easy_121. Best Time to Buy and Sell Stock.cpp

--- Easy_121. Best Time to Buy and Sell Stock.cpp
+++ Easy_121. Best Time to Buy and Sell Stock.cpp
@@ -39,3 +39,64 @@ public:
         return max;
     }
 };
+
+/*class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int n=prices.size();
+        if(n==1) return 0;
+        
+        int max=0;
+        int min=0;
+        find_max_min(prices,max,min);
+        
+        if(min<=max) return prices[max]-prices[min];
+        
+        int max_after_min=find_max(prices,min,n-1);
+        int min_befor_max=find_min(prices,0,max);
+        
+        int res1=prices[max_after_min]-prices[min];
+        int res2=prices[max]-prices[min_befor_max];
+        
+        return res1 > res2 ? res1 : res2;
+    }
+    
+    void find_max_min(vector<int>& prices, int& max, int & min)
+    {
+        int n=prices.size();
+        
+        for(int i=1; i<n; ++i)
+        {
+            if(prices[max]<prices[i]) max=i;
+            if(prices[min]>prices[i]) min=i;
+        }
+    }
+    
+    int find_max(vector<int>& prices, int begin, int end)
+    {
+        int max=begin;
+        for(int i=begin+1; i<=end; ++i)
+        {
+            if(prices[max]<prices[i]) max=i;
+        }
+        
+        //cout<<begin<<" "<<max<<endl;
+        
+        return max;
+    }
+    
+    int find_min(vector<int>& prices, int begin, int end)
+    {
+        int min=begin;
+        for(int i=begin+1; i<=end; ++i)
+        {
+            if(prices[min]>prices[i]) min=i;
+        }
+        
+        //cout<<begin<<" "<<min<<endl;
+        
+        return min;
+    }
+    
+};*/
