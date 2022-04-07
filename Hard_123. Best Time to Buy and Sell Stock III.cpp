987aecb3c890fe746db5397112f7a872589b3e42
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-07 16:58:28 +0800

    Update Hard_123. Best Time to Buy and Sell Stock III.cpp

--- Hard_123. Best Time to Buy and Sell Stock III.cpp
+++ Hard_123. Best Time to Buy and Sell Stock III.cpp
@@ -132,3 +132,30 @@ public:
         return dp[k-1];
     }
 };
+
+//time  : O(k*n)
+//space : O(k)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        const int n=prices.size();
+        
+        int buy1=-prices[0];
+        int sell1=0;
+        int buy2=-prices[0];
+        int sell2=0;
+        
+        for(int i=1; i<n; ++i)
+        {
+            buy1=max(buy1,-prices[i]);
+            sell1=max(sell1,buy1+prices[i]);
+            buy2=max(buy2,sell1-prices[i]);
+            sell2=max(sell2,buy2+prices[i]);
+           
+            //cout<<buy1<<" "<<sell1<<" "<<buy2<<" "<<sell2<<" "<<endl;           
+        }
+        
+        return sell2;
+    }
+};
