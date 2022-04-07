9671514a368c996233a039c2fdab27afab3e80f4
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-07 11:10:49 +0800

    Update Hard_123. Best Time to Buy and Sell Stock III.cpp

--- Hard_123. Best Time to Buy and Sell Stock III.cpp
+++ Hard_123. Best Time to Buy and Sell Stock III.cpp
@@ -1,39 +1,35 @@
+//TLE
+//time  : O(k*n*n)
+//space : O(k*n)
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) 
+    int maxProfit(vector<int>& prices)
     {
         const int n=prices.size();
         const int k=2+1;
-        int s0[n][k];
-        int s1[n][k];
         
-        for(int i=0; i<n; ++i) s0[i][0]=0;
-        for(int t=1; t<k; ++t) s0[0][t]=0;
+        int dp[k][n];
         
-        for(int i=0; i<n; ++i) s1[i][0]=0;
-        s1[0][0]=-prices[0];
-        s1[0][1]=-prices[0];
-        s1[0][2]=0;
+        for(int i=0; i<n; ++i) dp[0][i]=0;
+        for(int t=0; t<k; ++t) dp[t][0]=0;
         
-        for(int i=1; i<n; ++i)
+        for(int t=1; t<k; ++t)
         {
-            for(int t=1; t<k; ++t)
+            for(int i=1; i<n; ++i) 
             {
-                s0[i][t]=max(s0[i-1][t],s1[i-1][t-1]+prices[i]);
-                cout<<s0[i][t]<<" ";
+                int sell_at_i_profit=prices[i]-prices[0];
+                for(int j=i; j>=1; --j)
+                {
+                    sell_at_i_profit=max(sell_at_i_profit,prices[i]-prices[j]+dp[t-1][j]);
+                }
+                
+                dp[t][i]=max(dp[t][i-1],sell_at_i_profit);
+                
+                //cout<<dp[t][i]<<" ";
             }
-            cout<<endl;
-            
-            for(int t=1; t<k; ++t)
-            {
-                s1[i][t]=max(s1[i-1][t],s0[i-1][t-1]-prices[i]);
-            }
+            //cout<<endl;
         }
         
-        /*for(auto& i:s0) cout<<i<<"  ";
-        cout<<endl;
-        for(auto& i:s1) cout<<i<<" ";*/
-        
-        return s0[n-1][k-1];
+        return dp[k-1][n-1];
     }
 };
