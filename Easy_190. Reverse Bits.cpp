9428efcd0f91c65fd928956979629e0de4f65459
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-08 14:37:40 +0800

    Update Easy_190. Reverse Bits.cpp

--- Easy_190. Reverse Bits.cpp
+++ Easy_190. Reverse Bits.cpp
@@ -54,3 +54,26 @@ public:
         return res;
     }
 };
+
+//time  : O(1)
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {        
+        n = (n >> 16) | (n << 16);
+        
+        //ff00=0x 11111111 00000000, 00ff=0x 00000000 11111111
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        
+        //f0=0x 1111 0000, 0f=0x 0000 1111
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        
+        //c=0x 1100, 5=0x 0011
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        
+        //a=0x 10 10, 5=0x 01 01
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        
+        return n;
+    }
+};
