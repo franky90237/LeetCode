1a55396ea578edb100fb8b22726c8344decffc0e
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-11 14:25:59 +0800

    Update Easy_125. Valid Palindrome.cpp

--- Easy_125. Valid Palindrome.cpp
+++ Easy_125. Valid Palindrome.cpp
@@ -31,3 +31,27 @@ public:
         return alnum;
     }
 };
+
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    {
+        int n=s.size();
+        
+        int l=0, r=n-1;
+        while(l<r)
+        { 
+            if(!isalnum(s[l])) ++l;
+            if(!isalnum(s[r])) --r;
+            //cout<<s[l]<<" "<<s[r]<<endl;
+            if(isalnum(s[l]) && isalnum(s[r]))
+            {
+                if(tolower(s[l])!=tolower(s[r])) return false;
+                ++l;
+                --r;
+            }
+        }
+        
+        return true;
+    }
+};
