b39758eaa1261560928cf17f0625b20523f42ae1
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-14 17:15:34 +0800

    Update Medium_2095. Delete the Middle Node of a Linked List.cpp

--- Medium_2095. Delete the Middle Node of a Linked List.cpp
+++ Medium_2095. Delete the Middle Node of a Linked List.cpp
@@ -9,6 +9,7 @@
  * };
  */
 
+//2022-04-14
 //time  : O(n)
 //space : O(n)
 class Solution {
@@ -36,6 +37,49 @@ public:
         ListNode* pre=m[mid-1];
         pre->next=m[mid+1];
         
+        return head;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//2022-04-14
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) 
+    {
+        vector<ListNode*> m;
+        ListNode* cur=head;    
+        while(cur)
+        {
+            m.push_back(cur);
+            cur=cur->next;
+        }
+        
+        int cnt=m.size();
+        if(cnt==1) 
+        {
+            delete head;
+            return NULL;
+        }
+        
+        int mid=cnt/2;
+        delete m[mid];
+        ListNode* pre=m[mid-1];
+        if(mid+1>=cnt) pre->next=NULL;
+        else pre->next=m[mid+1];
+                
         return head;
     }
 };
