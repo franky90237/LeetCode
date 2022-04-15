86e73b1749342c0e206736defff3eb236031c301
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-15 17:44:16 +0800

    Update Medium_384. Shuffle an Array.cpp

--- Medium_384. Shuffle an Array.cpp
+++ Medium_384. Shuffle an Array.cpp
@@ -29,3 +29,35 @@ public:
         return data;
     }
 };
+
+//2022-04-15
+class Solution 
+{
+private:
+    vector<int> original;
+    vector<int> data;
+    
+public:
+    Solution(vector<int>& nums) 
+    {
+        original=nums;
+        data=nums;
+    }
+    
+    vector<int> reset() 
+    {        
+        return original;
+    }
+    
+    vector<int> shuffle() 
+    {
+        int n=data.size();
+        
+        for(int i=0; i<n; ++i)
+        {
+            swap(data[i],data[i+rand()%(n-i)]);
+        }
+                
+        return data;
+    }
+};
