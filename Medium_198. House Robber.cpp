3f5d19d32466eba9476e44b5fa0994d7bcd81e55
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-15 17:05:36 +0800

    Update Medium_198. House Robber.cpp

--- Medium_198. House Robber.cpp
+++ Medium_198. House Robber.cpp
@@ -39,3 +39,25 @@ public:
         return dp[now];
     }
 };
+
+//2022-04-15
+//dp iterative
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int dp[n+1];
+        
+        dp[n]=0;
+        dp[n-1]=nums[n-1];
+        for(int i=n-2; i>=0; --i)
+        {
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+        }
+        
+        return dp[0];
+    }
+};
