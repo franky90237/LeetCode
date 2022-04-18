36ff95cf45be00364ffb28f5a4f41cc3e61c9065
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-18 09:57:39 +0800

    Update Medium_204. Count Primes.cpp

--- Medium_204. Count Primes.cpp
+++ Medium_204. Count Primes.cpp
@@ -25,3 +25,31 @@ public:
         return cnt;
     }
 };
+
+//2022-04-18
+//time  : O(n^(1.5))
+//space : O(1)
+class Solution {
+public:
+    int countPrimes(int n) 
+    {
+        int cnt=0;
+        for(int i=2; i<n; ++i)
+        {
+            bool isPrime=true;
+            
+            for(int j=2; j<=sqrt(i); ++j)
+            {
+                if(i%j==0)
+                {
+                    isPrime=false;
+                    break;
+                }
+            }
+            
+            if(isPrime) ++cnt;
+        }
+        
+        return cnt;
+    }
+};
