60c519aab5fed7ae8b244b666f134e34961146a9
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-19 18:11:04 +0800

    Update Medium_1201. Ugly Number III.cpp

--- Medium_1201. Ugly Number III.cpp
+++ Medium_1201. Ugly Number III.cpp
@@ -112,3 +112,85 @@ public:
         return num;
     }
 };
+
+//2022-04-19
+//time  : O(log(2*10^(9)))
+//space : O(1)
+class Solution {
+public:
+    int nthUglyNumber(int n, int a, int b, int c) 
+    {
+        if(a==b && b==c) return a*n;
+        
+        vector<int> divisor({a,b,c});
+        sort(divisor.begin(),divisor.end());
+        
+        int size=1;
+        for(int i=1; i<3; ++i)
+        {
+            if(divisor[i]!=divisor[i-1]) 
+            {
+                divisor[size]=divisor[i];
+                ++size;
+            }
+        }
+        divisor.resize(size);
+                        
+        for(int i=size-1; i>0; --i)
+        {
+            for(int j=i-1; j>=0; --j)
+            {
+                if(divisor[i]%divisor[j]==0) 
+                {
+                    divisor.erase(divisor.begin()+i);
+                    break;
+                }
+            }
+        }
+        
+        //for(auto& i:divisor) cout<<i<<" ";
+        vector<int> lcm({})
+        int cnt=0;
+        int low=1;
+        int high=2000000000;
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            cnt=divisible_cnt(divisor,mid);
+            
+            //cout<<cnt<<" - "<<low<<" "<<mid<<" "<<high<<endl;
+            
+            if(cnt<=n) low=mid+1;
+            else       high=mid;
+        }
+        
+        return high;
+    }
+    
+    int divisible_cnt(vector<int>& divisor, int num)
+    {
+        int n=divisor.size();        
+        int cnt=0;
+        if(n==2)
+        {        
+            cnt+=num/divisor[0];
+            cnt+=num/divisor[1];
+            
+            cnt-=num/divisor[0]/divisor[1];
+        }
+        else 
+        {
+            cnt+=num/divisor[0];
+            cnt+=num/divisor[1];
+            cnt+=num/divisor[2];
+            
+            cnt-=num/divisor[0]/divisor[1];
+            cnt-=num/divisor[0]/divisor[2];
+            cnt-=num/divisor[1]/divisor[2];
+            
+            cnt+=num/divisor[0]/divisor[1]/divisor[2];
+        }
+        
+        return cnt;
+    }
+};
