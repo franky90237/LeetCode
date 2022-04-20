a81da964f66b973a88e1eb801e55a7c3e6e33dd0
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-20 14:10:10 +0800

    Update Easy_326. Power of Three.cpp

--- Easy_326. Power of Three.cpp
+++ Easy_326. Power of Three.cpp
@@ -15,3 +15,16 @@ public:
         return n==1;
     }
 };
+
+//2022-04-20
+//1162261467=3^19
+//3^19 is biggiest power of three int the range of INT_MAX
+//time  : O(1)
+//space : O(1)
+class Solution {
+public:
+    bool isPowerOfThree(int n) 
+    {
+        return n > 0 && 1162261467 % n == 0;
+    }
+};
