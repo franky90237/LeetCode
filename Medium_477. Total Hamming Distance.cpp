fdbe587ac1905a138e96fa7ff3e38fed0b5b4080
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-21 16:42:05 +0800

    Update Medium_477. Total Hamming Distance.cpp

--- Medium_477. Total Hamming Distance.cpp
+++ Medium_477. Total Hamming Distance.cpp
@@ -37,3 +37,27 @@ public:
         return number_of_1;
     }
 };
+
+//2022-04-21
+class Solution {   
+public:
+    int totalHammingDistance(vector<int>& nums) 
+    {        
+        int n=nums.size();        
+        
+        int res=0;        
+        for(int bit=0; bit<32; ++bit)
+        {
+            int number_of_1=0;
+            for(int i=0; i<n; ++i)
+            {
+                number_of_1 += (nums[i]>>bit) & 1;
+            }
+            
+            int number_of_0=n-number_of_1;
+            res+=number_of_1*number_of_0;
+        }
+        
+        return res;
+    }
+};
