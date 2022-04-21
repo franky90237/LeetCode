bf7400f6df966bc1e4b44e3a8a1f84a7e6e3287f
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-21 10:36:06 +0800

    Update Easy_338. Counting Bits.cpp

--- Easy_338. Counting Bits.cpp
+++ Easy_338. Counting Bits.cpp
@@ -14,7 +14,7 @@ public:
         int base=1;
         for(int i=2; i<=n; ++i)
         {
-            if(65536%i==0)
+            if(base*2==i)
             {
                 res[i]=1;
                 base=i;
@@ -24,6 +24,24 @@ public:
                 int diff=i-base;
                 res[i]=1+res[diff];
             }
+        }
+        
+        return res;
+    }
+};
+
+//2022-04-21
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int> res(n+1);
+        res[0]=0;
+        for(int i=1; i<=n; ++i)
+        {
+            res[i]=res[i/2]+i%2;
         }
         
         return res;
