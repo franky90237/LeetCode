0b6fce237b7983fbeea93ecb66c65dd5b84c878a
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-21 09:43:02 +0800

    Update Easy_191. Number of 1 Bits.cpp

--- Easy_191. Number of 1 Bits.cpp
+++ Easy_191. Number of 1 Bits.cpp
@@ -15,3 +15,21 @@ public:
         return cnt;
     }
 };
+
+//2022-04-21
+//time  : O(k), k is the number of 1 in n 
+//space : O(1)
+class Solution {
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        int cnt=0;
+        while(n>0)
+        {
+            ++cnt;
+            n=n&(n-1);
+        }
+        
+        return cnt;
+    }
+};
