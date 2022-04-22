c8a00f257e3e2e06e76a5506d21907208a14d8e7
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-22 09:54:11 +0800

    Update Easy_268. Missing Number.cpp

--- Easy_268. Missing Number.cpp
+++ Easy_268. Missing Number.cpp
@@ -14,3 +14,25 @@ public:
         return sum-missing_sum;
     }
 };
+
+//2022-04-22
+//bit manipulation
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int res=0;
+        
+        int i=0;
+        while(i<n) 
+        {
+            res=res^i^nums[i];
+            ++i;
+        }
+        
+        return res^i;
+    }
+};
