89d9bef9b542a4f1b078581f7a9a0ba62af2e793
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-22 14:21:07 +0800

    Update Medium_287. Find the Duplicate Number.cpp

--- Medium_287. Find the Duplicate Number.cpp
+++ Medium_287. Find the Duplicate Number.cpp
@@ -40,3 +40,33 @@ public:
         return low;
     }
 };
+
+//2022-04-22
+//clear binary search
+//time  : O(nlog(n))
+//space : O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        int n=nums.size()-1;
+        int low=1;
+        int high=n;
+        
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            
+            int cnt=0;
+            for(auto& i:nums)
+            {
+                if(i<=mid) ++cnt;
+            }
+                        
+            if(cnt>mid) high=mid;
+            else        low=mid+1;            
+        }
+        
+        return low;
+    }
+};
