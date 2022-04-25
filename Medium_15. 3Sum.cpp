ebdcd5dfadc01399bc5c7b6776d645318a03fa73
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-25 15:43:34 +0800

    Update Medium_15. 3Sum.cpp

--- Medium_15. 3Sum.cpp
+++ Medium_15. 3Sum.cpp
@@ -158,3 +158,41 @@ public:
         //cout<<endl;
     }
 };
+
+//2022-04-25
+//time  : O(n*n)
+//space : O(1)
+class Solution {    
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        int n=nums.size();
+        
+        sort(nums.begin(),nums.end());
+        
+        vector<vector<int>> res;
+        for(int i=0; i<n; ++i)
+        {
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            
+            int target=-nums[i];
+            int left=i+1;
+            int right=n-1;
+            while(left<right)
+            {
+                int sum=nums[left]+nums[right];
+                
+                if(sum==target)
+                {
+                    res.push_back({nums[i],nums[left],nums[right]});
+                    while(left<right && nums[left]==res[res.size()-1][1]) ++left;
+                    while(left<right && nums[right]==res[res.size()-1][2]) --right;
+                }
+                else if(sum>target) --right;
+                else ++left;
+            }
+        }
+        
+        return res;
+    }
+};
