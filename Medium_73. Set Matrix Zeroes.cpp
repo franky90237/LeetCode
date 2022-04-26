d3f69be0f25083f2220e79c1999221c37bf0bdca
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-04-26 11:15:43 +0800

    Update Medium_73. Set Matrix Zeroes.cpp

--- Medium_73. Set Matrix Zeroes.cpp
+++ Medium_73. Set Matrix Zeroes.cpp
@@ -33,3 +33,79 @@ public:
         }
     }
 };
+
+//2022-04-26
+//time  : O(m*n)
+//space : O(1)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        bool is_column0=false;
+        bool is_row0=false;
+        
+        if(matrix[0][0]==0)
+        {
+            is_column0=true;
+            is_row0=true;
+        }
+        else
+        {
+            for(int c=1; c<n; ++c)
+            {
+                if(matrix[0][c]==0) 
+                {
+                    is_row0=true;
+                    break;
+                }
+            }
+        
+            for(int r=1; r<m; ++r)
+            {
+                if(matrix[r][0]==0) 
+                {
+                    is_column0=true;
+                    break;
+                }
+            }
+        }
+        
+        for(int i=1; i<m; ++i)
+        {
+            for(int j=1; j<n; ++j)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<m; ++i)
+        {
+            for(int j=1; j<n; ++j)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        //for first row
+        if(is_row0)
+        {
+            for(int c=0; c<n; ++c) matrix[0][c]=0;
+        }
+        
+        //for first column
+        if(is_column0)
+        {
+            for(int r=0; r<m; ++r) matrix[r][0]=0;
+        }
+    }
+};
