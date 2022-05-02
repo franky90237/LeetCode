6cc17ee2c860232e91f159cc4564eccf3c624b53
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-02 14:53:03 +0800

    Update Medium_90. Subsets II.cpp

--- Medium_90. Subsets II.cpp
+++ Medium_90. Subsets II.cpp
@@ -36,3 +36,38 @@ public:
         return res;
     }
 };
+
+//2022-05-02
+//backtracking
+//time  : O(2^n)
+//space : O(n)
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        
+        vector<vector<int>> res;
+        vector<int> tmp;
+        solve(res,nums,tmp,0);
+        
+        //cout<<res.size()<<endl;
+        return res;
+    }
+    
+    void solve(vector<vector<int>>& res, vector<int>& nums, vector<int>& tmp, int now)
+    {
+        res.push_back(tmp);
+        
+        int n=nums.size();
+        if(now>=n) return;
+        
+        for(int i=now; i<n; ++i)
+        {
+            if(i!=now && i-1>=0 && nums[i]==nums[i-1]) continue;
+            tmp.push_back(nums[i]);
+            solve(res,nums,tmp,i+1);
+            tmp.pop_back();
+        }
+    }
+};
