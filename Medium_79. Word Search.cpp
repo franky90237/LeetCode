be9a1fc9595ce3877e4d5464c37b4eacb6df4fb3
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-03 11:12:08 +0800

    Create Medium_79. Word Search.cpp

---
+++ Medium_79. Word Search.cpp
@@ -0,0 +1,65 @@
+//2022-05-03
+//backtracking + dfs
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word)
+    {
+        int m=board.size();
+        int n=board[0].size();
+        
+        vector<vector<bool>> v(m,vector<bool>(n,false));
+        string s="";
+        
+        bool isFind=false;
+        for(int r=0; r<m; ++r)
+        {
+            for(int c=0; c<n; ++c)
+            {
+                if(board[r][c]==word[0])
+                {
+                    v[r][c]=true;
+                    s.push_back(board[r][c]);
+                    
+                    isFind=solve(board,word,v,s,r,c);
+                    if(isFind) return true;
+                    
+                    v[r][c]=false;
+                    s.pop_back();
+                }
+            }
+        }
+        
+        return isFind;
+    }
+    
+    bool solve(vector<vector<char>>& board, string& word, vector<vector<bool>>& v, string& s, int r, int c)
+    {
+        if(s[s.size()-1]!=word[s.size()-1]) return false;
+        if(s.size()==word.size()) return true;
+            
+        int m=board.size();
+        int n=board[0].size();
+        int dir[5]={0,1,0,-1,0};
+        
+        for(int i=1; i<5; ++i)
+        {
+            int rn=r+dir[i-1];
+            int cn=c+dir[i];
+            //cout<<board[r][c]<<"-   "<<rn<<" "<<cn<<endl;
+            
+            if(rn>=0 && rn<m && cn>=0 && cn<n && !v[rn][cn])
+            {
+                v[rn][cn]=true;
+                s.push_back(board[rn][cn]);
+                
+                bool isFind=solve(board,word,v,s,rn,cn);
+                if(isFind) return true;
+                
+                v[rn][cn]=false;
+                s.pop_back();
+            }
+        }
+        
+        return false;
+    }
+};
