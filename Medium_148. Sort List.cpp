9222aa70d0ceb1605994c41672a776d4cf33ba89
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-05 16:17:36 +0800

    Update Medium_148. Sort List.cpp

--- Medium_148. Sort List.cpp
+++ Medium_148. Sort List.cpp
@@ -8,87 +8,173 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+//2022-05-04
+//TLE
+//quick sort
+//time  : O(nlog(n))
+//space : O(log(n))
 class Solution {
 public:
-    ListNode* sortList(ListNode* head)
+    ListNode* sortList(ListNode* head) 
     {
-        merge_sort(head);
+        quick_sort(head,NULL);
         return head;
     }
     
-    void merge_sort(ListNode* head)
+    void quick_sort(ListNode* head, ListNode* tail)
+    {
+        if(head==tail || head->next==tail) return;
+        //printf("%d. \n",head->val);
+        
+        ListNode* pivot=partition(head,tail);
+        quick_sort(head,pivot);
+        quick_sort(pivot->next,tail);
+    }
+    
+    ListNode* partition(ListNode* head, ListNode* tail)
+    {
+        ListNode* small=head;
+        ListNode* curr=head->next;
+        
+        while(curr!=tail)
+        {
+            if(curr->val<head->val)
+            {
+                int tmp=curr->val;
+                curr->val=small->next->val;
+                small->next->val=tmp;
+                
+                small=small->next;
+            }
+            
+            curr=curr->next;
+        }
+        
+        if(small->val<head->val)
+        {
+            int tmp=small->val;
+            small->val=head->val;
+            head->val=tmp;
+        }
+        
+        //print_list(head);
+        
+        return small;
+    }
+    
+    void print_list(ListNode* head)
+    {
+        while(head)
+        {
+            printf("%d ",head->val);
+            head=head->next;
+        }
+        
+        printf("\n");
+    }
+};
+
+//2022-05-04
+//merge sort
+//time  : O(nlog(n))
+//space : O(1)
+class Solution {
+public:
+    ListNode* sortList(ListNode* head)
+    {        
+        return merge_sort(head);
+    }
+    
+    ListNode* merge_sort(ListNode* head)
     {
         int size=1;
         int len=get_len(head);
         
         while(size<=len)
         {
+            ListNode* end=NULL;
+            ListNode* pre=NULL;
             ListNode* cur=head;
             ListNode* fs=cur;
-            ListNode* fe=cur
+            ListNode* fe=cur;
             ListNode* ss=cur;
-            ListNode* ss=cur;
+            ListNode* se=cur;
             
-            int cnt=ceil(len/(2.0*size));
+            int cnt=ceil((float)len/(2.0*size));            
             for(int i=0; i<cnt; ++i)
-            {
+            {                
+                pre=end;
                 fs=cur;
                 
-                cur=spilt(*fs,*fe,*ss,*se,size);
-                ListNode* last=merge(*fs,*fe,*ss,*se,size);
+                cur=split(fs,fe,ss,se,size);
+                if(!ss) break;
+                //printf("%d %d %d %d - %d \n",fs->val, fe->val, ss->val, se->val, cur->val);
+                /*if(i==1) 
+                {
+                    printf("%d ",fs->val);
+                    printf("%d ",fe->val);
+                    printf("%d \n",ss->val);
+                    //printf("%d \n",cur->val);
+                }*/
                 
-                last->next=cur;
+                ListNode* start=merge(fs,fe,ss,se,cur,end);
+                if(i==0) head=start;
+                else if(pre) pre->next=start;
+                
+                //print_list(head);
             }
             
+            //cout<<cnt<<endl<<endl;
             size*=2;
         }
+        
+        return head;
     }
     
-    ListNode* split(ListNode** fs, ListNode** fe, ListNode** ss, ListNode** se, int size)
+    ListNode* split(ListNode*& fs, ListNode*& fe, ListNode*& ss, ListNode*& se, int size)
     {
         ListNode* cur=fs;
         
-        for(int i=0; i<size; ++i)
+        for(int i=0; cur && i<size; ++i)
         {
-            //**fe=&cur;
+            //fe=cur;
             cur=cur->next;
         }
         
-        **fe=&cur;
-        **ss=&cur;
+        fe=cur;
+        ss=cur;
         for(int i=0; cur && i<size; ++i)
         {
             //se=cur;
             cur=cur->next;
         }
-        **se=&cur;
+        se=cur;
         
         return cur;
     }
     
-    ListNode* merge(ListNode* fs, ListNode* fe, ListNode* ss, ListNode* se, int size)
+    ListNode* merge(ListNode*& fs, ListNode*& fe, ListNode*& ss, ListNode*& se, ListNode*& last, ListNode*& end)
     {
         ListNode dummy;
-        ListNode* cur=&dummy;
-        //int cf=0, cs=0;
+        ListNode* cur=&dummy;        
         
-        while(fs!=fe && st!=se)
+        while(fs!=fe && ss!=se)
         {
             if(fs->val < ss->val)
             {
                 cur->next=fs;
                 fs=fs->next;
-                //++cf;
             }
             else
             {
                 cur->next=ss;
                 ss=ss->next;
-                //++cs;
             }
-            
+                     
             cur=cur->next;
-        }
+        }                
         
         while(fs!=fe)
         {
@@ -104,7 +190,10 @@ public:
             cur=cur->next;
         }
         
-        return cur;
+        end=cur;
+        cur->next=last;
+        //print_list(&dummy);
+        return dummy.next;
     }
     
     int get_len(ListNode* head)
@@ -117,5 +206,16 @@ public:
         }
         
         return len;
+    }
+    
+    void print_list(ListNode* head)
+    {
+        while(head)
+        {
+            cout<<head->val<<" ";
+            head=head->next;
+        }
+        
+        cout<<endl;
     }
 };
