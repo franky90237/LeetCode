22977f4f6df7fdb920110e660cb86724b651f031
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-10 17:00:13 +0800

    Update Medium_62. Unique Paths.cpp

--- Medium_62. Unique Paths.cpp
+++ Medium_62. Unique Paths.cpp
@@ -22,3 +22,35 @@ public:
         return dp[0][0];
     }
 };
+
+//2022-05-10
+//reference : https://leetcode.com/problems/unique-paths/discuss/22958/Math-solution-O(1)-space
+//combination
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int uniquePaths(int m, int n)
+    {
+        if(m==1 || n==1) return 1;
+        
+        --m;
+        --n;
+        
+        if(m<n) swap(m,n);
+        
+        long res=1;
+        int c1=m+1;
+        int c2=1;        
+        while(c1<=m+n) 
+        {            
+            res*=c1;
+            res/=c2;
+            
+            ++c1;
+            ++c2;
+        }                
+        
+        return res;
+    }
+};
