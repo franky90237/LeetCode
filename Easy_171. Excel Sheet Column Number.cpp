9f69801b236c7085a94b73f3c0c4b76c4913cd85
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-13 17:09:12 +0800

    Update Easy_171. Excel Sheet Column Number.cpp

--- Easy_171. Excel Sheet Column Number.cpp
+++ Easy_171. Excel Sheet Column Number.cpp
@@ -20,3 +20,25 @@ public:
         return res;
     }
 };
+
+//2022-05-13
+//concise
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int titleToNumber(string columnTitle) 
+    {                
+        int n=columnTitle.size();
+        
+        int res=0;        
+        for(int i=0; i<n; ++i)
+        {
+            int number=(columnTitle[i]-'A')+1;
+            //cout<<number<<endl;
+            res=res*26+number;
+        }
+        
+        return res;
+    }
+};
