a70dcc093009150e8e4b2505d47fcb6577000a57
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-19 17:38:18 +0800

    Update Medium_767. Reorganize String.cpp

--- Medium_767. Reorganize String.cpp
+++ Medium_767. Reorganize String.cpp
@@ -1,41 +1,68 @@
+//2022-05-19
+//reference : https://leetcode.com/problems/reorganize-string/discuss/232469/Java-No-Sort-O(N)-0ms-beat-100
+//time  : O(n)
+//space : O(26)
 class Solution {
 public:
     string reorganizeString(string s)
-    {
-        string res(s.size(),'');
-        
+    {        
         unordered_map<char,int> letter;
-        //int letter[26]={0};
         int max_freq=0;
-        int number_of_max_freq=0;
+        char max_freq_letter;
         
-        for(auto& i:tasks)
+        for(auto& i:s)
         {
             ++letter[i];
-            max_freq=max(max_freq,letter[i]);
-        }
-        //printf("%d %d \n",max_freq,number_of_max_freq);
-        
-        auto it=letter.begin();
-        while(it!=letter.end())
-        {
-            
-        }
-        
-        int start=0;
-        for(int i=0; i<26; ++i)
-        {
-            
-            if(letter[i]==max_freq)
+            if(max_freq<letter[i])
             {
-                while(start<res.size())
-                {
-                    
-                }
+                max_freq=letter[i];
+                max_freq_letter=i;
             }
         }
+        //printf("%c %d \n",max_freq_letter,max_freq);
         
-        int empty_letter=(max_freq-1);
+        if(max_freq > 1 + (s.size()-1)/2) return "";
         
+        string ans(s.size(),'.');
+        
+        int i=0;
+        while(letter[max_freq_letter]>0)
+        {
+            ans[i]=max_freq_letter;
+            --letter[max_freq_letter];
+            
+            i+=2;
+        }
+        //cout<<ans<<endl;
+        
+        for(auto& it:letter)
+        {
+            while(it.second>0 && i<s.size())
+            {
+                ans[i]=it.first;
+                --it.second;
+                
+                i+=2;
+            }
+            
+            if(i>=s.size()) break;
+        }
+        //cout<<ans<<endl;
+        
+        i=1;
+        for(auto& it:letter)
+        {
+            while(it.second>0 && i<s.size())
+            {
+                ans[i]=it.first;
+                --it.second;
+                
+                i+=2;
+            }
+            
+            if(i>=s.size()) break;
+        }
+                        
+        return ans;
     }
 };
