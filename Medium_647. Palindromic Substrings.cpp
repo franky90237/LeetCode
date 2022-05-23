3ad0ca965e37f2ca3025b66ec4fa5f12a38a8790
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-23 10:55:25 +0800

    Update Medium_647. Palindromic Substrings.cpp

--- Medium_647. Palindromic Substrings.cpp
+++ Medium_647. Palindromic Substrings.cpp
@@ -33,3 +33,53 @@ public:
         return true;
     }
 };
+
+//2022-05-23
+//dp
+//time  : O(n*n)
+//space : O(n*n)
+class Solution {
+public:
+    int countSubstrings(string s)
+    {
+        int n=s.size();
+        
+        vector<vector<int>> substring(n,vector<int>(n,-1));
+        
+        int res=n;
+        for(int cnt=2; cnt<=n; ++cnt)
+        {
+            for(int start=0; start+cnt-1<n; ++start)
+            {
+                int end=start+cnt-1;
+                //cout<<s.substr(start,cnt)<<" "<<start+cnt-1<<endl;
+                
+                if(substring[start][end]==-1)
+                {
+                    //cout<<s.substr(start,cnt)<<endl;
+                    bool flag=is_palindromic(substring,s,start,end);
+                    substring[start][end]=flag;
+                }
+                
+                res+=substring[start][end];
+                
+            }
+        }
+        
+        return res;
+    }
+    
+    bool is_palindromic(vector<vector<int>>& substring, string& s, int start, int end)
+    {
+        while(start<end)
+        {
+            if(substring[start][end]!=-1) return substring[start][end];
+            if(s[start]!=s[end]) return false;
+            
+            ++start;
+            --end;
+        }
+        
+        return true;
+    }
+};
