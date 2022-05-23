24462d42f8122df56a22102d1b8809c5e644bef1
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-23 15:45:42 +0800

    Update Medium_238. Product of Array Except Self.cpp

--- Medium_238. Product of Array Except Self.cpp
+++ Medium_238. Product of Array Except Self.cpp
@@ -25,3 +25,30 @@ public:
         return res;
     }
 };
+
+//2022-05-23
+//clear one-pass no division operator
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        int n=nums.size();
+        
+        vector<int> res(n,1);
+        int pre=1;
+        int post=1;
+        for(int i=0; i<n; ++i)
+        {
+            //printf("%d, %d - %d, %d \n",pre,post, res[i], res[n-1-i]);
+            res[i]=res[i]*pre;
+            pre=pre*nums[i];
+            
+            res[n-1-i]=res[n-1-i]*post;
+            post=post*nums[n-1-i];
+        }
+        
+        return res;
+    }
+};
