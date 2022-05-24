506f10d8b458a913abcc9d4f6e7ea35f8f3cf195
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-24 15:59:43 +0800

    Create Medium_153. Find Minimum in Rotated Sorted Array.cpp

---
+++ Medium_153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,28 @@
+//2022-05-24
+//time  : O(log(n))
+//space : O(1)
+class Solution {
+public:
+    int findMin(vector<int>& nums)
+    {
+        int left=0;
+        int right=nums.size()-1;
+        int mid;
+        
+        while(left<right)
+        {
+            mid=(left+right)/2;
+            
+            if(nums[mid]>nums[right])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+        }
+        
+        return nums[left];
+    }
+};
