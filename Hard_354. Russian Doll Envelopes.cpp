835759fa98d5757343f4e5a8cfb466e4762c0176
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-25 15:13:51 +0800

    Update Hard_354. Russian Doll Envelopes.cpp

--- Hard_354. Russian Doll Envelopes.cpp
+++ Hard_354. Russian Doll Envelopes.cpp
@@ -83,3 +83,79 @@ public:
         printf("\n");
     }
 };
+
+//2022-05-25
+//sorting + longest increasing subsequence
+//time. : O(nlog(n))
+//space : O(n)
+class Solution {
+private:
+    struct mycompare
+    {
+        bool operator()(vector<int>& a, vector<int>& b)
+        {
+            //cout<<a[0]<<" "<<b[0]<<endl;
+            
+            if(a[0]<b[0]) return true;
+            if(a[0]==b[0])
+            {
+                if(a[1]>b[1]) return true;
+                else return false;
+            }
+            
+            return false;
+        }
+    } comp;
+    
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes)
+    {
+        sort(envelopes.begin(),envelopes.end(),comp);
+        //print(envelopes);
+        
+        int n=envelopes.size();
+        vector<int> lis;
+        lis.push_back(envelopes[0][1]);
+        for(int i=1; i<n; ++i)
+        {
+            int h=envelopes[i][1];
+            
+            if(lis.back()<h) lis.push_back(h);
+            else if(lis.back()==h) continue;
+            else 
+            {
+                int idx=binary_search(lis,h);
+                lis[idx]=h;
+            }
+        }
+        
+        return lis.size();
+        
+        return 0;
+    }
+    
+    int binary_search(vector<int>& lis, int target)
+    {
+        int left=0;
+        int right=lis.size()-1;
+        
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            
+            if(lis[mid]>=target) right=mid;
+            else left=mid+1;
+        }
+        
+        return left;
+    }
+    
+    void print(vector<vector<int>>& envelopes)
+    {
+        for(auto& i: envelopes)
+        {
+            printf("%d, %d \n",i[0],i[1]);
+        }
+        printf("\n");
+    }
+};
