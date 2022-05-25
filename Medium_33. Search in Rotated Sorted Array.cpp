5d8618b27070b79e31d1e65ccef32b365bbe818c
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-25 08:18:39 +0800

    Create Medium_33. Search in Rotated Sorted Array.cpp

---
+++ Medium_33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,32 @@
+//2022-05-25
+//time  : O(log(n))
+//space : O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        int left=0;
+        int right=nums.size()-1;
+        
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            //printf("%d %d %d\n",nums[left],nums[mid],nums[right]);
+            
+            if(nums[mid]==target) return mid;
+            
+            if(nums[mid]<nums[right])
+            {
+                if(nums[mid]<target && target<=nums[right]) left=mid+1;
+                else right=mid-1;
+            }
+            else
+            {
+                if(nums[left]<=target && target<nums[mid]) right=mid-1;
+                else left=mid+1;
+            }
+        }
+        
+        return -1;
+    }
+};
