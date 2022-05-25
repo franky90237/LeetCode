a8d74d184de3039f891d7c15d34c1e7d43bd4033
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-25 09:50:21 +0800

    Update Medium_11. Container With Most Water.cpp

--- Medium_11. Container With Most Water.cpp
+++ Medium_11. Container With Most Water.cpp
@@ -1,6 +1,6 @@
 //2022-05-25
 //TLE
-//time. : O(n*n)
+//time  : O(n*n)
 //space : O(1)
 class Solution {
 public:
@@ -15,6 +15,40 @@ public:
             {
                 int water=min(height[left],height[right])*(right-left);
                 res=max(res,water);
+            }
+        }
+        
+        return res;
+    }
+};
+
+//2022-05-25
+//greedy
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height)
+    {
+        int n=height.size();
+        
+        int left=0;
+        int right=n-1;
+        int res=0;
+        
+        while(left<right)
+        {
+            int width=right-left;
+            
+            if(height[left]<height[right])
+            {
+                res=max(res,height[left]*width);
+                ++left;
+            }
+            else
+            {
+                res=max(res,height[right]*width);
+                --right;
             }
         }
         
