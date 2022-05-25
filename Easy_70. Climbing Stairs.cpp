bb7535685b2287d84e7f7297a4c0a00ebf0dd44a
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-05-25 10:38:46 +0800

    Create Easy_70. Climbing Stairs.cpp

---
+++ Easy_70. Climbing Stairs.cpp
@@ -0,0 +1,22 @@
+//2022-05-25
+//dp iterative
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int climbStairs(int n)
+    {
+        if(n<=2) return n;
+        
+        int dp[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3; i<=n; ++i)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
