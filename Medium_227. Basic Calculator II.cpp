9ef1a29a2e1b4343b88f1283376c1578f3bd2b3a
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-02 18:12:27 +0800

    Update Medium_227. Basic Calculator II.cpp

--- Medium_227. Basic Calculator II.cpp
+++ Medium_227. Basic Calculator II.cpp
@@ -59,3 +59,50 @@ public:
         return res;
     }
 };
+
+//2022-06-02
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int calculate(string s)
+    {
+        s+="++";
+        int res=0;
+        int tmp=0;
+        int integer=0;
+        int prevOp='+';
+        
+        for(int i=0; i<s.size(); ++i)
+        {
+            char c=s[i];
+            if(c==' ') continue;
+            //printf("%c : %d %d %d\n",c,res,tmp,integer);
+            
+            if(isdigit(c))
+            {
+                integer=integer*10+(c-'0');
+            }
+            else
+            {
+                if(prevOp=='*') tmp=tmp*integer;
+                else if(prevOp=='/') tmp=tmp/integer;
+                else if(prevOp=='+')
+                {
+                    res=res+tmp;
+                    tmp=integer;
+                }
+                else if(prevOp=='-')
+                {
+                    res=res+tmp;
+                    tmp=-integer;
+                }
+                
+                integer=0;
+                prevOp=c;
+            }
+        }
+        
+        return res;
+    }
+};
