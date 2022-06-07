c2abdfc2da380edc597d691f804f2f90a1e03564
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-07 14:49:12 +0800

    Update Hard_76. Minimum Window Substring.cpp

--- Hard_76. Minimum Window Substring.cpp
+++ Hard_76. Minimum Window Substring.cpp
@@ -72,3 +72,79 @@ public:
         cout<<endl;
     }
 };
+
+//2022-06-07
+//two pointer
+//time  : O(n+m)
+//space : O(m)
+class Solution {
+public:
+    string minWindow(string s, string t)
+    {
+        unordered_map<char,int> dict,record;
+        for(auto& c:t) 
+        {
+            ++dict[c];
+            record[c]=0;
+        }
+        
+        int cnt=0;
+        int len=s.size()+1;
+        int resL=-1;
+        int resR=-1;
+        int left=0;
+        int right=0;
+        while(left<=right && right<s.size())
+        {
+            char c=s[right];
+            if(record.find(c)==record.end())
+            {
+                ++right;
+                continue;
+            }
+            
+            ++record[c];
+            if(record[c]==dict[c]) ++cnt;
+            
+            //cout<<right<<endl;
+            //print(record);
+            while(cnt==dict.size() && left<=right)
+            {
+                if(len>right-left+1)
+                {
+                    len=right-left+1;
+                    resL=left;
+                    resR=right;
+                }
+                
+                if(dict.find(s[left])!=dict.end())
+                {
+                    --record[s[left]];
+                    if(record[s[left]]<dict[s[left]]) --cnt;
+                    
+                    //cout<<left<<endl;
+                    //print(record);
+                }
+                
+                ++left;
+            }
+                
+            ++right;
+        }
+        
+        //cout<<resL<<" "<<resR<<endl;
+        return (resL==-1) ? "" : string(s,resL,resR-resL+1);
+    }
+    
+    void print(unordered_map<char,int>& record)
+    {
+        for(auto& i:record)
+        {
+            cout<<i.first<<":";
+            cout<<i.second<<" ";
+            cout<<endl;
+        }
+        
+        cout<<endl;
+    }
+};
