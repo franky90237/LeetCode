2b9f9862fc8ecfc7c1c1aedaf3207b28f932ca2f
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-08 07:27:30 +0800

    Update Medium_377. Combination Sum IV.cpp

--- Medium_377. Combination Sum IV.cpp
+++ Medium_377. Combination Sum IV.cpp
@@ -22,3 +22,33 @@ public:
         return sum;
     }
 };
+
+//2022-06-08
+//dp recursive
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target)
+    {
+        int dp[target+1];
+        for(int i=0; i<=target; ++i) dp[i]=-1;
+        
+        return solve(0,dp,nums,target);
+    }
+    
+    int solve(int now, int dp[], vector<int>& nums, int target)
+    {
+        int n=nums.size();
+        if(now>=n || target<0) return 0;
+        if(dp[target]!=-1) return dp[target];
+        if(target==0) return 1;
+        
+        int sum=0;
+        for(int i=now; i<n; ++i)
+        {
+            sum+=solve(0,dp,nums,target-nums[i]);
+        }
+        
+        dp[target]=sum;
+        return sum;
+    }
+};
