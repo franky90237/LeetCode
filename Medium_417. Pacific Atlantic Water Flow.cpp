5a8bd3a70ceec9c7649271ad2aabacdb6a33e5e5
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-13 06:28:14 +0800

    Update Medium_417. Pacific Atlantic Water Flow.cpp

--- Medium_417. Pacific Atlantic Water Flow.cpp
+++ Medium_417. Pacific Atlantic Water Flow.cpp
@@ -79,3 +79,102 @@ public:
         return side;
     }
 };
+
+//2022-06-13
+//bfs two pass
+class Solution 
+{   
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights)
+    {
+        int m=heights.size();
+        int n=heights[0].size();
+        
+        vector<vector<int>> pacific(m,vector<int>(n,0));
+        vector<vector<int>> atlantic(m,vector<int>(n,0));
+        
+        queue<vector<int>> pQ,aQ;
+        
+        for(int i=0; i<m; ++i)            
+        {
+            pacific[i][0]=1;
+            atlantic[i][n-1]=1;
+            
+            pQ.push({i,0});
+            aQ.push({i,n-1});
+        }
+                                     
+        for(int i=0; i<n; ++i)       
+        {
+            pacific[0][i]=1;
+            atlantic[m-1][i]=1;
+            
+            pQ.push({0,i});
+            aQ.push({m-1,i});
+        }
+        
+        bfs(pQ,heights,pacific);
+        bfs(aQ,heights,atlantic);
+        
+        //print_v(pacific);
+        //print_v(atlantic);
+        
+        vector<vector<int>> ans;
+        for(int i=0; i<m; ++i)
+        {
+            for(int j=0; j<n; ++j)
+            {
+                if(pacific[i][j] && atlantic[i][j]) ans.push_back({i,j});
+            }
+        }
+        
+        return ans;
+    }
+    
+    void bfs(queue<vector<int>>& q, vector<vector<int>>& heights, vector<vector<int>>& visited)
+    {
+        int m=heights.size();
+        int n=heights[0].size();
+        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+        while(!q.empty())
+        {
+            int r=q.front()[0];
+            int c=q.front()[1];
+            q.pop();
+            
+            
+            for(int i=0; i<4; ++i)
+            {
+                int r_n=r+dir[i][0];
+                int c_n=c+dir[i][1];
+                
+                if(r_n<0 || r_n>=m || c_n<0 || c_n>=n || visited[r_n][c_n])
+                {
+                    continue;
+                }
+                
+                if(heights[r_n][c_n]>=heights[r][c])
+                {
+                    visited[r_n][c_n]=true;
+                    q.push(vector<int>({r_n,c_n}));
+                }
+            }
+        }
+    }
+    
+    void print_v(vector<vector<int>>& v)
+    {
+        int m=v.size();
+        int n=v[0].size();
+        
+        cout<<m<<":"<<n<<endl;
+        for(int i=0; i<m; ++i)
+        {
+            for(int j=0; j<n; ++j)
+            {
+                cout<<v[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
