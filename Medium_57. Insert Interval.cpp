ca449391398e8fa8d8d9418c292b558fddf0f8e9
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-13 18:05:28 +0800

    Create Medium_57. Insert Interval.cpp

---
+++ Medium_57. Insert Interval.cpp
@@ -0,0 +1,67 @@
+//2022-06-13
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval)
+    {
+        int n=intervals.size();
+        
+        if(n==0) return {{newInterval[0],newInterval[1]}};
+        if(newInterval[0]>intervals.back()[1])
+        {
+            intervals.push_back({newInterval[0],newInterval[1]});
+            return intervals;
+        }
+        
+        vector<vector<int>> ans;
+        int i;
+        for(i=0; i<n; ++i)
+        {
+            if(newInterval[1]<intervals[i][0])
+            {
+                ans.push_back({newInterval[0],newInterval[1]});                
+                break;
+            }
+            
+            //cout<<i<<endl;
+            if(Overlapping(intervals[i],newInterval))
+            {                
+                do
+                {                    
+                    newInterval[0]=min(intervals[i][0],newInterval[0]);
+                    newInterval[1]=max(intervals[i][1],newInterval[1]);
+                    
+                    //cout<<i<<": "<<newInterval[0]<<" "<<newInterval[1]<<endl;
+                    ++i;                    
+                } while(i<n && Overlapping(intervals[i],newInterval));
+                
+                ans.push_back({newInterval[0],newInterval[1]});
+                break;
+            }
+            else
+            {
+                ans.push_back({intervals[i][0],intervals[i][1]});                
+            }
+        }        
+        
+        while(i<n)
+        {
+            ans.push_back({intervals[i][0],intervals[i][1]});
+            ++i;
+        }
+        
+        return ans;
+    }
+    
+    bool Overlapping(vector<int>& curInterval, vector<int>& newInterval)
+    {        
+        if( (newInterval[0]<=curInterval[0] && newInterval[1]>=curInterval[0]) ||
+            (newInterval[0]>=curInterval[0] && newInterval[0]<=curInterval[1]) )
+        {            
+            return true;
+        }
+        
+        return false;
+    }
+};
