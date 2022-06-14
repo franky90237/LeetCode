a204875a57f53c1a4a78dfd30caa40a5650864ba
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-14 15:38:14 +0800

    Update Medium_120. Triangle.cpp

--- Medium_120. Triangle.cpp
+++ Medium_120. Triangle.cpp
@@ -21,3 +21,35 @@ public:
         return min(sum1,sum2);
     }
 };
+
+//2022-06-14
+//dp recursive
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle)
+    {
+        int m=triangle.size();
+        int n=triangle[m-1].size();
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(0,0,triangle,dp);
+    }
+    
+    int solve(int row, int column, vector<vector<int>>& triangle, vector<vector<int>>& dp)
+    {
+        int m=triangle.size();
+        if(row==m) return 0;
+        
+        int n=triangle[row].size();
+        if(column==n) return 0;
+        
+        //cout<<row<<" "<<column<<endl;
+        if(dp[row][column]!=-1) return dp[row][column];
+        
+        int sum1=triangle[row][column]+solve(row+1,column,triangle,dp);
+        int sum2=triangle[row][column]+solve(row+1,column+1,triangle,dp);
+        
+        dp[row][column]=min(sum1,sum2);
+        return dp[row][column];
+    }
+};
