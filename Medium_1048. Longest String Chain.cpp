783f0947103929337ae69a412be1533661944394
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-16 17:15:20 +0800

    Update Medium_1048. Longest String Chain.cpp

--- Medium_1048. Longest String Chain.cpp
+++ Medium_1048. Longest String Chain.cpp
@@ -53,3 +53,63 @@ public:
         return false;
     }
 };
+
+//2022-06-16
+//dp recursive
+class Solution {
+public:
+    int longestStrChain(vector<string>& words)
+    {
+        int n=words.size();
+        
+        int dp[n];
+        for(int i=0; i<n; ++i) dp[i]=-1;
+        
+        int len=1;
+        for(int i=0; i<n; ++i)
+        {
+            len=max(len,solve(i,words,dp));
+            //cout<<i<<" "<<len<<endl;
+        }
+        
+        return len;
+    }
+    
+    int solve(int pre, vector<string>& words, int dp[])
+    {
+        int n=words.size();        
+        if(dp[pre]!=-1) return dp[pre];
+        
+        int len=1;
+        for(int i=0; i<n; ++i)
+        {
+            if(pre!=i && check_predecessor(pre,i,words))
+            {
+                len=max(len,1+solve(i,words,dp));
+            }
+        }
+        
+        dp[pre]=len;
+        return len;
+    }
+    
+    bool check_predecessor(int pre, int now, vector<string>& words)
+    {
+        if(words[now].size()!=words[pre].size()+1) return false;
+        
+        for(int exclude=0; exclude<words[now].size(); ++exclude)
+        {
+            int j=0;
+            for(int i=0; i<words[now].size(); ++i)
+            {
+                if(i==exclude) continue;
+                if(words[now][i]!=words[pre][j]) break;
+                ++j;
+            }
+            
+            if(j==words[pre].size()) return true;
+        }
+        
+        return false;
+    }
+};
