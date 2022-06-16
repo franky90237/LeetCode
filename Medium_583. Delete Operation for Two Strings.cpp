20dc48abe2a34f022f6b98422f19ed199387c3ae
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-16 08:18:59 +0800

    Update Medium_583. Delete Operation for Two Strings.cpp

--- Medium_583. Delete Operation for Two Strings.cpp
+++ Medium_583. Delete Operation for Two Strings.cpp
@@ -71,3 +71,31 @@ public:
         return len;
     }
 };
+
+//2022-06-16
+//dp iterative
+//time  : O(m*n)
+//space : O(m*n)
+class Solution {
+public:
+    int minDistance(string word1, string word2)
+    {
+        int m=word1.size();
+        int n=word2.size();
+        
+        int dp[m+1][n+1];
+        for(int r=0; r<=m; ++r) dp[r][0]=0;
+        for(int c=0; c<=n; ++c) dp[0][c]=0;                
+                
+        for(int i=1; i<=m; ++i)
+        {
+            for(int j=1; j<=n; ++j)
+            {
+                if(word1[i-1]==word2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }        
+        
+        return m-dp[m][n] + n-dp[m][n];
+    }    
+};
