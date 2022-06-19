b04b73f182c4252fa38ce4ee9f8c9472bf6f24bc
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-20 07:30:13 +0800

    Update Medium_435. Non-overlapping Intervals.cpp

--- Medium_435. Non-overlapping Intervals.cpp
+++ Medium_435. Non-overlapping Intervals.cpp
@@ -44,3 +44,58 @@ public:
         return (intervals[now][0]<intervals[pre][1]);
     }
 };
+
+//2022-06-20
+//MLE
+//dp recursive
+class Solution 
+{
+private:
+    struct Mycomp
+    {
+        bool operator()(vector<int>& a, vector<int>& b)
+        {
+            if(a[0]<b[0]) return true;
+            else if(a[0]==b[0]) return a[1]<b[1];
+            else return false;            
+        }
+    } comp;
+    
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) 
+    {
+        sort(intervals.begin(),intervals.end(),comp);
+        
+        int n=intervals.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(0,0,intervals,dp);
+    }
+    
+    int solve(int pre, int now, vector<vector<int>>& intervals, vector<vector<int>>& dp)
+    {
+        int n=intervals.size();
+        if(now>=n) return 0;
+        if(pre!=now && dp[pre][now]!=-1) return dp[pre][now];
+        
+        int res;
+        if(pre!=now && Overlapping(pre,now,intervals))
+        {
+            int sum1=1+solve(pre, now+1, intervals,dp);
+            int sum2=1+solve(now, now+1, intervals,dp);
+            
+            res=min(sum1,sum2);
+        }
+        else
+        {
+            res=solve(now, now+1, intervals,dp);
+        }
+        
+        dp[pre][now]=res;
+        return res;
+    }
+    
+    bool Overlapping(int pre, int now, vector<vector<int>>& intervals)
+    {
+        return (intervals[now][0]<intervals[pre][1]);
+    }
+};
