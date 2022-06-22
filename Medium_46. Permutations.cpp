69f6fe33674ff6fed17bd6df49e7055d104994f9
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-06-22 16:54:34 +0800

    Update Medium_46. Permutations.cpp

--- Medium_46. Permutations.cpp
+++ Medium_46. Permutations.cpp
@@ -68,3 +68,52 @@ public:
         }
     }
 };
+
+//2022-06-22
+//iterative
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        list<list<int>> ans;
+        ans.push_back(list<int>(1,nums[0]));
+        
+        for(int i=1; i<nums.size(); ++i)
+        {
+            //cout<<i<<" : "<<endl;
+            int cnt=ans.size();
+            for(int j=0; j<cnt; ++j)
+            {
+                //cout<<j<<endl;
+                list<int> tmp=ans.front();
+                ans.pop_front();
+                
+                for(auto it=tmp.begin(); it!=tmp.end(); ++it)
+                {                    
+                    tmp.insert(it,nums[i]);
+                    //for(auto& x:tmp) cout<<x<<" ";
+                    //cout<<endl;
+                    ans.push_back(tmp);
+                    //auto removed=--it;
+                    //++it;
+                    --it;
+                    it=tmp.erase(it);
+                }
+                
+                tmp.push_back(nums[i]);
+                //for(auto& x:tmp) cout<<x<<" ";
+                //cout<<endl;
+                ans.push_back(tmp);
+            }
+        }
+        
+        vector<vector<int>> final_ans;
+        while(!ans.empty())
+        {
+            final_ans.push_back(vector<int>(ans.back().begin(),ans.back().end()));
+            ans.pop_back();
+        }
+        
+        return final_ans;
+    }    
+};
