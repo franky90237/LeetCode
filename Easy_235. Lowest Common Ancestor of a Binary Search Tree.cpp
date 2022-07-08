58030a9bf18625d05fccb2254eae0dcb9a56f789
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-08 16:42:42 +0800

    Update Easy_235. Lowest Common Ancestor of a Binary Search Tree.cpp

--- Easy_235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Easy_235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -18,3 +18,42 @@ public:
         return root;
     }
 };
+
+//2022-07-08
+//time  : O(n*n)
+//space : O(height of the tree)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        TreeNode* ans=root;
+        
+        queue<TreeNode*> que;        
+        que.push(root);
+        while(!que.empty())
+        {
+            TreeNode* tmp=que.front();
+            que.pop();
+            
+            if(solve(tmp,p,q)==2) ans=tmp;
+            
+            if(tmp->left) que.push(tmp->left);
+            if(tmp->right) que.push(tmp->right);
+        }
+        
+        
+        return ans;
+    }
+    
+    int solve(TreeNode* root, TreeNode* p, TreeNode* q)
+    {        
+        if(!root) return 0;
+        
+        int left=solve(root->left,p,q);
+        if(root==p || root==q) ++left;
+        if(left==2) return 2;
+        
+        int right=solve(root->right,p,q);        
+        return left+right;
+    }
+};
