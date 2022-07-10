aed74862f0ed2492589b111489a06d52250bf9eb
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-11 07:59:12 +0800

    Update Medium_105. Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Medium_105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Medium_105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -37,3 +37,34 @@ public:
         return node;
     }
 };
+
+//2022-07-11
+//use hash map to accelerate the look up time in the inorder
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        unordered_map<int,int> val_index;
+        for(int i=0; i<inorder.size(); ++i) val_index[inorder[i]]=i;
+        
+        return build(preorder,0,inorder,0,inorder.size()-1,val_index);
+    }
+    
+    TreeNode* build(vector<int>& preorder, int cur, vector<int>& inorder, int l, int r, unordered_map<int,int>& val_index)
+    {
+        if(l>r) return NULL;
+        //cout<<cur<<" : "<<l<<" "<<r<<endl;
+        
+        TreeNode* node=new TreeNode(preorder[cur]);
+        if(l==r) return node;
+        
+        int mid=val_index[preorder[cur]];        
+        
+        node->left=build(preorder,cur+1,inorder,l,mid-1,val_index);
+        node->right=build(preorder,cur+mid-l+1,inorder,mid+1,r,val_index);                
+        
+        return node;
+    }
+};
