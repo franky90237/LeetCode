b4ae15f7d38a54e8b71f50dfb4be2325fe326a1b
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-12 19:31:36 +0800

    Create Hard_218. The Skyline Problem.cpp

---
+++ Hard_218. The Skyline Problem.cpp
@@ -0,0 +1,103 @@
+//2022-07-12
+//use max_heap and the line sweep concepts
+//time  : O(nlog(n))
+//space : O(n)
+class Solution 
+{
+private:
+    enum TYPE {start,end};
+    
+    class Entry
+    {
+    public:
+        int x;
+        int y;
+        int type;
+                
+        Entry(int x, int y, int type):x(x), y(y), type(type) {}
+    };
+    
+    struct line_comp
+    {
+    public:
+        bool operator()(const Entry& a, const Entry& b)
+        {
+            if(a.x==b.x)
+            {
+                if(a.type==0 && b.type==0) return a.y>b.y;
+                else if(a.type==1 && b.type==1) return a.y<b.y;
+                else return (a.type==0 && b.type==1);               
+            }
+            
+            return a.x<b.x;
+        }
+    } line_comp;
+    
+    class max_heap_comp
+    {
+    public:
+        bool operator()(const pair<int,int>& a, const pair<int,int>& b)
+        {            
+            return a.first<b.first;
+        }
+    };
+    
+    
+    void max_heap_remove_by_y(int y, priority_queue<pair<int,int>,vector<pair<int,int>>,max_heap_comp>& max_heap, unordered_map<int,int>& support)
+    {
+        if(max_heap.top().first==y)
+        {
+            //cout<<y<<" : "<<support.size()<<endl;
+            max_heap.pop();
+            while(support.find(max_heap.top().first)!=support.end())
+            {
+                //cout<<max_heap.top().first<<" ,, "<<max_heap.size()<<endl;
+                if(support[max_heap.top().first]==1) support.erase(max_heap.top().first);
+                else --support[max_heap.top().first];
+                max_heap.pop();
+            }
+        }
+        else
+        {
+            ++support[y];
+        }
+    }
+    
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings)
+    {
+        vector<Entry> line;
+        for(auto building:buildings)
+        {
+            line.push_back(Entry(building[0],building[2],0));
+            line.push_back(Entry(building[1],building[2],1));
+        }
+        
+        //line_comp comp;
+        sort(line.begin(),line.end(),line_comp);
+                
+        priority_queue<pair<int,int>,vector<pair<int,int>>,max_heap_comp> max_heap;
+        max_heap.push(pair<int,int>(0,0));
+        unordered_map<int,int> support;
+        
+        vector<vector<int>> ans;
+        for(auto& i:line)
+        {
+            //cout<<i.x<<" "<<i.y<<" "<<i.type<<endl;
+            
+            if(i.type==0)
+            {
+                if(max_heap.empty() || i.y>max_heap.top().first) ans.push_back({i.x,i.y});
+                max_heap.push(pair<int,int>(i.y,i.type));
+                //++max_heap_item[i.y];
+            }
+            else
+            {
+                max_heap_remove_by_y(i.y,max_heap,support);
+                if(i.y>max_heap.top().first) ans.push_back({i.x,max_heap.top().first});
+            }
+        }
+        
+        return ans;
+    }
+};
