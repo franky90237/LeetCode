420c63685d401e23b37eafb3274433e9e0047a3b
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-20 17:28:59 +0800

    Update Easy_141. Linked List Cycle.cpp

--- Easy_141. Linked List Cycle.cpp
+++ Easy_141. Linked List Cycle.cpp
@@ -29,15 +29,6 @@ public:
     }
 };
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-
 //2022-07-20
 //check every move
 //time  : O(n)
@@ -57,6 +48,28 @@ public:
             fast=fast->next;
             slow=slow->next;
             if(slow==fast) return true;
+        }
+        
+        return false;
+    }
+};
+
+//2022-07-20
+//change value of node
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) 
+    {
+        ListNode* cur=head;
+        
+        while(cur)
+        {
+            if(cur->val==INT_MAX) return true;
+            
+            cur->val=INT_MAX;
+            cur=cur->next;
         }
         
         return false;
