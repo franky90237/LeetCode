723b2a8ec4744450c3fbf18ce2b0459563cd2e4c
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-21 19:58:28 +0800

    Update Medium_122. Best Time to Buy and Sell Stock II.cpp

--- Medium_122. Best Time to Buy and Sell Stock II.cpp
+++ Medium_122. Best Time to Buy and Sell Stock II.cpp
@@ -78,3 +78,28 @@ public:
         return s0[n-1];
     }
 };
+
+//2022-07-21
+//greedy
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int profit=0;
+        int buy=prices[0];
+        
+        for(auto& price:prices)
+        {
+            if(price>buy)
+            {
+                profit+=price-buy;                
+            }
+            
+            buy=price;
+        }
+        
+        return profit;
+    }
+};
