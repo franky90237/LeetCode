bca7f0d03375fb1ccf7a58ff8e800da902f789df
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-27 21:40:36 +0800

    Create Easy_112. Path Sum.cpp

---
+++ Easy_112. Path Sum.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//2022-07-27
+//dfs
+//time  : O(n)
+//space : O(height of the tree)
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum)
+    {
+        if(!root) return false;        
+        return dfs(root,targetSum);
+    }
+    
+    bool dfs(TreeNode* root, int targetSum)
+    {        
+        if(!root) return false;
+        if(!root->left && !root->right) return (targetSum-root->val)==0;
+        
+        //cout<<root->val<<" "<<targetSum<<endl;
+        targetSum-=root->val;
+        bool left  = dfs(root->left,targetSum);
+        bool right = dfs(root->right,targetSum);
+                
+        return (left | right);        
+    }
+};
