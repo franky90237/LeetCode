6dd5e72debbcbe480a7a2cc5ce4ed8bfc86932ee
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-28 09:44:53 +0800

    Update and rename Medium_622. Design Circular Queue.c to Medium_622. Design Circular Queue.cpp

--- Medium_622. Design Circular Queue.cpp
+++ Medium_622. Design Circular Queue.cpp
@@ -75,13 +75,63 @@ public:
     }
 };
 
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
+//2022-07-28
+class MyCircularQueue
+{
+private:
+    vector<int> queue;
+    int cnt;
+    int front;
+    int rear;
+    
+public:
+    MyCircularQueue(int k) 
+    {
+        queue.resize(k);
+        cnt=0;
+        front=0;
+        rear=0;
+    }
+    
+    bool enQueue(int value) 
+    {
+        if(isFull()) return false;
+                
+        queue[rear]=value;
+        rear=(rear+1)%queue.size();
+        ++cnt;
+        return true;
+    }
+    
+    bool deQueue()
+    {
+        if(isEmpty()) return false;
+        
+        front=(front+1)%queue.size();
+        --cnt;
+        return true;
+    }
+    
+    int Front()
+    {
+        if(isEmpty()) return -1;
+        return queue[front];
+    }
+    
+    int Rear()
+    {
+        if(isEmpty()) return -1;
+        int pre=(rear-1 < 0) ? queue.size()-1 : rear-1;
+        return queue[pre];
+    }
+    
+    bool isEmpty()
+    {
+        return cnt==0;
+    }
+    
+    bool isFull()
+    {
+        return cnt==queue.size();
+    }
+};
