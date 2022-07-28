b97979351b3945d068f53ac16ad3661dc307847c
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-28 20:28:56 +0800

    Update Medium_113. Path Sum II.cpp

--- Medium_113. Path Sum II.cpp
+++ Medium_113. Path Sum II.cpp
@@ -44,3 +44,49 @@ public:
         num.pop_back();
     }
 };
+
+//2022-07-28
+//bfs
+//time  : O(n)
+//space : O(3*n)
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum)
+    {
+        if(!root) return {};
+        
+        vector<vector<int>> ans;        
+        
+        queue<tuple<TreeNode*, int, vector<int>>> q;
+        q.push({root,targetSum-root->val,{root->val}});
+        
+        while(!q.empty())
+        {
+            tuple<TreeNode*, int, vector<int>> tmp=q.front();
+            q.pop();
+            
+            TreeNode* n=get<0>(tmp);
+            int diff=get<1>(tmp);
+            vector<int>& num=get<2>(tmp);                        
+            
+            if(!n->left && !n->right)
+            {
+                if(diff==0) ans.push_back(num);
+            }
+            
+            if(n->left)
+            {
+                num.push_back(n->left->val);
+                q.push({n->left,diff-n->left->val,num});
+                num.pop_back();
+            }
+            if(n->right)
+            {
+                num.push_back(n->right->val);
+                q.push({n->right,diff-n->right->val,num});
+            }
+        }
+        
+        return ans;
+    }
+};
