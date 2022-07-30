eb829ecadc849d5b6e9170303a983417299b9354
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-30 11:43:57 +0800

    Update Medium_199. Binary Tree Right Side View.cpp

--- Medium_199. Binary Tree Right Side View.cpp
+++ Medium_199. Binary Tree Right Side View.cpp
@@ -44,3 +44,32 @@ public:
         return ans;
     }
 };
+
+//2022-07-30
+//dfs
+//time  : O(n)
+//space : O(height of the tree)
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root)
+    {
+        vector<int> ans;
+        int level=0;
+        
+        dfs(root,level,ans);
+        return ans;
+    }
+    
+    void dfs(TreeNode* root,int level,vector<int>& ans)
+    {
+        if(!root) return;
+        if(level==ans.size()) 
+        {
+            //cout<<root->val<<endl;
+            ans.push_back(root->val);
+        }
+        
+        dfs(root->right,level+1,ans);
+        dfs(root->left,level+1,ans);
+    }
+};
