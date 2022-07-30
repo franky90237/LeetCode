17306c00d6e4625f29a5108e825a3be84eb68af0
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-30 19:56:15 +0800

    Update Medium_1423. Maximum Points You Can Obtain from Cards.cpp

--- Medium_1423. Maximum Points You Can Obtain from Cards.cpp
+++ Medium_1423. Maximum Points You Can Obtain from Cards.cpp
@@ -23,3 +23,33 @@ public:
         return max(pick_left,pick_right);
     }
 };
+
+//2022-07-30
+//MLE
+//time  : O(k*n)
+//space : O(k)
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k)
+    {
+        /*
+        maxPoints = max(cardPoints[0]+cardPoints[1:n],cardPoints[n]+cardPoints[0:n-1])
+        */
+        int n=cardPoints.size();
+        vector<vector<vector<int>>> dp(k+1,vector<vector<int>>(n,vector<int>(n,-1)));
+        
+        return solve(dp,cardPoints,0,n-1,k);
+    }
+    
+    int solve(vector<vector<vector<int>>>& dp, vector<int>& cardPoints, int l, int r, int k)
+    {
+        if(k==0) return 0;
+        if(dp[k][l][r]!=-1) return dp[k][l][r];
+        
+        int pick_left = cardPoints[l] + solve(dp,cardPoints,l+1,r,k-1);
+        int pick_right = cardPoints[r] + solve(dp,cardPoints,l,r-1,k-1);
+        
+        dp[k][l][r]=max(pick_left,pick_right);
+        return dp[k][l][r];
+    }
+};
