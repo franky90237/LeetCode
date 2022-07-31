2325d563a6347ae5111b7dc9f88e37ecab977f75
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-07-31 19:08:20 +0800

    Update Medium_662. Maximum Width of Binary Tree.cpp

--- Medium_662. Maximum Width of Binary Tree.cpp
+++ Medium_662. Maximum Width of Binary Tree.cpp
@@ -120,3 +120,51 @@ public:
         return ans;
     }    
 };
+
+//2022-07-31
+//bfs, optimize to prevent overflow
+//time  : O(n)
+//space : O(height of the tree)
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) 
+    {
+        /*
+        [1] [3,2] [5,n,n,n]
+        */
+        
+        queue<pair<TreeNode*,unsigned int>> q;        
+        q.push({root,0});
+        
+        unsigned int ans=1;
+        while(!q.empty())
+        {
+            unsigned int min_val=q.front().second;
+            unsigned int start=q.front().second-min_val;
+            unsigned int end=start;
+            
+            int n=q.size();
+            for(int i=0; i<n; ++i)
+            {
+                TreeNode* tmp=q.front().first;
+                unsigned int parent_val=q.front().second-min_val;
+                q.pop();
+                end=parent_val;
+                
+                if(tmp->left)
+                {
+                    q.push({tmp->left,parent_val*2});
+                }
+                if(tmp->right)
+                {                    
+                    q.push({tmp->right,parent_val*2+1});
+                }
+            }
+            
+            //cout<<end-start+1<<endl;
+            ans=max(ans,end-start+1);
+        }
+        
+        return ans;
+    }    
+};
