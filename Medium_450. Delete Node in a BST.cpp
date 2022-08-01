34c4b70f7e6d514e388ce4fc2a4103ecdeb43ea6
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-01 17:41:49 +0800

    Update Medium_450. Delete Node in a BST.cpp

--- Medium_450. Delete Node in a BST.cpp
+++ Medium_450. Delete Node in a BST.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) 
@@ -176,5 +177,117 @@ public:
         
         delete target;
         return root;
+    }
+};
+
+//2022-08-01
+//time  : O(log(n))
+//space : O(height of the tree)
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key)
+    {
+        if(!root) return NULL;
+        if(!root->left && !root->right)
+        {
+            if(root->val!=key) return root;
+            else
+            {
+                delete root;
+                return NULL;
+            }
+        }
+        
+        if(root->val==key)
+        {
+            TreeNode* pre_suc=NULL;
+            TreeNode* suc=root;
+            find_successor(root,pre_suc,suc);
+            
+            if(pre_suc!=root)
+            {
+                pre_suc->left=NULL;
+                pre_suc->left=suc->right;
+            }
+            
+            //cout<<pre_suc->val<<" "<<suc->val<<endl;
+            if(suc)
+            {
+                suc->left=root->left;
+                if(pre_suc->right!=suc) suc->right=root->right;
+            }
+            else
+            {
+                suc=root->left;                
+            }
+            
+            delete root;
+            return suc;
+        }
+        
+        TreeNode* pre=find_key(root,NULL,key);
+        if(!pre) return root;
+        TreeNode* target= (pre->val < key) ? pre->right : pre->left;            
+        //cout<<((pre) ? pre->val : -99)<<endl;
+        
+        if(!target->left && !target->right)
+        {
+            if(pre->val < key) pre->right=NULL;
+            else pre->left=NULL;            
+        }
+        else if(!target->left || !target->right)
+        {
+            TreeNode* new_child;
+            if(target->left) new_child=target->left;
+            else new_child=target->right;
+            
+            if(pre->val < key) pre->right=new_child;
+            else pre->left=new_child;
+        }
+        else
+        {
+            TreeNode* pre_suc=NULL;
+            TreeNode* suc=target;
+            find_successor(target,pre_suc,suc);
+            
+            if(pre_suc!=target)
+            {
+                pre_suc->left=NULL;
+                pre_suc->left=suc->right;
+            }
+            
+            if(pre->val < key) pre->right=suc;
+            else pre->left=suc;
+                        
+            
+            suc->left=target->left;
+            if(pre_suc->right!=suc) suc->right=target->right;
+        }
+        
+        delete target;        
+        return root;
+    }    
+    
+    TreeNode* find_key(TreeNode* root, TreeNode* pre, int key)
+    {
+        //cout<<root->val<<endl;
+        if(!root) return NULL;
+        if(root->val==key) return pre;        
+        else if(root->val < key) return find_key(root->right,root,key);        
+        else return find_key(root->left,root,key);
+    }
+    
+    TreeNode* find_successor(TreeNode* root, TreeNode*& pre, TreeNode*& suc)
+    {
+        pre=suc;
+        suc=root->right;
+        
+        while(suc && suc->left) 
+        {
+            pre=suc;
+            suc=suc->left;
+        }
+        
+        return suc;
     }
 };
