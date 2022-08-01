766f719a90135cc2b52329b47edb1f01dff6d080
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-01 20:42:54 +0800

    Update Easy_14. Longest Common Prefix.cpp

--- Easy_14. Longest Common Prefix.cpp
+++ Easy_14. Longest Common Prefix.cpp
@@ -28,3 +28,66 @@ public:
         return ans;
     }
 };
+
+//2022-08-01
+//trie
+//time  : O(n*m), m is the size of max str
+//spce  : O(n*m)
+struct TrieNode
+{
+    bool is_word;
+    unordered_map<char,TrieNode*> next;
+};
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs)
+    {
+        TrieNode* head=bulid_trie(strs);
+        if(!head) return "";
+        /*for(auto it=head; it->next.size()!=0; )
+        {
+            cout<<(it->next).begin()->first<<endl;
+            it=(it->next).begin()->second;
+        }*/
+        
+        string ans="";
+        
+        TrieNode* cur=head;
+        //cout<<cur->next.size()<<endl;
+        while(!cur->next.empty() && cur->next.size()==1 && !cur->is_word)
+        {
+            ans+=cur->next.begin()->first;
+            cur=cur->next.begin()->second;            
+        }
+        
+        return ans;
+    }
+    
+    TrieNode* bulid_trie(vector<string>& strs)
+    {
+        TrieNode* head=new TrieNode;
+        head->is_word=false;
+        
+        for(auto& str:strs)
+        {
+            if(str.empty()) return NULL;
+            
+            TrieNode* cur=head;
+            for(auto& c:str)
+            {
+                if(cur->next.find(c) == cur->next.end())
+                {
+                    cur->next[c]=new TrieNode;
+                    cur->next[c]->is_word=false;
+                }
+                
+                cur=cur->next[c];
+            }
+            
+            cur->is_word=true;
+        }
+        
+        return head;
+    }
+};
