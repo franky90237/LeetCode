f4a041d22a2bd1bb1ae7a3b7a2c9a2c3e359ce62
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-02 19:57:56 +0800

    Update Medium_146. LRU Cache.cpp

--- Medium_146. LRU Cache.cpp
+++ Medium_146. LRU Cache.cpp
@@ -142,10 +142,3 @@ public:
         cout<<endl;        
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
