6c70c598a6d5b5b8c553f4fc53c30a5658b0f68e
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-02 17:10:25 +0800

    Update Medium_11. Container With Most Water.cpp

--- Medium_11. Container With Most Water.cpp
+++ Medium_11. Container With Most Water.cpp
@@ -55,3 +55,56 @@ public:
         return res;
     }
 };
+
+//2022-08-02
+//time  : O(n*n)
+//space : O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height)
+    {
+        int n=height.size();
+        
+        int max_area=0;
+        for(int i=0; i<n; ++i)
+        {            
+            cal_max_area(height,0,i,max_area,true);
+            cal_max_area(height,i,n-1,max_area,false);
+            //cout<<i<<" "<<max_area<<endl;
+        }
+        
+        return max_area;
+    }
+    
+    void cal_max_area(vector<int>& height, int left, int right, int& max_area, bool left_to_right)
+    {
+        if(left_to_right)
+        {
+            for(int i=left; i<right; ++i)
+            {
+                int area=height[right]*(right-i);                
+                if(area <= max_area) return;
+                
+                if(height[i]>=height[right] && area > max_area) 
+                {
+                    max_area=area;
+                    return;
+                }
+            }
+        }
+        else
+        {
+            for(int i=right; i>left; --i)
+            {
+                int area=height[left]*(i-left);
+                if(area <= max_area) return;
+                
+                if(height[i]>=height[left] && area > max_area) 
+                {
+                    max_area=area;
+                    return;
+                }
+            }
+        }
+    }
+};
