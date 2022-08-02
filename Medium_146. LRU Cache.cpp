84b07cd638286d0242cb88c0eaf273e48caa5d4e
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-02 19:57:37 +0800

    Update Medium_146. LRU Cache.cpp

--- Medium_146. LRU Cache.cpp
+++ Medium_146. LRU Cache.cpp
@@ -70,3 +70,82 @@ public:
         cout<<endl;        
     }
 };
+
+//2022-08-02
+class LRUCache
+{
+private:
+    int max_size;
+    list<pair<int,int>> cache;
+    unordered_map<int,list<pair<int,int>>::iterator> record;    
+    
+public:
+    LRUCache(int capacity) 
+    {
+        /*
+        ["LRUCache","get","put","get","put","put","get","get"]
+        [[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
+        
+        (2,6)
+        (1,5) (2,6)
+        (1,2) (2,6)
+        
+        */
+        max_size=capacity;
+    }
+    
+    int get(int key)
+    {
+        //cout<<"get : "<<key<<endl;
+        if(record.find(key)!=record.end())
+        {
+            list<pair<int,int>>::iterator it=record[key];
+            int value=(*it).second;
+            cache.erase(it);
+            
+            cache.push_front({key,value});
+            record[key]=cache.begin();
+            
+            //pirnt_list();
+            return value;
+        }
+        
+        return -1;
+    }
+    
+    void put(int key, int value)
+    {
+        if(cache.size()==max_size && record.find(key)==record.end())
+        {
+            int least_key=cache.back().second;
+            
+            cache.pop_back();
+            record.erase(least_key);        
+        }
+        
+        if(record.find(key)!=record.end())
+        {
+            list<pair<int,int>>::iterator it=record[key];
+            cache.erase(it);
+            record.erase(key);
+        }
+        
+        cache.push_front({key,value});
+        record[key]=cache.begin();
+        
+        //pirnt_list();
+    }
+    
+    void pirnt_list()
+    {
+        for(auto& it:cache) cout<<"("<<it.first<<","<<it.second<<") ";
+        cout<<endl;        
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
