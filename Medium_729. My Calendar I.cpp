fbde33d3f81cc3c81af74ce2a1f06ad988cfb7fb
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-04 15:01:02 +0800

    Update Medium_729. My Calendar I.cpp

--- Medium_729. My Calendar I.cpp
+++ Medium_729. My Calendar I.cpp
@@ -188,3 +188,71 @@ public:
         return false;
     }
 };
+
+//2022-08-04
+//clear condition + lowe bound
+//time  : O(n)
+class MyCalendar
+{
+private:
+    set<pair<int,int>> events;
+    
+    bool overlap(int start, int end)
+    {
+        if(events.empty()) return false;
+        
+        int& new_start=start;
+        int& new_end=end;
+        
+        set<pair<int,int>>::iterator nxt=events.lower_bound({start,end});
+                
+        //for(auto& i :events) cout<<"("<<i.first<<","<<i.second<<") ";
+        //cout<<endl;
+        //cout<<events.size()<<" : "<<start<<","<<end<<"  |　";
+        //cout<<nxt->first<<","<<nxt->second<<endl;
+        
+        //check if nxt is really the next event
+        if(nxt!=events.end() && new_end > nxt->first) return true;
+        
+        //check if pre exist and overlap        
+        if(nxt!=events.begin())
+        {
+            set<pair<int,int>>::iterator pre=--nxt;
+            if(new_start < pre->second) return true;
+        }
+        
+        return false;
+    }
+    
+public:
+    MyCalendar() 
+    {
+        /*
+        |------|   |--------|
+            |----------|
+                    
+        overlpa : (new_start < pre_end) || (new_end > nxt_start)
+        
+        (47,50)
+        (33,41)
+        (25,32)
+        (19,25)
+        (3,8)
+        (8,13)
+        
+        cur_s=10, cur_e=20
+        new_s=5, new_s=8
+        */        
+    }
+    
+    bool book(int start, int end) 
+    {
+        if(!overlap(start,end)) 
+        {
+            events.insert({start,end});
+            return true;
+        }
+        
+        return false;
+    }
+};
