fd37c17eede0d834043117bf37ca264dc15cabc0
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-04 08:52:58 +0800

    Update Medium_729. My Calendar I.cpp

--- Medium_729. My Calendar I.cpp
+++ Medium_729. My Calendar I.cpp
@@ -121,3 +121,70 @@ public:
         return false;
     }
 };
+
+//2022-08-04
+//clear condition + lowe bound
+//time  : O(n)
+class MyCalendar 
+{
+private:
+    set<pair<int,int>> events;
+    
+    bool overlap(int start, int end)
+    {
+        if(events.empty()) return false;
+        
+        int& new_start=start;
+        int& new_end=end;
+        
+        set<pair<int,int>>::iterator it=events.upper_bound({start,end});
+        //cout<<start<<"  "<<end<<" ---- ";
+        //cout<<it->first<<" "<<it->second<<endl;
+        
+        while(true)
+        {            
+            int cur_start=it->first;
+            int cur_end=it->second;
+                        
+            if((new_start < cur_end) && (new_end > cur_start)) return true;
+            
+            if(it==events.begin()) break;
+            --it;            
+        }
+        
+        return false;
+    }
+    
+public:
+    MyCalendar() 
+    {
+        /*
+        ------     --------     ----------- ----------
+           ----
+                ------
+                --------------
+                
+        not overlap : (new_start >= cur_end) || (new_end <= cur_start)
+        overlpa : (new_start < cur_end) && (new_end > cur_start)
+        
+        (47,50)
+        (33,41)
+        (25,32)
+        (19,25)
+        
+        cur_s=10, cur_e=20
+        new_s=5, new_s=8
+        */
+    }
+    
+    bool book(int start, int end) 
+    {
+        if(!overlap(start,end)) 
+        {
+            events.insert({start,end});
+            return true;
+        }
+        
+        return false;
+    }
+};
