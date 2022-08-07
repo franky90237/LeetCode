fcf8c5f66b31c57a4c43a9a7aa48e264039933d6
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-08 07:52:28 +0800

    Create Medium_416. Partition Equal Subset Sum.cpp

---
+++ Medium_416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,89 @@
+//2022-08-08
+//time  : O(2^n)
+//space : O(n)
+class Solution {
+public:
+    bool canPartition(vector<int>& nums)
+    {
+        int all_sum=accumulate(nums.begin(),nums.end(),0);
+        if(all_sum%2==1) return false;
+        
+        return solve(nums,0,0,all_sum);
+    }
+    
+    bool solve(vector<int>& nums, int cur, int cur_sum, int all_sum)
+    {
+        int n=nums.size();
+        
+        if(cur_sum*2==all_sum) 
+        {
+            //cout<<cur<<" "<<cur_sum<<endl;
+            return true;
+        }
+        if(cur>=n || cur_sum*2>all_sum) return false;
+        /*if(cur_sum*2==all_sum) 
+        {
+            cout<<cur<<" "<<cur_sum<<endl;
+            return true;
+        }*/      
+        
+        for(int i=cur; i<n; ++i)
+        {
+            bool is_find=solve(nums,i+1,cur_sum+nums[i],all_sum);
+            if(is_find) 
+            {
+                //cout<<cur<<" "<<cur_sum<<endl;
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//2022-08-08
+//dp recursive
+//time  : O(n*m), m is the sum of the nums
+//space : O(n*m), m is the sum of the nums
+class Solution 
+{
+private:
+    vector<vector<int>> dp;
+    
+public:
+    bool canPartition(vector<int>& nums)
+    {
+        int all_sum=accumulate(nums.begin(),nums.end(),0);
+        if(all_sum%2==1) return false;
+        
+        dp.resize(nums.size(),vector<int>(all_sum+1,-1));
+        return solve(nums,0,0,all_sum);
+    }
+    
+    bool solve(vector<int>& nums, int cur, int cur_sum, int all_sum)
+    {
+        int n=nums.size();
+        
+        if(cur_sum*2==all_sum) 
+        {
+            //cout<<cur<<" "<<cur_sum<<endl;
+            return true;
+        }
+        if(cur>=n || cur_sum*2>all_sum) return false;
+        if(dp[cur][cur_sum]!=-1) return dp[cur][cur_sum];
+        
+        bool is_find=false;
+        for(int i=cur; i<n; ++i)
+        {
+            is_find=solve(nums,i+1,cur_sum+nums[i],all_sum);
+            if(is_find) 
+            {
+                //cout<<cur<<" "<<cur_sum<<endl;
+                break;
+            }
+        }
+        
+        dp[cur][cur_sum]=is_find;
+        return dp[cur][cur_sum];
+    }
+};
