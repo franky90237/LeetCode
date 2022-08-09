4366029f85549336a874649ee62ab2f1dfadd369
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-09 17:21:12 +0800

    Update Hard_1220. Count Vowels Permutation.cpp

--- Hard_1220. Count Vowels Permutation.cpp
+++ Hard_1220. Count Vowels Permutation.cpp
@@ -51,3 +51,40 @@ public:
         return sum;
     }
 };
+
+//2022-08-09
+//dp + graph
+//time  : O(n)
+//space : O(5*n)
+class Solution {
+public:
+    int countVowelPermutation(int n)
+    {
+        /*
+        a : {e}
+        e : {a,i}
+        i : {a,e,o,u}
+        o : {i,u}
+        u : {a}                
+        */
+        
+        vector<vector<long>> dp(n+1,vector<long>(5,0));
+        for(int i=0; i<5 ;++i) dp[1][i]=1;
+        int modulo = 1e9 + 7;
+        
+        for(int i=2; i<=n; ++i)
+        {
+            //cout<<i<<endl;
+            dp[i][0] = dp[i-1][1] % modulo;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % modulo;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][3] + dp[i-1][4]) % modulo;
+            dp[i][3] = (dp[i-1][2] + dp[i-1][4]) % modulo;
+            dp[i][4] = dp[i-1][0] % modulo;
+        }
+        
+        long sum = 0;
+        for(auto i:dp[n]) sum = (sum+i) % modulo;
+        
+        return sum;
+    }
+};
