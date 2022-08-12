0ec651f1ba2f75cd416542eae17f56a27d1b559f
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-12 18:09:11 +0800

    Update Hard_41. First Missing Positive.cpp

--- Hard_41. First Missing Positive.cpp
+++ Hard_41. First Missing Positive.cpp
@@ -24,3 +24,34 @@ public:
         return nums.back()+1;
     }
 };
+
+//2022-08-12
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums)
+    {        
+        int n=nums.size();
+        unordered_set<int> table;
+        
+        int small=-1;
+        for(auto& num:nums)
+        {
+            if(num>0)
+            {
+                small=num;
+                table.insert(num);
+            }
+        }
+        
+        if(small==-1) return 1;
+        
+        for(int i=1; i<=INT_MAX; ++i)
+        {
+            if(!table.count(i)) return i;
+        }
+        
+        return INT_MAX+1;
+    }
+};
