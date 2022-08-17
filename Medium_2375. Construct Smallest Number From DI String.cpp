9d322550a62a8fac60015f64cd34f599a1d62bbb
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-17 17:21:31 +0800

    Update Medium_2375. Construct Smallest Number From DI String.cpp

--- Medium_2375. Construct Smallest Number From DI String.cpp
+++ Medium_2375. Construct Smallest Number From DI String.cpp
@@ -46,3 +46,36 @@ public:
         return false;
     }
 };
+
+//2022-08-17
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    string smallestNumber(string pattern)
+    {
+        int n=pattern.size();
+        
+        string ans;
+        for(int i=1; i<=n+1; ++i)
+        {
+            ans.push_back(i+'0');
+        }
+        //cout<<ans<<endl;
+        
+        int start=0;
+        for(int i=0; i<n; ++i)
+        {
+            if(pattern[i]=='I')
+            {
+                //cout<<start<<" "<<i<<endl;
+                reverse(ans.begin()+start,ans.begin()+i+1);
+                start=i+1;
+            }
+        }
+        
+        reverse(ans.begin()+start,ans.end());
+                
+        return ans;
+    }      
+};
