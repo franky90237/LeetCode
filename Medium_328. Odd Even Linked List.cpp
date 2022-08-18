4a39e6dde8c3281a6b9c52b336cb3e9fd160c5cb
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-18 17:03:34 +0800

    Update Medium_328. Odd Even Linked List.cpp

--- Medium_328. Odd Even Linked List.cpp
+++ Medium_328. Odd Even Linked List.cpp
@@ -35,3 +35,39 @@ public:
         return head;
     }
 };
+
+//2022-08-18
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head)
+    {
+        /*
+        1->2->3->4->5
+        
+        o : 5
+        e : X
+        1->3->5 2->4->X
+        */
+        
+        if(!head || !head->next) return head;
+        
+        ListNode* odd=head;
+        ListNode* even_head=head->next;
+        ListNode* even=head->next;
+        
+        while(even && even->next)
+        {
+            odd->next=even->next;
+            even->next=even->next->next;
+            
+            odd=odd->next;
+            even=even->next;
+        }
+        
+        odd->next=even_head;        
+        
+        return head;
+    }
+};
