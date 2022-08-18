eb0cef4375f16935d987f8ce7d8c00540da041ba
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-18 18:24:22 +0800

    Update Medim_1338. Reduce Array Size to The Half.cpp

--- Medim_1338. Reduce Array Size to The Half.cpp
+++ Medim_1338. Reduce Array Size to The Half.cpp
@@ -1,5 +1,5 @@
 //2022-08-18
-//time  : O(n)
+//time  : O(nlog(n))
 //space : O(n)
 class Solution {
 public:
@@ -18,6 +18,36 @@ public:
             ++ans;
             size+=i;
             if(size <= arr.size()/2) break;
+        }
+        
+        return ans;
+    }
+};
+
+//2022-08-18
+//counting sort
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int minSetSize(vector<int>& arr)
+    {
+        unordered_map<int,int> table;
+        for(auto& val:arr) ++table[val];
+        
+        int cnt[10001]={0};
+        for(auto& i:table) ++cnt[i.second];
+        
+        int ans=0;
+        int frequency=10000;
+        int size=arr.size();
+        while(size>arr.size()/2)
+        {            
+            while(cnt[frequency]==0) --frequency;
+            
+            ++ans;   
+            size-=frequency;
+            --cnt[frequency];
         }
         
         return ans;
