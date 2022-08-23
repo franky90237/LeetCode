a376b90b4c023cef4f06f0441232890c6c6e43aa
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-23 20:00:26 +0800

    Update Hard_871. Minimum Number of Refueling Stops.cpp

--- Hard_871. Minimum Number of Refueling Stops.cpp
+++ Hard_871. Minimum Number of Refueling Stops.cpp
@@ -137,3 +137,42 @@ public:
         return -1;
     }
 };
+
+//2022-08-22
+//clear, dp iterative
+//time  : O(n*n)
+//space : O(n*n)
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations)
+    {
+        int n=stations.size();
+        if(n==0) return (startFuel>=target) ? 0 : -1;
+        
+        long dp[n+1][n+1];
+        memset(dp,0,sizeof(dp[0][0])*(n+1)*(n+1));
+        for(int i=0; i<=n; ++i) dp[i][0]=startFuel;       
+        
+        for(int i=1; i<=n; ++i)
+        {
+            for(int j=1; j<=i; ++j)
+            {                
+                dp[i][j]=dp[i-1][j];                
+                if(dp[i-1][j-1]>=stations[i-1][0]) dp[i][j]=max(dp[i][j],dp[i-1][j-1]+stations[i-1][1]);
+            }
+        }
+        
+        /*for(int i=0; i<=n; ++i)
+        {
+            for(int j=0; j<=n; ++j) cout<<dp[i][j]<<" ";
+            cout<<endl;
+        }*/
+        
+        for(int j=0; j<=n; ++j)
+        {
+            if(dp[n-1][j]>=target) return j;
+        }
+        
+        return -1;
+    }
+};
