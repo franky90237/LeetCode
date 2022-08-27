e93f956f8efee570e8060f822507d71ab1d30ff5
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-08-27 10:47:11 +0800

    Create Medium_61. Rotate List.cpp

---
+++ Medium_61. Rotate List.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//2022-08-27
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k)
+    {        
+        if(!head || !head->next || k==0) return head;
+        
+        int size=0;
+        ListNode* first_rear=reverse_list(head,size);
+        
+        k=k%size;        
+        if(k==0) return reverse_list(first_rear,size);
+        
+        ListNode* second_head=NULL;
+        ListNode* first_head=reverse_front_k_list(first_rear,second_head,k);
+        first_rear->next=reverse_list(second_head,size);
+        
+        return first_head;
+    }
+    
+    ListNode* reverse_list(ListNode* head, int& size)
+    {
+        ListNode* pre=NULL;
+        ListNode* cur=head;
+        
+        while(cur)
+        {
+            ++size;
+            
+            ListNode* nxt=cur->next;
+            cur->next=pre;
+            pre=cur;
+            cur=nxt;
+        }
+        
+        return pre;
+    }
+    
+    ListNode* reverse_front_k_list(ListNode* head, ListNode*& second_head, int k)
+    {
+        ListNode* pre=NULL;
+        ListNode* cur=head;
+        
+        while(k>0)
+        {
+            second_head=cur->next;
+            cur->next=pre;
+            pre=cur;
+            cur=second_head;
+            
+            --k;
+        }
+        
+        return pre;
+    }    
+};
