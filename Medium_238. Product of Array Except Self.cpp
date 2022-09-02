4c5aa2a899e4d44baa19f4a6f7fe83b7fbda0c39
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-02 17:48:23 +0800

    Update Medium_238. Product of Array Except Self.cpp

--- Medium_238. Product of Array Except Self.cpp
+++ Medium_238. Product of Array Except Self.cpp
@@ -52,3 +52,45 @@ public:
         return res;
     }
 };
+
+//2022-09-02
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        /*
+        1,2,3,4
+        
+        1,2,6,24
+        
+        a0 = 1
+        a1 = n1*a0 = n1
+        a2 = n2*a1 = n2*n1
+        a3 = n3*a2 = n3*n2*n1
+        
+        0 :      n1 * n2 * n3
+        1 : n0 *      n2 * n3
+        2 : n0 * n1 *      n3
+        3 : n0 * n1 * n2
+        */
+        
+        int n=nums.size();
+        
+        vector<int> prefix_product(n,1);        
+        for(int i=1; i<n; ++i)
+        {
+            prefix_product[i]=prefix_product[i-1]*nums[i-1];
+        }
+        
+        int suffix_prduct=nums[n-1];
+        for(int i=n-2; i>=0; --i)
+        {
+            prefix_product[i] *= suffix_prduct;
+            suffix_prduct *= nums[i];
+        }
+        
+        return prefix_product;
+    }
+};
