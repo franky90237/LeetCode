16e5799b1602ba3e81caa127911497e736aa15a6
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-07 13:40:18 +0800

    Update Medium_75. Sort Colors.cpp

--- Medium_75. Sort Colors.cpp
+++ Medium_75. Sort Colors.cpp
@@ -51,3 +51,84 @@ public:
         }
     }
 };
+
+//2022-09-07
+//time  : O(n)
+//sapce : O(1)
+class Solution {
+public:
+    void sortColors(vector<int>& nums)
+    {
+        /*
+
+        i
+        *  
+        1,2,2,0,0
+        ^       ^
+        f       s
+
+        i=0:
+        *
+        1,2,2,0,0
+        ^       ^
+        f       s
+
+        i=1:
+          *
+        1,0,2,0,2
+        ^     ^
+        f     s
+
+        i=1:
+          *
+        0,1,2,0,2
+          ^   ^
+          f   s
+
+        i=2:
+            *
+        0,1,0,2,2
+          ^ ^
+          f s
+
+        i=2:
+            *
+        0,0,1,2,2
+            ^
+            s
+            f
+
+        i=3:
+              *
+        0,0,1,2,2
+            ^
+            s
+            f
+
+        */        
+        int n=nums.size();
+        
+        int p0=0;
+        int p2=n-1;
+        int i=0;
+        while(i<=p2)
+        {
+            if(nums[i]==2)
+            {
+                swap(nums[i],nums[p2]);
+                --p2;
+            }
+            else if(nums[i]==0)
+            {
+                swap(nums[i],nums[p0]);
+                ++p0;
+                ++i;
+            }
+            else
+            {
+                ++i;
+            }
+        }
+        
+    }
+};
