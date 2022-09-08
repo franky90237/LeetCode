fe26660f3dc055936df5fdc227e94a9ca202e1ee
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-08 18:41:06 +0800

    Update Medium_560. Subarray Sum Equals K.cpp

--- Medium_560. Subarray Sum Equals K.cpp
+++ Medium_560. Subarray Sum Equals K.cpp
@@ -22,3 +22,49 @@ public:
         return ans;
     }
 };
+
+//2022-09-08
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k)
+    {
+        /*
+        1,1,1
+        1,2,3
+
+        4,5,-9,8,-8,2
+
+        1,1,-1,1,1 (1,1) (1,-1,1) (1,-1,1,1) (1,1)
+
+        1, 1,-1, 1, 1
+        1, 2, 1, 2, 3
+
+             1,1,1
+        s: 0 1 2 3
+        a: 0 0 1 
+        h: (0,1) (1,1) (2,1)
+        */        
+        int n=nums.size();
+        
+        unordered_map<int,int> table;
+        table[0]=1;
+        
+        int ans=0;
+        int sum=0;
+        for(int i=0; i<n; ++i)
+        {
+            sum+=nums[i];
+            int target=sum-k;
+            if(table.find(target)!=table.end())
+            {
+                ans+=table[target];
+            }
+            
+            ++table[sum];
+        }
+        
+        return ans;
+    }
+};
