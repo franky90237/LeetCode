451f335865ab2304d85b24a929f09b9b8a4b0063
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-13 17:32:42 +0800

    Create Medium_1268. Search Suggestions System.cpp

---
+++ Medium_1268. Search Suggestions System.cpp
@@ -0,0 +1,115 @@
+//2022-09-13
+//time  : O(n*m)
+//space : O(n*m), m is the average size of each product
+class trie_node
+{
+public:
+    vector<trie_node*> next;
+    bool isWord;
+        
+    trie_node()
+    {
+        next.resize(26,NULL);
+        isWord=false;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord)
+    {
+        trie_node* root=build_trie(products);
+        
+        vector<vector<string>> ans;
+        
+        string substring="";        
+        for(int i=0; i<searchWord.size(); ++i)
+        {
+            substring.push_back(searchWord[i]);
+            
+            vector<string> match;
+            if(!search_trie(root,substring,match)) break;
+            
+            if(match.size()>3) 
+            {
+                sort(match.begin(),match.end());
+                ans.push_back({match[0],match[1],match[2]});
+            }
+            else
+            {
+                ans.push_back(match);
+            }            
+            
+            //cout<<match.size()<<" "<<ans.size()<<endl;
+        }
+        
+        for(int i=ans.size(); i<searchWord.size(); ++i)
+        {
+            ans.push_back({});
+        }
+        
+        return ans;
+    }
+    
+    trie_node* build_trie(vector<string>& products)
+    {
+        trie_node* root=new trie_node();
+        
+        for(auto& product:products)
+        {
+            trie_node* cur=root;
+            
+            for(auto c:product)
+            {
+                int idx=c-'a';
+                
+                if(!cur->next[idx])
+                {
+                    trie_node* tmp=new trie_node();
+                    cur->next[idx]=tmp;
+                }
+                
+                cur=cur->next[idx];
+            }
+            
+            cur->isWord=true;
+        }
+        
+        return root;
+    }
+    
+    bool search_trie(trie_node* root, string prefix, vector<string>& match)
+    {        
+        string tmp="";        
+        for(auto& c:prefix)
+        {
+            tmp.push_back(c);
+            int idx=c-'a';
+            
+            if(!root->next[idx]) return false;
+            //if(root->isWord) match.push_back(tmp);
+            
+            root=root->next[idx];
+        }
+                
+        dfs(root,tmp,match);
+        
+        return true;
+    }
+    
+    void dfs(trie_node* root, string& tmp, vector<string>& match)
+    {
+        if(!root) return;
+        if(root->isWord) match.push_back(tmp);
+        
+        for(int i=0; i<26; ++i)
+        {
+            if(root->next[i])
+            {
+                tmp.push_back(i+'a');
+                dfs(root->next[i],tmp,match);
+                tmp.pop_back();
+            }
+        }
+    }
+};
