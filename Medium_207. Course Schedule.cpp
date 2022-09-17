873fcf46c763de1cac093624a3606d6042c782c6
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-17 14:31:11 +0800

    Update Medium_207. Course Schedule.cpp

--- Medium_207. Course Schedule.cpp
+++ Medium_207. Course Schedule.cpp
@@ -116,3 +116,58 @@ public:
         return false;
     }
 };
+
+//2022-09-17
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vector<vector<int>> graph(numCourses, vector<int>());
+        int indegree[numCourses];
+        for(int i=0; i<numCourses; ++i) indegree[i]=0;
+        build_graph(numCourses, prerequisites, graph, indegree);
+        
+        queue<int> q;
+        for(int i=0; i<numCourses; ++i)
+        {
+            //cout<<i<<" : "<<indegree[i]<<endl;
+            if(indegree[i]==0)
+            {
+                q.push(i);                
+            }
+        }
+        
+        int cnt=0;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ++cnt;
+            
+            for(auto& neighbor : graph[node])
+            {
+                --indegree[neighbor];
+                if(indegree[neighbor]==0)
+                {
+                    q.push(neighbor);
+                }
+            }
+        }
+        
+        return cnt==numCourses;
+    }
+        
+    void build_graph(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& graph, int indegree[])
+    {
+        for(auto& course : prerequisites)
+        {
+            int pre=course[1];
+            int nxt=course[0];
+            
+            graph[pre].push_back(nxt);
+            ++indegree[nxt];
+        }        
+    }
+};
