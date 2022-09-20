79da475e4c94838435daad101b1d4bb42f257211
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-20 08:59:35 +0800

    Update Easy_704. Binary Search.cpp

--- Easy_704. Binary Search.cpp
+++ Easy_704. Binary Search.cpp
@@ -1,5 +1,5 @@
 //2022-09-20
-//time  : O(long(n))
+//time  : O(log(n))
 //space : O(1)
 class Solution {
 public:
@@ -17,5 +17,26 @@ public:
         }
         
         return nums[r]==target ? r : -1;
+    }
+};
+
+//2022-09-20
+//time  : O(log(n))
+//space : O(log(n))
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        return solve(nums, target, 0, nums.size()-1);
+    }
+    
+    int solve(vector<int>& nums, int target, int l, int r)
+    {
+        if(l>r) return -1;
+        
+        int mid=l+(r-l)/2;
+        if(nums[mid] == target) return mid;
+        else if(target > nums[mid]) return solve(nums, target, mid+1, r);
+        else return solve(nums, target, l, mid-1);
     }
 };
