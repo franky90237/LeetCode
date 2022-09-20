73f8d55f0103abb2530a279c0bae932585bc96e9
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-20 11:13:12 +0800

    Update Medium_33. Search in Rotated Sorted Array.cpp

--- Medium_33. Search in Rotated Sorted Array.cpp
+++ Medium_33. Search in Rotated Sorted Array.cpp
@@ -30,3 +30,35 @@ public:
         return -1;
     }
 };
+
+//2022-09-20
+//time  : O(log(n))
+//space : O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        int l=0;
+        int r=nums.size()-1;
+        
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            //cout<<l<<" "<<mid<<" "<<r<<endl;
+            if(target==nums[mid]) return mid;
+            
+            if(nums[l]<=nums[mid])
+            {                
+                if(nums[l]<=target && target<nums[mid]) r=mid-1;
+                else l=mid+1;
+            }
+            else
+            {
+                if(nums[mid]<target && target<=nums[r]) l=mid+1;
+                else r=mid-1;
+            }
+        }
+        
+        return -1;
+    }
+};
