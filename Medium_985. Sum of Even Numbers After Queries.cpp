67b97799a04a724e1ba2d353f33e9b0cf9b1f4f7
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-21 14:41:37 +0800

    Update Medium_985. Sum of Even Numbers After Queries.cpp

--- Medium_985. Sum of Even Numbers After Queries.cpp
+++ Medium_985. Sum of Even Numbers After Queries.cpp
@@ -84,3 +84,64 @@ public:
         return ans;
     }
 };
+
+//2022-09-21
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        /*
+        [1,2,3,4] [[1,0],[-3,1],[-4,0],[2,3]]       
+        sum : 4+2=6
+
+        1. [1,0]
+        [1,2,3,4] -> [2,2,3,4]  
+        sum : 6+2=8
+        ans : 8
+
+        2. [-3,1] 
+        [2,2,3,4] -> [2,-1,3,4]
+        sum : 8-6=6
+        ans : 8,6
+
+        3. [-4,0] 
+        [2,-1,3,4] -> [-2,-1,3,4]
+        sum : 6-2-2=2
+        ans : 8,6,2
+
+        4. [2,3] 
+        [-2,-1,3,4] -> [-2,-1,3,6]     
+        sum : 2-4+6=4
+        ans : 8,6,2,4
+        */
+        
+        int sum=0;        
+        for(auto& num : nums)
+        {
+            if(num%2==0) sum+=num;            
+        }
+        
+        vector<int> ans;
+        for(auto& query : queries)
+        {
+            int val=query[0];
+            int idx=query[1];
+            
+            if(val == 0) 
+            {
+                ans.push_back(sum);
+                continue;
+            }
+            
+            if(nums[idx]%2==0) sum-=nums[idx];
+            nums[idx]=nums[idx]+val;
+            if(nums[idx]%2==0) sum+=nums[idx];
+            
+            ans.push_back(sum);
+        }        
+        
+        return ans;
+    }
+};
