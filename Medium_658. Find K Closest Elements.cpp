9f5fb35de80fddc6e71adfe33a75ca050cf8ae9f
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-09-30 18:25:58 +0800

    Update Medium_658. Find K Closest Elements.cpp

--- Medium_658. Find K Closest Elements.cpp
+++ Medium_658. Find K Closest Elements.cpp
@@ -51,3 +51,78 @@ public:
         return ans;
     }
 };
+
+//2022-09-30
+//time  : O(log(n))
+//space : O(1)
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x)
+    {
+        int l=0;
+        int r=arr.size()-k;
+        while(l < r)
+        {
+            int mid=l+(r-l)/2;
+            
+            if(x <= arr[mid])
+            {
+                r=mid;
+            }
+            else if(x >= arr[mid+k])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                int diff_mid=abs(x-arr[mid]);
+                int diff_midK=abs(x-arr[mid+k]);
+                
+                if(diff_mid <= diff_midK)
+                {
+                    r=mid;
+                }
+                else
+                {
+                    l=mid+1;
+                }
+            }
+        }
+        
+        vector<int> ans;
+        for(int i=l; i<l+k; ++i)
+        {
+            ans.push_back(arr[i]);
+        }
+        
+        return ans;
+    }
+};
+
+//2022-09-30
+//time  : O(n-k)
+//space : O(1)
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x)
+    {
+        int l=0;
+        int r=arr.size()-1;
+        while(r-l+1 > k)
+        {
+            int diff_l=abs(x-arr[l]);
+            int diff_r=abs(x-arr[r]);
+            
+            if(diff_l <= diff_r) --r;
+            else ++l;
+        }
+        
+        vector<int> ans;
+        for(int i=l; i<l+k; ++i)
+        {
+            ans.push_back(arr[i]);
+        }
+        
+        return ans;
+    }
+};
