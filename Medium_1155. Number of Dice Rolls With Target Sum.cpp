73254170d224a82c83cc4d70011ad2176b87e1ed
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-03 14:48:17 +0800

    Update Medium_1155. Number of Dice Rolls With Target Sum.cpp

--- Medium_1155. Number of Dice Rolls With Target Sum.cpp
+++ Medium_1155. Number of Dice Rolls With Target Sum.cpp
@@ -30,3 +30,38 @@ public:
         return res;
     }
 };
+
+//2022-10-03
+//time  : O(n*target*k)
+//space : O(n*target)
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target)
+    {
+        int mod=1e9+7;
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[0][0]=1;
+        
+        for(int i=1; i<=n; ++i)
+        {
+            for(int x=1; x<=target; ++x)
+            {
+                for(int val=max(x-k, 0); val<=x-1; ++val)
+                {
+                    dp[i][x]+=dp[i-1][val];
+                    dp[i][x]%=mod;
+                }
+                
+                //dp[i][x]%=mod;
+            }
+        }
+        
+        /*for(int i=0; i<=n; ++i)
+        {
+            for(int x=0; x<=target; ++x) cout<<dp[i][x]<<" ";
+            cout<<endl;
+        }*/
+        
+        return dp[n][target];
+    }
+};
