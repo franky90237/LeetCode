e0611917b0e822aed681606897bb845bb080932d
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-04 15:46:06 +0800

    Update Easy_112. Path Sum.cpp

--- Easy_112. Path Sum.cpp
+++ Easy_112. Path Sum.cpp
@@ -35,3 +35,19 @@ public:
         return (left | right);        
     }
 };
+
+//2022-10-04
+//time  : O(n)
+//space : O(h), h is the height of the tree
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        if(!root) return false;
+        
+        targetSum-=root->val;
+        if(!root->left && !root->right) return targetSum==0;
+        
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
+};
