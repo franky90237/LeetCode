38b84f914fb73214965fcef1053628dc6204f507
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-05 20:26:03 +0800

    Update Hard_2430. Maximum Deletions on a String.cpp

--- Hard_2430. Maximum Deletions on a String.cpp
+++ Hard_2430. Maximum Deletions on a String.cpp
@@ -45,3 +45,46 @@ public:
         return true;
     }
 };
+
+//2022-10-05
+//time  : O(n*n)
+//space : O(n*n)
+class Solution {
+public:
+    int deleteString(string s) 
+    {
+        int n=s.size();
+        
+        vector<vector<int>> lcs(n+1, vector<int>(n+1, 0));
+        for(int i=n-1; i>=0; --i)
+        {
+            for(int k=n-1; k>=0; --k)
+            {
+                if(s[i] == s[k])
+                {
+                    lcs[i][k]=1+lcs[i+1][k+1];
+                }
+            }
+        }
+        
+        vector<int> dp(n+1, 1);
+        dp[n]=0;    
+                
+        for(int i=n-1; i>=0; --i)
+        {
+            int len=n-i;
+            for(int k=1; k<=len/2; ++k)
+            {
+                if(lcs[i][i+k] >= k)
+                {
+                    dp[i]=max(dp[i], 1+dp[i+k]);
+                }                
+            }                        
+        }
+        
+        //for(int i=0; i<n; ++i) cout<<dp[i]<<" ";
+        //cout<<endl;
+        
+        return dp[0];
+    }
+};
