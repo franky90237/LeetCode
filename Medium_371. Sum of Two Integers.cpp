0eba37b2c49931b5fc7d61c5cac7a00e35dfa032
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-07 19:30:08 +0800

    Update Medium_371. Sum of Two Integers.cpp

--- Medium_371. Sum of Two Integers.cpp
+++ Medium_371. Sum of Two Integers.cpp
@@ -45,3 +45,23 @@ public:
         return res;
     }
 };
+
+//2022-10-07
+//time  : O(32)
+//space : O(1)
+class Solution {
+public:
+    int getSum(int a, int b) 
+    {    
+        while(b != 0)
+        {
+            unsigned int carry=a&b;
+            int sum=a^b;
+            
+            a=sum;
+            b=carry<<1;
+        }
+        
+        return a;
+    }
+};
