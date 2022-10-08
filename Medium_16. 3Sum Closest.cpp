5e426f3c968588cd7e3699915c46970cee847263
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-09 06:32:07 +0800

    Update Medium_16. 3Sum Closest.cpp

--- Medium_16. 3Sum Closest.cpp
+++ Medium_16. 3Sum Closest.cpp
@@ -164,3 +164,52 @@ public:
         return ans;
     }
 };
+
+//2022-10-09
+//time  : O(n*n)
+//space : O(1)
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        
+        int ans=nums[0]+nums[1]+nums[2];
+        int i=0;
+        while(i <= n-3)
+        {
+            int l=i+1;
+            int r=n-1;
+            while(l < r)
+            {
+                int sum=nums[i]+nums[l]+nums[r];
+                
+                if(sum==target)
+                {
+                    return sum;
+                }                
+                else if(abs(target-sum) < abs(target-ans))
+                {
+                    ans=sum;
+                }
+                
+                if(sum > target)
+                {
+                    --r;
+                    while(l < r && nums[r] == nums[r+1]) --r;                     
+                }
+                else
+                {
+                    ++l;
+                    while(l < r && nums[l] == nums[l-1]) ++l;                     
+                }                                             
+            }        
+            
+            ++i;
+            while(i <= n-3 && nums[i] == nums[i-1]) ++i;
+        }
+        
+        return ans;
+    }
+};
