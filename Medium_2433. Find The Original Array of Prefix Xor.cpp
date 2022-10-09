e1e9cfe9b309e48228358641d59aac2aaf8ca115
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-09 18:56:59 +0800

    Update Medium_2433. Find The Original Array of Prefix Xor.cpp

--- Medium_2433. Find The Original Array of Prefix Xor.cpp
+++ Medium_2433. Find The Original Array of Prefix Xor.cpp
@@ -75,3 +75,19 @@ public:
         return pref;
     }
 };
+
+//2022-10-09
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref)
+    {        
+        for(int i=pref.size()-1; i>=1; --i)
+        {            
+            pref[i] = pref[i-1] ^ pref[i];            
+        }
+        
+        return pref;
+    }
+};
