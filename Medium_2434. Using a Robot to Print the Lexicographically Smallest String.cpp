a76f024fd1cf21c1ef203f4964b9a5542b4016c0
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-10 10:01:49 +0800

    Update Medium_2434. Using a Robot to Print the Lexicographically Smallest String.cpp

--- Medium_2434. Using a Robot to Print the Lexicographically Smallest String.cpp
+++ Medium_2434. Using a Robot to Print the Lexicographically Smallest String.cpp
@@ -49,3 +49,48 @@ public:
         return false;
     }
 };
+
+//2022-10-10
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    string robotWithString(string s) 
+    {
+        int n=s.size();         
+        
+        int freq[26]={0};
+        for(auto& c: s) ++freq[c-'a'];
+        
+        int smallest=0;
+        string paper;
+        string t;
+        for(int i=0; i<n; ++i)
+        {
+            char c=s[i];
+            t.push_back(c);
+            --freq[c-'a'];         
+            
+            while(smallest < 26 && freq[smallest] == 0) ++smallest;
+            
+            while(!t.empty() && t.back() <= (smallest+'a'))
+            {
+                //cout<<t.back()<<" "<<t<<endl;
+                paper.push_back(t.back());
+                t.pop_back();                
+            }
+            
+            /*cout<<t<<" : "<<paper<<endl;
+            for(auto& it: table) cout<<"("<<it.first<<", "<<it.second<<") ";
+            cout<<endl;*/
+        }
+        
+        while(!t.empty())
+        {
+            paper.push_back(t.back());
+            t.pop_back();          
+        }
+                        
+        return paper;
+    }    
+};
