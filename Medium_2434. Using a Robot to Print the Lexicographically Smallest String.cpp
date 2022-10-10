3c926c2d65fe7304e68bc86418566a7cd2d3141f
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-10 08:09:27 +0800

    Create Medium_2434. Using a Robot to Print the Lexicographically Smallest String.cpp

---
+++ Medium_2434. Using a Robot to Print the Lexicographically Smallest String.cpp
@@ -0,0 +1,51 @@
+//2022-10-09
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    string robotWithString(string s) 
+    {
+        int n=s.size();         
+        
+        map<char, int> table;
+        for(int i=0; i<n; ++i)
+        {            
+            ++table[s[i]];
+        }
+                
+        string paper;
+        string t;
+        for(int i=0; i<n; ++i)
+        {
+            char c=s[i];
+            t.push_back(c);
+            --table[c];
+            if(table[c]==0) table.erase(c);
+            
+            while(!t.empty() && smallest(table, t.back()))
+            {
+                //cout<<t.back()<<" "<<t<<endl;
+                paper.push_back(t.back());
+                t.pop_back();                
+            }
+            
+            /*cout<<t<<" : "<<paper<<endl;
+            for(auto& it: table) cout<<"("<<it.first<<", "<<it.second<<") ";
+            cout<<endl;*/
+        }
+        
+        while(!t.empty())
+        {
+            paper.push_back(t.back());
+            t.pop_back();          
+        }
+                        
+        return paper;
+    }
+    
+    bool smallest(map<char, int>& table, char c)
+    {    
+        if(table.begin()->first >= c) return true;
+        return false;
+    }
+};
