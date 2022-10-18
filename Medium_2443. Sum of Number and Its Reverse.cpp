2d0cc3b81084350f65ce5f50fbd2a87b0cf5fff4
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-19 07:26:24 +0800

    Update Medium_2443. Sum of Number and Its Reverse.cpp

--- Medium_2443. Sum of Number and Its Reverse.cpp
+++ Medium_2443. Sum of Number and Its Reverse.cpp
@@ -26,3 +26,44 @@ public:
         return sum;
     }
 };
+
+//2022-10-19
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) 
+    {
+        int revI;
+        int increment=1;
+        for(int i=0; i<=num; ++i)
+        {
+            if(i%10 == 0)
+            {
+                revI=reverse_int(i);
+                if(revI == 1) increment *= 10;                
+            }
+            else
+            {
+                revI = revI + increment;
+            }
+            
+            //cout<<i<<" "<<revI<<endl;
+            if(i + revI == num) return true;
+        }
+        
+        return false;
+    }
+    
+    int reverse_int(int num)
+    {
+        int sum=0;
+        while(num != 0)
+        {
+            sum = sum*10 + num%10;
+            num /= 10;
+        }
+        
+        return sum;
+    }
+};
