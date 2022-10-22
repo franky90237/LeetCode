fe12a6a37cff564919e0b63e064ef264b9c6d2fc
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-22 08:25:31 +0800

    Update Medium_2249. Count Lattice Points Inside a Circle.cpp

--- Medium_2249. Count Lattice Points Inside a Circle.cpp
+++ Medium_2249. Count Lattice Points Inside a Circle.cpp
@@ -32,7 +32,66 @@ public:
     }
 };
 
-//2022-10-21
+//2022-10-22
+//TLE
+//bfs
+class Solution {
+public:
+    int countLatticePoints(vector<vector<int>>& circles) 
+    {
+        set<pair<int, int>> point;       
+        for(auto& circle: circles)
+        {
+            int cx=circle[0];
+            int cy=circle[1];
+            int r=circle[2];
+
+            bfs(point, cx, cy, r);
+        }
+        
+        return point.size();
+    }
+    
+    void bfs(set<pair<int, int>>& point, int cx, int cy, int r)
+    {
+        int dir[4][2]={{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        vector<vector<bool>> visited(cx+r+1, vector<bool>(cy+r+1, false));
+        //bool visited[200][200]={false};
+        visited[cx][cy]=true;
+            
+        queue<pair<int, int>> q;
+        q.push({cx, cy});        
+        
+        point.insert({cx, cy});        
+        while(r > 0)
+        {
+            int size=q.size();
+            while(size > 0)
+            {
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+                
+                for(int i=0; i<4; ++i)
+                {
+                    int nx=x+dir[i][0];
+                    int ny=y+dir[i][1];
+                    if(visited[nx][ny]) continue;
+                    
+                    q.push({nx, ny});
+                    point.insert({nx, ny});                    
+                }
+                
+                --size;
+            }
+            
+            --r;
+        }
+    }
+};
+
+//2022-10-22
 //time  : O(n*200*200)
 //space : O(1)
 class Solution {
