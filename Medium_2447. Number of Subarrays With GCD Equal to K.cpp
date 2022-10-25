af166becb227d579080b44f316ad34ad298ff1c2
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-25 22:51:40 +0800

    Update Medium_2447. Number of Subarrays With GCD Equal to K.cpp

--- Medium_2447. Number of Subarrays With GCD Equal to K.cpp
+++ Medium_2447. Number of Subarrays With GCD Equal to K.cpp
@@ -20,3 +20,52 @@ public:
         return ans;
     }    
 };
+
+//2022-10-25
+//time  : O(n*m), m is the preGCD count
+//space : O(n*m)
+class Solution {
+public:
+    int subarrayGCD(vector<int>& nums, int k) 
+    {        
+        int n=nums.size();                
+        int ans=0;
+        
+        unordered_map<int, int> preGCD[n];
+        if(nums[0]%k == 0) 
+        {
+            ++preGCD[0][nums[0]];
+            if(nums[0] == k) ++ans;
+        }
+                
+        for(int i=1; i<n; ++i)
+        {
+            if(nums[i]%k != 0) continue;
+                        
+            ++preGCD[i][nums[i]];
+            if(nums[i] == k) ++ans;
+            
+            for(auto& it: preGCD[i-1])
+            {
+                int curGCD=gcd(nums[i], it.first);
+                if(curGCD%k == 0)
+                {
+                    preGCD[i][curGCD] += it.second;
+                    if(curGCD == k) ans += it.second;
+                }
+            }
+            //cout<<ans<<endl;
+        }
+        
+        /*for(int i=0; i<n; ++i)
+        {
+            for(auto& it: preGCD[i])
+            {
+                cout<<"("<<it.first<<","<<it.second<<") ";
+            }
+            cout<<endl;
+        }*/
+        
+        return ans;
+    }
+};
