c209c67772fcdd4660818bed5444c6742f2c6bbe
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-26 21:20:22 +0800

    Update Medium_523. Continuous Subarray Sum.cpp

--- Medium_523. Continuous Subarray Sum.cpp
+++ Medium_523. Continuous Subarray Sum.cpp
@@ -29,3 +29,54 @@ public:
         return false;
     }
 };
+
+//2022-10-26
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) 
+    {               
+        int n=nums.size();
+        if(n <= 1) return false;      
+        
+        unordered_map<int, int> remainder;
+        remainder[nums[0]%k]=0;
+        remainder[0]=-1; 
+        
+        int r=nums[0];        
+        for(int i=1; i<nums.size(); ++i)
+        {
+            r=(r+nums[i])%k;
+            if(remainder.find(r) != remainder.end() && i-remainder[r] >= 2) return true;
+            
+            if(remainder.find(r) != remainder.end())
+            {
+                remainder[r]=min(remainder[r], i);
+            }
+            else
+            {
+                remainder[r]=i;
+            }
+        }
+        
+        return false;
+    }
+};
+
+
+/*
+
+n[i]+n[i+1]+...n[k]
+sum[i]=n[0]+n[1]+..n[i-1]
+
+(n[i]+n[i+1]+...n[k])%k=0
+n[i]+n[i+1]+...n[k]=sum[k+1]-sum[i]
+
+(n[i]+n[i+1]+...n[k])%k = (sum[k+1]-sum[i])%k = 0
+
+sum[k+1]%k = sum[i]%k
+
+29%6=5
+23%6=5
+*/
