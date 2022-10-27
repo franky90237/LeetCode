d572f1e96cd4fae4a51bde2a109023a8f8c3c6bd
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-27 08:10:22 +0800

    Update Medium_560. Subarray Sum Equals K.cpp

--- Medium_560. Subarray Sum Equals K.cpp
+++ Medium_560. Subarray Sum Equals K.cpp
@@ -25,7 +25,7 @@ public:
 
 //2022-09-08
 //time  : O(n)
-//space : O(1)
+//space : O(n)
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k)
@@ -63,6 +63,47 @@ public:
             }
             
             ++table[sum];
+        }
+        
+        return ans;
+    }
+};
+
+//2022-10-27
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k)
+    {        
+        /*
+        3
+        1,2,3
+            ^
+        sum : 1 3 6  
+        tar : -2 0 3
+        ans : 0 1 2 
+        set : {0,3,6}
+
+        n[i]+[i+1]+...+n[x]=k
+        sum[x+1]-sum[i]=k
+        */
+        
+        int n=nums.size();       
+        
+        unordered_map<int, int> prefixSum;
+        prefixSum[0]=1;
+        
+        int ans=0;
+        int sum=0;
+        for(int i=0; i<n; ++i)
+        {                                            
+            sum+=nums[i];
+            int target=sum-k;
+            if(prefixSum.find(target) != prefixSum.end()) ans += prefixSum[target];
+            ++prefixSum[sum];
+            
+            //cout<<sum<<" "<<target<<" | "<<ans<<endl;
         }
         
         return ans;
