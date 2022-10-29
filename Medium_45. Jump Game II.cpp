5473dd1982156aeb1fa28693380de05a70d04332
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-10-29 08:51:51 +0800

    Update Medium_45. Jump Game II.cpp

--- Medium_45. Jump Game II.cpp
+++ Medium_45. Jump Game II.cpp
@@ -29,3 +29,43 @@ public:
         return minCnt;
     }
 };
+
+//2022-10-29
+//time  : O(n*m), m is average num[i]
+//spcae : O(n)
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        int n=nums.size();
+        if(n <= 1) return 0;
+        
+        vector<int> dp(n, 9999);        
+        dp[n-1]=0;
+        
+        for(int i=n-2; i>=0; --i)
+        {
+            if(i+nums[i] >= n-1) 
+            {
+                dp[i]=1;
+                continue;
+            }
+                    
+            for(int step=1; step<=nums[i]; ++step)
+            {
+                if(i+step >= n-1)
+                {
+                    dp[i]=1;
+                    break;
+                }
+                
+                dp[i]=min(dp[i], 1+dp[i+step]);
+            }
+        }    
+        
+        //for(int i=0; i<n; ++i) cout<<dp[i]<<" ";
+        //cout<<endl;
+        
+        return dp[0];
+    }    
+};
