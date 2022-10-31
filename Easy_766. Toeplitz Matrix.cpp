37363e967aa5f17c63f1a98249ca03b4734b4806
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-11-01 07:42:28 +0800

    Create Easy_766. Toeplitz Matrix.cpp

---
+++ Easy_766. Toeplitz Matrix.cpp
@@ -0,0 +1,41 @@
+//2022-11-01
+//time  : O(m*n)
+//space : O(1)
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) 
+    {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        for(int r=m-1; r>=0; --r)
+        {
+            //cout<<r<<endl;
+            int val=matrix[r][0];
+            int i=r;
+            int k=0;
+            while(i < m && k < n)
+            {
+                if(matrix[i][k] != val) return false;
+                ++i;
+                ++k;
+            }
+        }
+        
+        for(int c=1; c<n-1; ++c)
+        {
+            //cout<<c<<endl;
+            int val=matrix[0][c];
+            int i=0;
+            int k=c;
+            while(i < m && k < n)
+            {
+                if(matrix[i][k] != val) return false;
+                ++i;
+                ++k;
+            }
+        }
+                
+        return true;
+    }
+};
