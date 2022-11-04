310400cda3c81be4d508060bf7de484538fadcb5
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-11-04 08:37:04 +0800

    Update Medium_2458. Height of Binary Tree After Subtree Removal Queries.cpp

--- Medium_2458. Height of Binary Tree After Subtree Removal Queries.cpp
+++ Medium_2458. Height of Binary Tree After Subtree Removal Queries.cpp
@@ -117,3 +117,189 @@ public:
         return height;
     }
 };
+
+//2022-11-04
+//TLE
+//time  : O(nlog(n))
+//space : O(n)
+struct Item
+{   
+    int level;
+    int height;
+    
+    Item()
+    {
+        
+    }
+    
+    Item(int l, int h)
+    {
+        level=l;
+        height=h;
+    }    
+};
+
+class Solution {
+public:
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) 
+    {
+        unordered_map<int, Item> table;
+        vector<vector<int>> hierarchy;
+        dfs(0, root, table, hierarchy);
+        
+        unordered_map<int, int> visited;
+        vector<int> ans;
+        for(int num: queries)
+        {
+            if(visited.find(num) == visited.end())
+            {
+                int height=getTrimHeight(root->val, num, table, hierarchy);
+                ans.push_back(height);
+                visited[num]=height;
+            }
+            else
+            {
+                ans.push_back(visited[num]);
+            }
+        }
+        
+        return ans;
+    }
+    
+    int dfs(int level, TreeNode* root, unordered_map<int, Item>& table, vector<vector<int>>& hierarchy)
+    {
+        if(!root) return -1;
+        
+        if(level == hierarchy.size()) hierarchy.push_back({});
+        hierarchy[level].push_back(root->val);
+        
+        int lLen=dfs(level+1, root->left, table, hierarchy);
+        int rLen=dfs(level+1, root->right, table, hierarchy);                
+        int height = max(lLen, rLen)+1;
+        Item tmp(level, height);
+        table[root->val]=tmp;
+        
+        return height;
+    }
+    
+    int getTrimHeight(int root, int cur, unordered_map<int, Item>& table, vector<vector<int>>& hierarchy)
+    {
+        int level=table[cur].level;
+        int height=level-1;
+        for(int sibling: hierarchy[level])
+        {
+            if(sibling == cur) continue;
+            height=max(height, table[sibling].level+table[sibling].height);
+        }
+                
+        return height;
+    }
+};
+
+//2022-11-04
+//time  : O(nlog(n))
+//space : O(n)
+struct Item
+{
+    int parent;
+    int left;
+    int right;
+    int level;
+    int height;
+    
+    Item()
+    {
+        
+    }
+    
+    Item(int p, int _left, int r, int _level, int h)
+    {
+        parent=p;
+        left=_left;
+        right=r;
+        level=_level;
+        height=h;
+    }    
+};
+
+class Solution {
+public:
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) 
+    {
+        unordered_map<int, Item> table;
+        vector<vector<int>> hierarchy;
+        dfs(0, NULL, root, table, hierarchy);
+        
+        unordered_map<int, int> visited;
+        vector<int> ans;
+        for(int num: queries)
+        {
+            if(visited.find(num) == visited.end())
+            {
+                int level=table[num].level;
+                int horizen=hierarchy[level].size();
+                int vertical=log(level)/log(2);
+                
+                int height;
+                if(horizen < vertical) height=getHeightHorizental(root->val, num, table, hierarchy);
+                else height=getHeightVertical(root->val, num, table);
+                
+                ans.push_back(height);
+                visited[num]=height;
+            }
+            else
+            {
+                ans.push_back(visited[num]);
+            }
+        }
+        
+        return ans;
+    }
+    
+    int dfs(int level, TreeNode* parent, TreeNode* root, unordered_map<int, Item>& table, vector<vector<int>>& hierarchy)
+    {
+        if(!root) return -1;
+        
+        if(level == hierarchy.size()) hierarchy.push_back({});
+        hierarchy[level].push_back(root->val);
+        
+        int lLen=dfs(level+1, root, root->left, table, hierarchy);
+        int rLen=dfs(level+1, root, root->right, table, hierarchy);
+        int pVal = (parent) ? parent->val : -1;
+        int lVal = (root->left) ? root->left->val : -1;
+        int rVal = (root->right) ? root->right->val : -1;        
+        int height = max(lLen, rLen)+1;
+        
+        Item tmp(pVal, lVal, rVal, level, height);
+        table[root->val]=tmp;
+        
+        return height;
+    }
+    
+    int getHeightHorizental(int root, int cur, unordered_map<int, Item>& table, vector<vector<int>>& hierarchy)
+    {
+        int level=table[cur].level;
+        int height=level-1;
+        for(int sibling: hierarchy[level])
+        {
+            if(sibling == cur) continue;
+            height=max(height, table[sibling].level+table[sibling].height);
+        }
+                
+        return height;
+    }
+    
+    int getHeightVertical(int root, int cur, unordered_map<int, Item>& table)
+    {   
+        int height=-1;
+        while(cur != root)
+        {
+            int parent = table[cur].parent;
+            int sibling = (table[parent].left != cur) ? table[parent].left : table[parent].right; 
+            height=max(height, table[sibling].height)+1;
+            cur=parent;
+        }
+           
+        return height;
+    }
+};
