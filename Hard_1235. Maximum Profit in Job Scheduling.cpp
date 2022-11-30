35c841f69fde719c35f06939a3051a025acb23a1
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-11-30 19:49:31 +0800

    Update Hard_1235. Maximum Profit in Job Scheduling.cpp

--- Hard_1235. Maximum Profit in Job Scheduling.cpp
+++ Hard_1235. Maximum Profit in Job Scheduling.cpp
@@ -67,3 +67,55 @@ public:
         return input[pre][1] > input[cur][0];
     }
 };
+
+//2022-11-30
+//time  : O(n)
+//space : O(n)
+class Solution 
+{
+private:
+    int dp[50001];
+    
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
+    {
+        int n=startTime.size();
+        
+        vector<vector<int>> input;
+        for(int i=0; i<n; ++i)
+        {
+            input.push_back({startTime[i], endTime[i], profit[i]});
+        }
+        sort(input.begin(), input.end());
+        
+        memset(dp, -1, sizeof(dp));
+        dp[n]=0;
+        int ans=0;
+        for(int i=n-1; i>=0; --i)
+        {
+            int idx=binarySearch(input, i+1, input[i][1]);
+            int profit = input[i][2] + dp[idx];
+            dp[i]=max(dp[i+1], profit);
+            ans = max(ans, dp[i]);
+        }
+        
+        return ans;
+    }    
+    
+    int binarySearch(vector<vector<int>>& input, int l, int target)
+    {
+        int r=input.size()-1;
+        //if(l >= r) return r+1;
+        
+        while(l < r)
+        {
+            int m=l+(r-l)/2;
+            if(target <= input[m][0]) r=m;
+            else l=m+1;
+        }
+        
+        if(target <= input[r][0]) return r;
+        return r+1;
+    }
+    
+};
