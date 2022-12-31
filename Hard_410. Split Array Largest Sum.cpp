3359d21f35795dfd99fb3861b1b68935794a61d1
franky90237 <30579359+franky90237@users.noreply.github.com>
2022-12-31 13:10:42 +0800

    Update Hard_410. Split Array Largest Sum.cpp

--- Hard_410. Split Array Largest Sum.cpp
+++ Hard_410. Split Array Largest Sum.cpp
@@ -62,3 +62,50 @@ public:
         return cnt <= k;
     }
 };
+
+//2022-12-31
+//time  : O(n*n*k)
+//space : O(n*k)
+class Solution 
+{
+private:
+    vector<vector<int>> dp;
+    
+public:
+    int splitArray(vector<int>& nums, int k)
+    {        
+        int n=nums.size();
+        
+        vector<int> prefix_sum(n+1);
+        prefix_sum[0]=0;        
+        for(int i=1; i<=n; ++i)
+        {
+            prefix_sum[i]=nums[i-1]+prefix_sum[i-1];
+        }
+        
+        dp.resize(n, vector<int>(k+1, -1));
+        return solve(nums, prefix_sum, 0, k);
+    }
+    
+    int solve(vector<int>& nums, vector<int>& prefix_sum, int cur, int k)
+    {
+        int n=nums.size();
+        if(k == 1) return prefix_sum[n]-prefix_sum[cur];
+        if(dp[cur][k] != -1)
+        {
+            return dp[cur][k];
+        }
+        
+        int sum=0;
+        int ans=INT_MAX;
+        for(int i=cur; i<=n-k; ++i)
+        {
+            sum += nums[i];
+            int largestSum=max(sum, solve(nums, prefix_sum, i+1, k-1));
+            ans=min(ans, largestSum);
+        }
+        
+        dp[cur][k]=ans;
+        return ans;
+    }
+};
