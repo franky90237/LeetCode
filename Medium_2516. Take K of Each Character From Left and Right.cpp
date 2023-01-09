f752ad47d3f4ea1139d4681bbc69766d2f7347f1
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-01-09 20:36:18 +0800

    Update Medium_2516. Take K of Each Character From Left and Right.cpp

--- Medium_2516. Take K of Each Character From Left and Right.cpp
+++ Medium_2516. Take K of Each Character From Left and Right.cpp
@@ -48,3 +48,46 @@ public:
         return table[0] >= k && table[1] >= k && table[2] >= k;
     }
 };
+
+//2022-01-09
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int takeCharacters(string s, int k) 
+    {
+        vector<int> table(3, 0);
+        for(auto c: s)
+        {
+            ++table[c-'a'];
+        }
+        
+        if(!match(table ,k)) return -1;
+                
+        int n=s.size();
+        int l=0;
+        int r=0;
+        int ans=n;
+        while(r < n)
+        {
+            --table[s[r]-'a'];
+            
+            while(l <= r && !match(table, k))
+            {
+                ++table[s[l]-'a'];
+                ++l;                
+            }
+            
+            ans = min(ans, n-(r-l+1));
+            
+            ++r;
+        }
+        
+        return ans;
+    }
+    
+    bool match(vector<int>& table, int k)
+    {
+        return table[0] >= k && table[1] >= k && table[2] >= k;
+    }
+};
