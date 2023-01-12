1c646eddbe338ce7e444bd3ff75f16c857fabe25
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-01-12 08:24:53 +0800

    Update Hard_862. Shortest Subarray with Sum at Least K.cpp

--- Hard_862. Shortest Subarray with Sum at Least K.cpp
+++ Hard_862. Shortest Subarray with Sum at Least K.cpp
@@ -32,3 +32,44 @@ public:
         return (ans==n+1) ? -1 : ans;
     }    
 };
+
+//2023-01-12
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        deque<pair<long long, int>> dq;
+        long long sum=0;
+        int ans=n+1;
+        
+        for(int i=0; i<n; ++i)
+        {
+            sum += nums[i];
+            
+            if(sum >= k)
+            {
+                ans=min(ans, i+1);
+            }
+            
+            while(!dq.empty() && sum-dq.front().first >= k)
+            {
+                int idx=dq.front().second;
+                dq.pop_front();
+                ans=min(ans, i-idx);
+            }
+            
+            while(!dq.empty() && sum <= dq.back().first)
+            {
+                dq.pop_back();
+            }
+            
+            //cout<<i<<" "<<ans<<" "<<dq.size()<<endl;
+            dq.push_back({sum, i});
+        }
+        
+        return (ans==n+1) ? -1 : ans;
+    }    
+};
