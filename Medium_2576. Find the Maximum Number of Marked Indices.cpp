7db5ed49e9d6757d0b00ce4a51d5f990a45ce4fc
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-02-26 12:37:02 +0800

    Create Medium_2576. Find the Maximum Number of Marked Indices.cpp

---
+++ Medium_2576. Find the Maximum Number of Marked Indices.cpp
@@ -0,0 +1,47 @@
+//2023-02-26
+//time  : O(nlog(n))
+//space : O(1)
+class Solution {
+public:
+    int maxNumOfMarkedIndices(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        
+        int l=0;
+        int r=nums.size()/2;
+        int ans=0;
+        
+        while(l <= r)
+        {
+            int mid=l+(r-l)/2;
+            
+            if(check(nums, mid))
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        
+        return ans*2;
+    }
+    
+    bool check(vector<int>& nums, int cnt)
+    {
+        int n=nums.size();
+        int l=0;
+        int r=n-cnt;
+        
+        while(cnt > 0 && 2*nums[l] <= nums[r])
+        {
+            --cnt;
+            ++l;
+            ++r;
+        }
+        
+        return cnt==0;
+    }
+};
