138ea2dd127812b9ade9102f5637087f02ae2188
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-03-25 17:59:33 +0800

    Update Medium_2597. The Number of Beautiful Subsets.cpp

--- Medium_2597. The Number of Beautiful Subsets.cpp
+++ Medium_2597. The Number of Beautiful Subsets.cpp
@@ -53,3 +53,42 @@ public:
         return pick+not_pick;
     }
 };
+
+//2023-03-25
+//time  : O(2^n)
+//space : O(n)
+class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k)
+    {
+        vector<int> subset(1001, 0);
+        return solve(nums, k, 0, subset)-1;
+    }
+    
+    int solve(vector<int>& nums, int k, int cur, vector<int>& subset)
+    {
+        int n=nums.size();
+        if(cur >= n)
+        {
+            return 1;
+        }
+        
+        int pick=0;
+        int not_pick=0;
+            
+        int big=nums[cur]+k;
+        int small=nums[cur]-k;
+        
+        if((big >= subset.size() || subset[big] == 0) && (small < 0 || subset[small] == 0))
+        {
+            ++subset[nums[cur]];
+            pick=solve(nums, k, cur+1, subset);
+            --subset[nums[cur]];                        
+        }
+        
+        not_pick=solve(nums, k, cur+1, subset);
+        
+        //cout<<cur<<" | "<<pick<<" "<<not_pick<<endl;
+        return pick+not_pick;
+    }
+};
