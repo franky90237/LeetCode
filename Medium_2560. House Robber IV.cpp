cc00c7bd3da4a77a901c35b68ff70097cbf1b7a0
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-04-11 07:39:09 +0800

    Update Medium_2560. House Robber IV.cpp

--- Medium_2560. House Robber IV.cpp
+++ Medium_2560. House Robber IV.cpp
@@ -63,3 +63,68 @@ public:
         return false;
     }
 };
+
+//2023-04-11
+//time  : O(nlog(max(nums)))
+//space : O(1)
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k)
+    {
+        int low=INT_MAX;
+        int high=INT_MIN;
+        for(auto num: nums)
+        {
+            low=min(low, num);
+            high=max(high, num);
+        }
+        
+        int ans=low;
+        while(low <= high)
+        {
+            int mid=low+(high-low)/2;
+            //cout<<low<<" "<<mid<<" "<<high<<endl;
+            if(check(nums, k, mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    bool check(vector<int>& nums, int k, int target)
+    {
+        int n=nums.size();
+        int pre1=0;
+        int pre2=0;    
+        
+        for(int i=0; i<n; ++i)
+        {
+            int notPick = pre1;
+            int pick = pre1;
+                    
+            if(nums[i] <= target)
+            {                
+                pick=1+pre2;                
+            }
+            
+            int cur = max(notPick, pick);
+            //cout<<i<<" : "<<pre2<<" "<<pre1<<" "<<cur<<endl;
+            if(cur >= k)
+            {
+                return true;
+            }
+            
+            pre2=pre1;
+            pre1=cur;            
+        }
+        
+        return false;
+    }
+};
