d95dbf8c1852153972b2c5df2899b044ea477b78
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-04-12 21:13:55 +0800

    Update Medium_669. Trim a Binary Search Tree.cpp

--- Medium_669. Trim a Binary Search Tree.cpp
+++ Medium_669. Trim a Binary Search Tree.cpp
@@ -78,3 +78,29 @@ public:
         return val >= low && val <= high;
     }
 };
+
+//2023-04-12
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high)
+    {
+        if(!root)
+        {
+            return NULL;
+        }
+        
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        
+        if(root->val > high)
+        {
+            return root->left;
+        }
+        else if(root->val < low)
+        {
+            return root->right;
+        }
+        
+        return root;
+    }
+};
