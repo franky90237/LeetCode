85e865f9bda1fc2a8aa4a56489a57e60b26e2f63
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-04-29 20:05:00 +0800

    Update Medium_209. Minimum Size Subarray Sum.cpp

--- Medium_209. Minimum Size Subarray Sum.cpp
+++ Medium_209. Minimum Size Subarray Sum.cpp
@@ -108,3 +108,33 @@ public:
         return false;
     }
 };
+
+//2023-04-29
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums)
+    {
+        int n=nums.size();
+        int ans=n+1;
+        int sum=0;
+        int left=0;
+        for(int right=0; right<n; ++ right)
+        {
+            sum += nums[right];
+            while(sum-nums[left] >= target)
+            {
+                sum -= nums[left];
+                ++left;
+            }
+            
+            if(sum >= target)
+            {
+                ans = min(ans, right-left+1);
+            }
+        }
+        
+        return ans==n+1 ? 0 : ans;
+    }
+};
