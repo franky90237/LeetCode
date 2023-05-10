a38ac5325277e868baeeac8b7ee45ba8a575c84b
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-05-10 21:07:44 +0800

    Update Medium_1004. Max Consecutive Ones III.cpp

--- Medium_1004. Max Consecutive Ones III.cpp
+++ Medium_1004. Max Consecutive Ones III.cpp
@@ -38,3 +38,33 @@ public:
         return ans;
     }
 };
+
+//2023-05-10
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        int left=0;
+        int ans=0;
+        for(int right=0; right<n; ++right)
+        {
+            if(nums[right] == 0)
+            {
+                while(left <= right && k <= 0)
+                {
+                    if(nums[left] == 0) ++k;
+                    ++left;
+                }
+                
+                --k;
+            }
+            
+            ans=max(ans, right-left+1);
+        }
+        
+        return ans;
+    }
+};
