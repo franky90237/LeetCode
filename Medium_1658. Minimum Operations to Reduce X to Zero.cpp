91b6cd1750f6e0ec12de4cc31ac9e4e03daa9918
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-06-28 20:51:21 +0800

    Update Medium_1658. Minimum Operations to Reduce X to Zero.cpp

--- Medium_1658. Minimum Operations to Reduce X to Zero.cpp
+++ Medium_1658. Minimum Operations to Reduce X to Zero.cpp
@@ -42,3 +42,56 @@ public:
         return (ans == n+1) ? -1 : ans;
     }
 };
+
+//2023-06-28
+//time  : O(n)
+//spcae : O(1)
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x)
+    {
+        int n=nums.size();
+        int left=0;
+        while(left < n && x > 0)
+        {
+            x -= nums[left];
+            ++left;
+        }
+        
+        int ans=INT_MAX;
+        if(x == 0) ans=left;
+        
+        int right=n-1;
+        while(left > 0)
+        {
+            --left;
+            x += nums[left];        
+            
+            while(x > 0 && right > left)
+            {
+                x -= nums[right];
+                --right;
+            }
+            
+            if(x == 0)
+            {                
+                ans = min(ans, left+n-right-1);
+            }
+            
+            //cout<<left<<" "<<right<<" | "<<ans<<endl;
+        }       
+        
+        while(x > 0 && right >= 0)
+        {
+            x -= nums[right];
+            --right;
+        }
+        
+        if(x == 0)
+        {                
+            ans = min(ans, n-right-1);
+        }
+        
+        return ans==INT_MAX ? -1 : ans;
+    }
+};
