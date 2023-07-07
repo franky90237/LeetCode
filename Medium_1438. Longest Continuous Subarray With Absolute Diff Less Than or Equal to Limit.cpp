4235401bbdbc79f3aeaaa1bf7a9464fe0707a7b0
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-07-08 07:50:11 +0800

    Update Medium_1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

--- Medium_1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ Medium_1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -24,3 +24,50 @@ public:
         return ans;
     }
 };
+
+//2023-07-08
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit)
+    {
+        int n=nums.size();
+        int ans=0;
+        deque<int> dqMax;
+        deque<int> dqMin;
+        for(int left=0, right=0; right<n; ++right)
+        {
+            while(!dqMax.empty() && nums[dqMax.back()] <= nums[right])
+            {
+                dqMax.pop_back();
+            }
+            
+            while(!dqMin.empty() && nums[dqMin.back()] >= nums[right])
+            {
+                dqMin.pop_back();
+            }
+            
+            dqMax.push_back(right);
+            dqMin.push_back(right);
+            
+            while(nums[dqMax.front()] - nums[dqMin.front()] > limit)
+            {
+                if(dqMax.front() < dqMin.front()) 
+                {
+                    left=dqMax.front()+1;
+                    dqMax.pop_front();
+                }
+                else 
+                {
+                    left=dqMin.front()+1;
+                    dqMin.pop_front();
+                }
+            }
+            
+            ans = max(ans, right-left+1);
+        }
+        
+        return ans;
+    }
+};
