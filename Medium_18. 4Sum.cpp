7369d36e992d44ac14a0d36df3c25ed8e8c0e04a
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-07-26 21:54:45 +0800

    Update Medium_18. 4Sum.cpp

--- Medium_18. 4Sum.cpp
+++ Medium_18. 4Sum.cpp
@@ -61,3 +61,59 @@ public:
         }        
     }
 };
+
+//2023-07-26
+//time  : O(n*n*n)
+//space : O(1)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) 
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int n=nums.size();
+        int c=0;
+        int d=n-1;
+        for(int c=0; c<n-1; ++c)
+        {
+            if(c > 0 && nums[c] == nums[c-1]) continue;
+            
+            for(int d=c+1; d<n; ++d)
+            {
+                if(d > c+1 && nums[d] == nums[d-1]) continue;
+                twoSum(nums, (long long)target-nums[c]-nums[d], c, d, ans);
+            }
+        }
+        
+        return ans;
+    }
+    
+    void twoSum(vector<int>& nums, long long target, int c, int d, vector<vector<int>>& ans)
+    {
+        int n=nums.size();
+        int a=d+1;
+        int b=n-1;
+        while(a < b)
+        {
+            long long sum=nums[a]+nums[b];
+            if(sum == target) 
+            {
+                ans.push_back({nums[a], nums[b], nums[c], nums[d]});
+                
+                ++a;
+                while(a < b && nums[a] == nums[a-1]) ++a;
+                
+                --b;
+                while(a < b && nums[b] == nums[b+1]) --b;
+            }
+            else if(sum > target)
+            {
+                --b;
+            }
+            else
+            {
+                ++a;
+            }
+        }
+    }
+};
