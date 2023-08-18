89f8319311095d0950a81697440de1d491b6710e
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-08-18 19:20:58 +0800

    Update Medium_650. 2 Keys Keyboard.cpp

--- Medium_650. 2 Keys Keyboard.cpp
+++ Medium_650. 2 Keys Keyboard.cpp
@@ -34,3 +34,44 @@ public:
         return ans;
     }
 };
+
+//2023-08-18
+//time  : O(n)
+//space : O(n)
+class Solution 
+{
+private:
+    vector<int> dp;
+    
+public:
+    int minSteps(int n) 
+    {                
+        if(n == 1) return 0;
+        
+        dp.resize(n+1, -1);
+        return solve(1, n);
+    }
+    
+    int solve(int cnt, int n)
+    {
+        if(cnt == n) return 0;
+        if(cnt > n) return INT_MAX;
+        if(dp[cnt] != -1) return dp[cnt];
+        
+        int ans=INT_MAX;
+        int i=2;
+        int cur=i*cnt;        
+        while(cur <= n)
+        {
+            int res=solve(cur, n);
+            if(res != INT_MAX) res += i; 
+            ans=min(ans, res);
+                        
+            ++i;
+            cur=i*cnt;
+        }
+        
+        dp[cnt]=ans;
+        return ans;
+    }
+};
