8a92745505b33ef79fc2a2e2532153a9cc4fe126
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-08-26 07:57:07 +0800

    Update Medium_221. Maximal Square.cpp

--- Medium_221. Maximal Square.cpp
+++ Medium_221. Maximal Square.cpp
@@ -87,3 +87,52 @@ public:
         return dp[r][c];
     }
 };
+
+//2023-08-26
+//time  : O(m*n)
+//space : O(m*n)
+class Solution 
+{
+private:
+    vector<vector<int>> dp;
+    
+public:
+    int maximalSquare(vector<vector<char>>& matrix) 
+    {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        dp.resize(m, vector<int>(n));
+        
+        int len=0;
+        for(int r=0; r<m; ++r) 
+        {
+            dp[r][0]=matrix[r][0]-'0';
+            len=max(len, dp[r][0]);
+        }
+        for(int c=0; c<n; ++c) 
+        {
+            dp[0][c]=matrix[0][c]-'0';
+            len=max(len, dp[0][c]);
+        }
+                
+        for(int r=1; r<m; ++r)
+        {
+            for(int c=1; c<n; ++c)
+            {
+                if(matrix[r][c] == '0') 
+                {
+                    dp[r][c]=0;
+                    continue;
+                }
+                
+                int left=1+dp[r][c-1];
+                int mid=1+dp[r-1][c-1];
+                int up=1+dp[r-1][c];
+                dp[r][c]=min(left, min(mid, up));
+                len=max(len, dp[r][c]);
+            }
+        }
+        
+        return len*len;
+    }    
+};
