5a55cdf9e1000f417883e1b41ccb81c3f046b47a
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-08-29 20:09:51 +0800

    Create Hard_2835. Minimum Operations to Form Subsequence With Target Sum.cpp

---
+++ Hard_2835. Minimum Operations to Form Subsequence With Target Sum.cpp
@@ -0,0 +1,53 @@
+//2023-08-29
+//time  : O(nlog(n))
+//space : O(1)
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int target) 
+    {
+        int n=nums.size();
+        long long total=accumulate(nums.begin(), nums.end(), 0LL);
+        if(total < target) return -1;
+        
+        sort(nums.begin(), nums.end());
+        
+        int ans=0;        
+        int idx=0;
+        long long powerOf2=1;
+        for(int i=0; i<32; ++i)
+        {
+            if(((target>>i)&1) == 0) continue;
+            
+            powerOf2=1<<i;
+            long long sum=0;
+            while(idx < n)
+            {
+                sum+=nums[idx];
+                if(sum >= powerOf2)
+                {
+                    int diff=getDiff(sum, powerOf2);
+                    ans += diff;
+                    if(diff > 0) i += diff-1;
+                    ++idx;
+                    break;
+                }
+                
+                ++idx;                
+            }
+        }
+        
+        return ans;
+    }
+    
+    int getDiff(int a, int b)
+    {
+        int cnt=0;
+        while((a>>1) >= b)
+        {
+            ++cnt;
+            a=a>>1;
+        }
+        
+        return cnt;
+    }
+};
