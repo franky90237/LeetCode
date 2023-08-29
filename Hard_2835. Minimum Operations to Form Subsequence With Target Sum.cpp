5f33072b88782ad23cdf2d5f6917e19714a92137
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-08-29 20:40:50 +0800

    Update Hard_2835. Minimum Operations to Form Subsequence With Target Sum.cpp

--- Hard_2835. Minimum Operations to Form Subsequence With Target Sum.cpp
+++ Hard_2835. Minimum Operations to Form Subsequence With Target Sum.cpp
@@ -51,3 +51,52 @@ public:
         return cnt;
     }
 };
+
+//2023-08-29
+//time  : O(n)
+//space : O(32)
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int target) 
+    {
+        int n=nums.size();
+        long long total=accumulate(nums.begin(), nums.end(), 0LL);
+        if(total < target) return -1;
+        
+        vector<int> bitCnt(33, 0);
+        for(auto num: nums)
+        {
+            int power=log2(num);
+            ++bitCnt[power];
+        }
+        
+        int ans=0;
+        for(int i=0; i<32; ++i)
+        {
+            //cout<<endl<<i<<" "<<((target>>i)&1)<<endl;
+            if(((target>>i)&1) == 1)
+            {
+                //cout<<i<<" "<<ans<<" | "<<bitCnt[i]<<endl;
+                if(bitCnt[i] >= 1)
+                {
+                    --bitCnt[i];
+                }
+                else
+                {
+                    while(i < 32 && bitCnt[i] == 0)
+                    {
+                        ++ans;
+                        ++i;
+                    }
+
+                    --bitCnt[i];
+                    --i;
+                }
+            }
+            
+            bitCnt[i+1]+=bitCnt[i]/2;
+        }
+        
+        return ans;
+    }    
+};
