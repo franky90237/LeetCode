79d3086c2fd6814332049461c8e3614f34c28f1c
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-09-03 22:34:09 +0800

    Update Medium_1248. Count Number of Nice Subarrays.cpp

--- Medium_1248. Count Number of Nice Subarrays.cpp
+++ Medium_1248. Count Number of Nice Subarrays.cpp
@@ -21,3 +21,47 @@ public:
         return ans;
     }
 };
+
+//2023-09-03
+//time  : O(n)
+//spcae : O(1)
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        int left=0;
+        while(left < n && nums[left] % 2 == 0) ++left;
+        
+        int pre=-1;
+        int cnt=0;
+        int ans=0;
+        for(int right=left; right<n; ++right)
+        {
+            if(nums[right] % 2 == 1) 
+            {                
+                ++cnt;
+            }
+            
+            if(cnt > k)
+            {
+                pre=left;
+                ++left;
+                while(left < right && nums[left] % 2 != 1)
+                {
+                    ++left;
+                }
+                --cnt;                                
+            }
+            
+            if(cnt == k)
+            {
+                ans += left-pre;
+            }
+            
+            //cout<<left<<" "<<right<<" | "<<ans<<endl;
+        }
+        
+        return ans;
+    }
+};
