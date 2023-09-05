d24bd500a05b3f05bd65670bb6092445495b4bd4
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-09-05 21:19:10 +0800

    Update Hard_2836. Maximize Value of Function in a Ball Passing Game.cpp

--- Hard_2836. Maximize Value of Function in a Ball Passing Game.cpp
+++ Hard_2836. Maximize Value of Function in a Ball Passing Game.cpp
@@ -69,3 +69,65 @@ public:
         return *max_element(ans.begin(), ans.end());
     }
 };
+
+//2023-09-05
+//time  : O(nlog(k))
+//spcae : O(nlog(k))
+class Solution {
+public:
+    long long getMaxFunctionValue(vector<int>& receiver, long long k) 
+    {
+        int n=receiver.size();
+        int cnt=ceil(log2(k))+1;
+        vector<vector<int>> destination(cnt, vector<int>(n));
+        vector<vector<long long>> value(cnt, vector<long long>(n, 0));
+        
+        for(int i=0; i<n; ++i)
+        {           
+            destination[0][i]=receiver[i];            
+            value[0][i]=i+receiver[i];
+        }
+        
+        for(int i=1; i<cnt; ++i)
+        {            
+            for(int x=0; x<n; ++x)
+            {                
+                destination[i][x] = destination[i-1][destination[i-1][x]];
+                value[i][x] = value[i-1][x] + value[i-1][destination[i-1][x]] - destination[i-1][x];
+            }
+        }
+        
+        /*
+        for(int i=0; i<cnt; ++i)
+        {
+            for(int x=0; x<n; ++x) cout<<destination[i][x]<<" ";
+            cout<<" | ";
+            for(int x=0; x<n; ++x) cout<<value[i][x]<<" ";
+            cout<<endl;
+        }
+        */
+                
+        vector<int> final_destination(n);
+        vector<long long> final_value(n);
+        for(int i=0; i<n; ++i)
+        {
+            final_destination[i]=i;
+            final_value[i]=i;
+        }
+            
+        for(int i=0; i<cnt; ++i)
+        {
+            //cout<<i<<" : ";
+            if(((k>>i)&1LL) == 0) continue;
+            
+            for(int x=0; x<n; ++x)
+            {
+                final_value[x] = final_value[x] + value[i][final_destination[x]] - final_destination[x];
+                final_destination[x] = destination[i][final_destination[x]];                
+            }
+            //cout<<endl;
+        }
+                
+        return *max_element(final_value.begin(), final_value.end());
+    }
+};
