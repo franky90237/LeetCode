55cef77b9065b2a019130b4e22a28333197e1a2f
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-09-13 20:26:22 +0800

    Update Medium_2845. Count of Interesting Subarrays.cpp

--- Medium_2845. Count of Interesting Subarrays.cpp
+++ Medium_2845. Count of Interesting Subarrays.cpp
@@ -23,3 +23,26 @@ public:
         return ans;
     }
 };
+
+//2023-09-13
+//time  : O(n)
+//spcae : O(n)
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) 
+    {
+        unordered_map<int, int> pre;
+        pre[0]=1;
+        long long ans=0;
+        long long cnt=0;
+        for(int i=0; i<nums.size(); ++i)
+        {
+            if(nums[i] % modulo == k) cnt=(cnt+1)%modulo;
+            int target=(cnt-k+modulo) % modulo;
+            ans += pre[target];
+            ++pre[cnt];
+        }
+        
+        return ans;
+    }
+};
