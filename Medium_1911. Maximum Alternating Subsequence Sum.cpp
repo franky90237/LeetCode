edc75ba7732503cd76d5bf3af2a28dd7b0d818a8
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-09-23 10:52:29 +0800

    Update Medium_1911. Maximum Alternating Subsequence Sum.cpp

--- Medium_1911. Maximum Alternating Subsequence Sum.cpp
+++ Medium_1911. Maximum Alternating Subsequence Sum.cpp
@@ -28,3 +28,31 @@ public:
         return dp[cur][isPlus];
     }
 };
+
+//2023-09-23
+//time  : O(n)
+//spcae : O(n)
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<vector<long long>> dp;
+        dp.resize(n, vector<long long>(2, 0));
+        
+        int plus=1;
+        int minus=0;
+        
+        dp[n-1][plus]=nums[n-1];
+        dp[n-1][minus]=0;
+        
+        for(int i=n-2; i>=0; --i)
+        {
+            //cout<<i<<endl;
+            dp[i][plus] = max(nums[i]+dp[i+1][minus], dp[i+1][plus]);
+            dp[i][minus] = max(-nums[i]+dp[i+1][plus], dp[i+1][minus]); 
+        }
+        
+        return dp[0][plus];
+    }    
+};
