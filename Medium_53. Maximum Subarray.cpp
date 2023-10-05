bc9486357a08489734819102a5841fda145c1806
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-10-05 20:28:03 +0800

    Update Medium_53. Maximum Subarray.cpp

--- Medium_53. Maximum Subarray.cpp
+++ Medium_53. Maximum Subarray.cpp
@@ -52,3 +52,28 @@ public:
         return ans;
     }
 };
+
+//2023-10-05
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int ans=INT_MIN;
+        int sum=0;
+        for(int i=0; i<nums.size(); ++i)
+        {
+            sum += nums[i];
+            ans = max(ans, sum);
+            
+            if(sum < 0)
+            {                
+                sum = 0;                
+            }                        
+        }
+        
+        return ans;
+    }
+};
