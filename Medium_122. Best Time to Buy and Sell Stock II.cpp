cc965b3e2ee20fb240096e3927823adb8c7e9d35
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-10-05 20:07:29 +0800

    Update Medium_122. Best Time to Buy and Sell Stock II.cpp

--- Medium_122. Best Time to Buy and Sell Stock II.cpp
+++ Medium_122. Best Time to Buy and Sell Stock II.cpp
@@ -134,3 +134,25 @@ public:
         return notHold[n-1];        
     }
 };
+
+//2023-10-04
+//time  : O(n)
+//spcae : O(n)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int n=prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[n-1][0]=0;
+        dp[n-1][1]=prices[n-1];
+        
+        for(int i=n-2; i>=0; --i)
+        {
+            dp[i][0]=max(dp[i+1][0], dp[i+1][1]-prices[i]);
+            dp[i][1]=max(dp[i+1][1], dp[i+1][0]+prices[i]);
+        }
+        
+        return dp[0][0];
+    }
+};
