77e75741c5e3b79eb9ce6c3bcbdac785536554b0
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-10-16 18:25:41 +0800

    Update Medium_713. Subarray Product Less Than K.cpp

--- Medium_713. Subarray Product Less Than K.cpp
+++ Medium_713. Subarray Product Less Than K.cpp
@@ -26,3 +26,30 @@ public:
         return ans;
     }
 };
+
+//2023-10-16
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        int ans=0;
+        int product=1;
+        int left=0;
+        for(int right=0; right<n; ++right)
+        {
+            product *= nums[right];
+            while(left <= right && product >= k)
+            {
+                product /= nums[left];
+                ++left;
+            }
+            
+            ans += right-left+1;
+        }
+        
+        return ans;
+    }
+};
