ccb428dd2763bbc9036ff8773ed85ffbedd53daa
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-10-16 16:16:04 +0800

    Update Medium_152. Maximum Product Subarray.cpp

--- Medium_152. Maximum Product Subarray.cpp
+++ Medium_152. Maximum Product Subarray.cpp
@@ -26,3 +26,81 @@ public:
         return res;
     }
 };
+
+//2023-10-16
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int n=nums.size();        
+               
+        set<int> prefix;
+        int ans=nums[0];
+        int cur=1;
+        for(int i=0; i<n; ++i)
+        {
+            cur *= nums[i];
+            if(cur < 0 && !prefix.empty())
+            {                
+                ans = max(ans, cur / *(prefix.rbegin()));
+            }
+            else
+            {
+                ans = max(ans, cur);
+            }
+            
+            if(cur < 0)
+            {
+                prefix.insert(cur);
+            } 
+            else if(cur == 0)
+            {
+                cur=1;
+                prefix.clear();
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//2023-10-16
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int n=nums.size();        
+               
+        int maxNegative=INT_MIN;
+        int ans=nums[0];
+        int cur=1;
+        for(int i=0; i<n; ++i)
+        {
+            cur *= nums[i];
+            if(cur < 0 && maxNegative != INT_MIN)
+            {                
+                ans = max(ans, cur / maxNegative);
+            }
+            else
+            {
+                ans = max(ans, cur);
+            }
+            
+            if(cur < 0)
+            {
+                maxNegative=max(maxNegative, cur);
+            } 
+            else if(cur == 0)
+            {
+                cur=1;
+                maxNegative=INT_MIN;
+            }
+        }
+        
+        return ans;
+    }
+};
