bea7b62e8066649249eafe6aa57ec40c9cf4df76
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-10-27 21:02:42 +0800

    Update Medium_2419. Longest Subarray With Maximum Bitwise AND.cpp

--- Medium_2419. Longest Subarray With Maximum Bitwise AND.cpp
+++ Medium_2419. Longest Subarray With Maximum Bitwise AND.cpp
@@ -25,3 +25,40 @@ public:
         return ans;
     }
 };
+
+//2023-10-27
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int maxNum=nums[0];
+        int maxLen=1;
+        for(int i=0; i<n; ++i)
+        {
+            if(nums[i] < maxNum) continue;
+            
+            int cur=i;
+            while(cur < n && nums[cur] == nums[i])
+            {                
+                ++cur;
+            }
+            
+            if(nums[i] == maxNum)
+            {
+                maxLen=max(maxLen, cur-i);
+            }
+            else
+            {
+                maxNum=nums[i];
+                maxLen=cur-i;
+            }                        
+            
+            i=cur-1;
+        }
+        
+        return maxLen;
+    }
+};
