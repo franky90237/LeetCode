132203434822ea3438c5777a297ec51ce7de3b17
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-10-28 21:10:40 +0800

    Update Medium_2044. Count Number of Maximum Bitwise-OR Subsets.cpp

--- Medium_2044. Count Number of Maximum Bitwise-OR Subsets.cpp
+++ Medium_2044. Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -53,3 +53,34 @@ public:
         return pick+notPick;
     }
 };
+
+//2023-10-28
+//time  : O(n*100000)
+//space : O(n*100000)
+class Solution 
+{
+private:
+    int target=0;
+    vector<vector<int>> dp;
+public:
+    int countMaxOrSubsets(vector<int>& nums) 
+    {
+        for(int num: nums) target |= num;
+        
+        dp.resize(nums.size(), vector<int>(target+1, -1));
+        return solve(nums, 0, 0);        
+    }
+    
+    int solve(vector<int>& nums, int cur, int idx)
+    {
+        int n=nums.size();
+        if(idx >= n) return cur==target;
+        if(dp[idx][cur] != -1) return dp[idx][cur];
+                
+        int pick=solve(nums, cur|nums[idx], idx+1);
+        int notPick=solve(nums, cur, idx+1);
+        
+        dp[idx][cur]=pick+notPick;        
+        return dp[idx][cur];
+    }
+};
