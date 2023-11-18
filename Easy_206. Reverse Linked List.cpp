d81250d124b19839d0792661ced261f6bf09d1f0
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-11-19 07:54:35 +0800

    Update Easy_206. Reverse Linked List.cpp

--- Easy_206. Reverse Linked List.cpp
+++ Easy_206. Reverse Linked List.cpp
@@ -31,3 +31,43 @@ public:
         return pre;
     }
 };
+
+//2023-11-19
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode* pre=NULL;
+        ListNode* cur=head;
+        while(cur)
+        {
+            ListNode* nxt=cur->next;
+            cur->next=pre;
+            pre=cur;
+            cur=nxt;
+        }
+        
+        return pre;
+    }
+};
+//2023-11-19
+//time  : O(n)
+//space : O(1)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {        
+        return reverseList(NULL, head); 
+    }
+    
+    ListNode* reverseList(ListNode* pre, ListNode* head)
+    {
+        if(!head) return pre;
+        
+        ListNode* nxt=head->next;
+        head->next=pre;
+        return reverseList(head, nxt);
+    }
+};
