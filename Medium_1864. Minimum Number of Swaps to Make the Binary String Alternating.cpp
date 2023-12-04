05cebfe6fb05d3363da5bd9221e97afdc3eb70a9
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-12-04 11:48:51 +0800

    Update Medium_1864. Minimum Number of Swaps to Make the Binary String Alternating.cpp

--- Medium_1864. Minimum Number of Swaps to Make the Binary String Alternating.cpp
+++ Medium_1864. Minimum Number of Swaps to Make the Binary String Alternating.cpp
@@ -62,3 +62,60 @@ public:
 "001"
 "011111"
 */
+
+//2023-12-04
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    int minSwaps(string s) 
+    {
+        int n=s.size();
+        int one=0;
+        int zero=0;
+        for(char c: s)
+        {
+            if(c == '1') ++one;
+            else ++zero;
+        }
+
+        if(abs(one-zero) >= 2) return -1;
+
+        bool first = (zero >= one) ? 0 : 1;
+        string ans1(n, ' ');
+        string ans2(n, ' ');
+        for(int i=0; i<n; ++i)
+        {
+            if(first == 0)
+            {
+                ans1[i]='0';
+                ans2[i]='1';
+            }
+            else
+            {
+                ans1[i]='1';
+                ans2[i]='0';           
+            }
+
+            first=!first;
+        }
+
+        int res1=1e9;
+        int res2=1e9;
+        res1=getCnt(s, ans1);
+        if(zero == one) res2=getCnt(s, ans2);
+        return min(res1, res2);
+    }
+
+    int getCnt(string& a, string& b)
+    {
+        int n=a.size();
+        int cnt=0;
+        for(int i=0; i<n; ++i)
+        {
+            if(a[i] != b[i]) ++cnt;
+        }
+
+        return cnt/2;
+    }
+};
