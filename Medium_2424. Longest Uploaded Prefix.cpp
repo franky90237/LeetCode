c5134a93d7d557c0af429576c1cd9202dbda4cea
franky90237 <30579359+franky90237@users.noreply.github.com>
2023-12-08 16:32:07 +0800

    Create Medium_2424. Longest Uploaded Prefix.cpp

---
+++ Medium_2424. Longest Uploaded Prefix.cpp
@@ -0,0 +1,76 @@
+//2023-12-08
+//time  : O(n)
+//spsace : O(n)
+class UnionFind
+{
+private:
+    vector<int> parent;
+    vector<int> size;
+
+public:
+    UnionFind() = default;
+    
+    UnionFind(int n) : parent(n), size(n, 1)
+    {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    void combine(int a, int b)
+    {
+        int rootA=find(a);
+        int rootB=find(b);
+        if(rootA == rootB) return;
+
+        if(size[rootA] < size[rootB])
+        {
+            parent[rootA]=rootB;
+            size[rootB] += size[rootA];
+        }
+        else
+        {
+            parent[rootB]=rootA;
+            size[rootA] += size[rootB];
+        }
+    }
+
+    int find(int a)
+    {
+        if(a == parent[a]) return a;
+        return parent[a]=find(parent[a]);
+    }
+
+    int getSize(int a)
+    {
+        int parent=find(a);
+        return size[parent];
+    }
+};
+
+class LUPrefix 
+{
+private:
+    int maxNumber;
+    vector<bool> used;
+    UnionFind uf;
+
+public:
+    LUPrefix(int n) : maxNumber(n), used(n+1, false), uf(n+1)
+    {
+
+    }
+    
+    void upload(int video) 
+    {
+        //cout<<video<<endl;
+        if(video-1 >= 0 && used[video-1]) uf.combine(video, video-1);
+        if(video+1 <= maxNumber && used[video+1]) uf.combine(video, video+1);
+        used[video]=true;
+    }
+    
+    int longest() 
+    {
+        //cout<<"longest"<<endl;
+        if(!used[1]) return 0;        
+        return uf.getSize(1);
+    }
+};
