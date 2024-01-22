61b8236261d59b744d69beb0b23669371c9d194d
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-01-22 20:34:55 +0800

    Update Medium_200. Number of Islands.c

--- Medium_200. Number of Islands.c
+++ Medium_200. Number of Islands.c
@@ -408,3 +408,43 @@ public:
         return uf.count;
     }
 };
+
+//2024-01-22
+//time  : O(m*n)
+//spcae : O(1)
+class Solution {       
+public:
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        int m=grid.size();
+        int n=grid[0].size();        
+        
+        int ans=0;
+        for(int i=0; i<m; ++i)
+        {
+            for(int j=0; j<n; ++j)
+            {
+                if(grid[i][j] == '1')
+                {
+                    dfs(grid, i, j);
+                    ++ans;
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    void dfs(vector<vector<char>>& grid, int i, int j)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') return;
+        
+        grid[i][j]='7';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+};
