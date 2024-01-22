a86587a1c05969edc6bc843512f8763278f986a0
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-01-22 19:51:18 +0800

    Update Hard_297. Serialize and Deserialize Binary Tree.cpp

--- Hard_297. Serialize and Deserialize Binary Tree.cpp
+++ Hard_297. Serialize and Deserialize Binary Tree.cpp
@@ -79,3 +79,69 @@ public:
         return root;
     }
 };
+
+//2024-01-22
+//time  : O(n)
+//space : O(n)
+class Codec 
+{
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) 
+    {
+        string res;
+        dfs(root, res);
+        return res;
+    }
+    
+    void dfs(TreeNode* root, string& res)
+    {
+        if(!root)
+        {
+            res += "N,";
+            return;
+        }
+        
+        res += to_string(root->val) + ",";
+        dfs(root->left, res);
+        dfs(root->right, res);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) 
+    {
+        deque<string> nodes;
+        string cur;
+        for(int i=0; i<data.size(); ++i)
+        {            
+            if(data[i] == ',')
+            {
+                nodes.emplace_back(cur);
+                cur="";
+                continue;
+            }
+            
+            if(data[i] == 'N') cur = "N";
+            else cur.push_back(data[i]);
+        }                
+        
+        return buildTree(nodes);
+    }
+    
+    TreeNode* buildTree(deque<string>& nodes)
+    {
+        if(nodes.front() == "N") 
+        {
+            nodes.pop_front();
+            return NULL;
+        }
+        
+        TreeNode* cur=new TreeNode(stoi(nodes.front()));
+        nodes.pop_front();
+        cur->left = buildTree(nodes);
+        cur->right = buildTree(nodes);
+        
+        return cur;
+    }
+};
