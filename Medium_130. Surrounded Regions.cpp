61f4ed41b62db86944d594b8074e6da88bd849aa
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-01-23 20:54:57 +0800

    Update Medium_130. Surrounded Regions.cpp

--- Medium_130. Surrounded Regions.cpp
+++ Medium_130. Surrounded Regions.cpp
@@ -115,3 +115,49 @@ public:
         dfs(board, r, c+1);
     }
 };
+
+//2024-01-23
+//time  : O(m*n)
+//space : O(m*n)
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) 
+    {
+        int m=board.size();
+        int n=board[0].size();
+        
+        for(int r=0; r<m; ++r)
+        {
+            if(board[r][0] == 'O') dfs(board, r, 0);
+            if(board[r][n-1] == 'O') dfs(board, r, n-1);
+        }
+        
+        for(int c=0; c<n; ++c)
+        {
+            if(board[0][c] == 'O') dfs(board, 0, c);
+            if(board[m-1][c] == 'O') dfs(board, m-1, c);
+        }
+        
+        for(int r=0; r<m; ++r)
+        {
+            for(int c=0; c<n; ++c)
+            {
+                if(board[r][c] == 'O') board[r][c]='X';
+                else if(board[r][c] == 'V') board[r][c]='O';
+            }
+        }
+    }
+    
+    void dfs(vector<vector<char>>& board, int r, int c)
+    {
+        int m=board.size();
+        int n=board[0].size();
+        if(r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') return;
+        
+        board[r][c]='V';
+        dfs(board, r+1, c);
+        dfs(board, r-1, c);
+        dfs(board, r, c+1);
+        dfs(board, r, c-1);
+    }
+};
