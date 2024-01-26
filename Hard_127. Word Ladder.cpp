f4c59d2448dff2e51fe459f439e12b6e7becda04
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-01-26 14:51:30 +0800

    Update Hard_127. Word Ladder.cpp

--- Hard_127. Word Ladder.cpp
+++ Hard_127. Word Ladder.cpp
@@ -46,3 +46,58 @@ public:
         return 0;
     }    
 };
+
+//2024-01-26
+//time  : O(n*n*m*26)
+//space : O(m*n)
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
+    {
+        unordered_set<string> table(wordList.begin(), wordList.end());
+        if(table.count(endWord) == 0) return 0;
+        
+        unordered_set<string> vis;
+        vis.insert(beginWord);
+        
+        queue<string> q;
+        q.push(beginWord);
+        
+        int cnt=1;
+        while(!q.empty())
+        {            
+            int size=q.size();
+            for(; size > 0; --size)
+            {
+                string cur=q.front();
+                q.pop();
+                //cout<<cur<<endl;
+                if(cur == endWord) return cnt;
+
+                for(int i=0; i<cur.size(); ++i) //O(m*26*m)
+                {
+                    int original=cur[i];
+
+                    for(char c='a'; c<='z'; ++c)
+                    {
+                        if(c == original) continue;
+
+                        cur[i]=c;
+                        if(vis.count(cur) == 0 && table.count(cur) != 0)
+                        {                    
+                            vis.insert(cur);
+                            q.push(cur);
+                        }                    
+                    }
+
+                    cur[i]=original;
+                }                                
+            }
+            
+            //cout<<endl;
+            ++cnt;
+        }
+        
+        return 0;
+    }
+};
