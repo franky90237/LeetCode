bcb1433eff5d237c377ed8119f9d6e7db679bd6f
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-09 20:44:37 +0800

    Update Medium_673. Number of Longest Increasing Subsequence.cpp

--- Medium_673. Number of Longest Increasing Subsequence.cpp
+++ Medium_673. Number of Longest Increasing Subsequence.cpp
@@ -53,3 +53,133 @@ public:
         return res;
     }
 };
+
+//2024-02-09
+//time  : O(nlog(n))
+//sapce : O(n)
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) 
+    {
+        int n=nums.size();
+        
+        vector<vector<int>> arr;
+        vector<vector<int>> cnt;
+        
+        arr.push_back({nums[0]});
+        cnt.push_back({1});
+        
+        for(int i=1; i<n; ++i)
+        {
+            if(nums[i] > arr.back().back())
+            {
+                int idxArr = arr.size();               
+                int idxSingleArr = bs(arr[idxArr-1], nums[i]);
+                int sum = getSum(cnt[idxArr-1], idxSingleArr);
+                //cout<<i<<" "<<idxSingleArr<<" "<<sum<<endl;                
+                arr.push_back({nums[i]});
+                cnt.push_back({sum});
+            }
+            else
+            {
+                int idxArr = bs(arr, nums[i]);
+                if(idxArr == 0)
+                {
+                    if(nums[i] == arr[idxArr].back())
+                    {
+                        cnt[idxArr].back() += 1;
+                    }
+                    else
+                    {
+                        arr[idxArr].push_back(nums[i]);
+                        cnt[idxArr].push_back(1 + cnt[idxArr].back());
+                    }
+                }
+                else
+                {
+                    int idxSingleArr = bs(arr[idxArr-1], nums[i]);
+                    int sum = getSum(cnt[idxArr-1], idxSingleArr);
+                    //cout<<i<<" "<<idxSingleArr<<" "<<sum<<endl;
+                    
+                    if(nums[i] == arr[idxArr].back())
+                    {
+                        cnt[idxArr].back() += sum;
+                    }
+                    else
+                    {
+                        arr[idxArr].push_back(nums[i]);
+                        cnt[idxArr].push_back(sum + cnt[idxArr].back());
+                    }
+                }
+            }
+        }        
+        
+        /*
+        for(int i=0; i<arr.size(); ++i)
+        {
+            cout<<"[";
+            for(int a : arr[i]) cout<<a<<" ";
+            cout<<"] ";
+        } cout<<endl;
+                        
+        for(int i=0; i<cnt.size(); ++i)
+        {
+            cout<<"[";
+            for(int a : cnt[i]) cout<<a<<" ";
+            cout<<"] ";
+        } cout<<endl;
+        */              
+        
+        return cnt.back().back();
+    }
+    
+    int bs(vector<vector<int>>& arr, int target)
+    {
+        int low=0;
+        int high=arr.size()-1;
+        
+        while(low < high)
+        {
+            int mid=low+(high-low)/2;
+            if(target > arr[mid].back())
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+    
+    int bs(vector<int>& arr, int target)
+    {
+        //if(arr[0] < target) return 0;
+        
+        int low=0;
+        int high=arr.size()-1;
+        
+        while(low < high)
+        {
+            int mid=low+(high-low)/2;
+            if(target <= arr[mid])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+    
+    int getSum(vector<int>& arr, int begin)
+    {
+        if(begin == 0) return arr.back();
+        return arr.back() - arr[begin-1];
+    }
+};
