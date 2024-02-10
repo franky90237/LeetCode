7061c2c2152edb376149b329751dd94eaad4dec2
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-10 11:09:07 +0800

    Update Medium_416. Partition Equal Subset Sum.cpp

--- Medium_416. Partition Equal Subset Sum.cpp
+++ Medium_416. Partition Equal Subset Sum.cpp
@@ -231,3 +231,32 @@ public:
         return dp[0][sum];
     }    
 };
+
+//2024-02-10
+//time  : O(n*sum)
+//space : O(sum)
+class Solution {    
+public:
+    bool canPartition(vector<int>& nums) 
+    {
+        int sum = accumulate(nums.begin(), nums.end(), 0);        
+        if(sum % 2 == 1) return false;        
+        sum /= 2;
+        
+        int n=nums.size();
+        vector<bool> dp(sum+1, false);
+        dp[0] = true;
+        
+        for(int i=n-1; i>=0; --i)
+        {
+            for(int curSum=sum; curSum>=0; --curSum)
+            {
+                bool pick = (curSum-nums[i] >= 0) ? dp[curSum-nums[i]] : false;
+                bool notPick = dp[curSum];
+        
+                dp[curSum] = (pick || notPick);
+            }
+        }
+        return dp[sum];
+    }    
+};
