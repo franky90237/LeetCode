26d7ffbd8cca3e5bd11199864578aed8b1aaac56
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-13 07:37:38 +0800

    Update Medium_62. Unique Paths.cpp

--- Medium_62. Unique Paths.cpp
+++ Medium_62. Unique Paths.cpp
@@ -84,3 +84,24 @@ public:
         
     }
 };
+
+//2024-02-13
+//time  : O(m*n)
+//space : O(n)
+class Solution {
+public:
+    int uniquePaths(int m, int n) 
+    {      
+        vector<int> dp(n, 1);        
+        
+        for(int r=1; r<m; ++r)
+        {
+            for(int c=1; c<n; ++c)
+            {
+                dp[c] = dp[c] + dp[c-1];
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
