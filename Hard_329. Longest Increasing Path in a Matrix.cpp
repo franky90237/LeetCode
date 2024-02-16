717ca99bb168c1f2edb81cf8426179797ca3616f
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-17 07:30:17 +0800

    Update Hard_329. Longest Increasing Path in a Matrix.cpp

--- Hard_329. Longest Increasing Path in a Matrix.cpp
+++ Hard_329. Longest Increasing Path in a Matrix.cpp
@@ -52,3 +52,65 @@ public:
         return max_len;     
     }
 };
+
+//2024-02-17
+//time  : O(m*n)
+//space : O(m*n)
+class Solution 
+{
+private:
+    vector<vector<int>> dp;
+    
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) 
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        dp.resize(m, vector<int>(n, -1));
+
+        int res = 0;
+        for (int r = 0; r < m; ++r)
+        {
+            for (int c = 0; c < n; ++c)
+            {
+                int cnt = f(matrix, r, c);
+                res = max(res, cnt);
+            }
+        }
+
+        /*
+        for (int r = 0; r < m; ++r)
+        {
+            for (int c = 0; c < n; ++c) cout << dp[r][c] << " "; cout << endl;
+        }
+        */
+        
+        return res;
+    }
+    
+    int f(vector<vector<int>>& arr, int r, int c)
+    {
+        static int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+        int m = arr.size();
+        int n = arr[0].size();
+
+        if (dp[r][c] == -2) return 0;
+        if (dp[r][c] != -1) return dp[r][c];
+        dp[r][c] = -2;
+        //cout << r << " " << c << endl;
+
+        int maxCnt = 0;
+        for (int i = 0; i < 4; ++i)
+        {
+            int nr = r + dir[i][0];
+            int nc =  c + dir[i][1];
+            if (nr < 0 || nr >= m || nc < 0 || nc >= n || arr[r][c] >= arr[nr][nc]) continue;
+            
+            int cnt = f(arr, nr, nc);
+            maxCnt = max(maxCnt, cnt);
+        }
+
+        return dp[r][c] = maxCnt + 1;
+    }
+};
