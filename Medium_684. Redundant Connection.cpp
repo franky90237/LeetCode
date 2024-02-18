c31a0afc7179e8b6af07403274e7f878a1ff6f79
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-18 10:19:43 +0800

    Update Medium_684. Redundant Connection.cpp

--- Medium_684. Redundant Connection.cpp
+++ Medium_684. Redundant Connection.cpp
@@ -415,3 +415,115 @@ public:
         }
     }
 };
+
+//2024-02-18
+//time  : O(e*v)
+//space : O(v + e + v)
+class Solution 
+{
+private:
+    vector<vector<int>> g;
+    vector<int> vis;    
+    
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {        
+        int n = edges.size();
+        
+        g.resize(n+1);
+        vis.resize(n+1, 0);
+        
+        for(auto& edge : edges)
+        {
+            int a = edge[0];
+            int b = edge[1];
+            
+            if(canReach(a, b)) return {a, b};
+            
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+        
+        return {};
+    }
+    
+    bool canReach(int a, int b)
+    {
+        if(a == b) return true;
+        if(vis[a]) return false;
+        vis[a] = true;
+        
+        for(int next : g[a])
+        {
+            if(canReach(next, b)) return true;
+        }
+        
+        vis[a] = false;
+        return false;
+    }
+};
+
+//2024-02-18
+//time  : O(v)
+//space : O(v + e + v)
+class Solution 
+{
+private:
+    vector<vector<int>> g;
+    vector<int> degree;     
+    
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {        
+        int n = edges.size();    
+        
+        buildGraph(edges);
+        
+        queue<int> q;
+        for(int i=1; i<=n; ++i)
+        {
+            if(degree[i] == 1) q.push(i);            
+        }
+        
+        while(!q.empty())
+        {
+            int cur=q.front();
+            q.pop();
+            
+            for(int next : g[cur])
+            {
+                --degree[next];
+                if(degree[next] == 1) q.push(next);                
+            }
+        }
+                
+        for(int i=n-1; i>=0; --i)
+        {
+            int a=edges[i][0];
+            int b=edges[i][1];
+            if(degree[a] > 1 && degree[b] > 1) return {a, b};
+        }
+        
+        return {};
+    }
+    
+    void buildGraph(vector<vector<int>>& edges)
+    {
+        int n=edges.size();        
+        
+        g.resize(n+1);
+        degree.resize(n+1, 0);
+        
+        for(auto edge : edges)
+        {
+            int a=edge[0];
+            int b=edge[1];
+            
+            ++degree[a];
+            ++degree[b];
+            
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+    }
+};
