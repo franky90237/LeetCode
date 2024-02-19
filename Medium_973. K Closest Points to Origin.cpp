e49279a0974699726f86f522a3b9cefd111f672c
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-19 18:11:20 +0800

    Update Medium_973. K Closest Points to Origin.cpp

--- Medium_973. K Closest Points to Origin.cpp
+++ Medium_973. K Closest Points to Origin.cpp
@@ -42,3 +42,68 @@ public:
         return res;
     }
 };
+
+//2024-02-19
+//time  : O(log(n))
+//space : O(1)
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
+    {
+        quickSelect(points, k-1);
+        
+        vector<vector<int>> res;
+        for(int i=0; i<k; ++i)
+        {
+            res.push_back(points[i]);            
+        }
+        
+        return res;
+    }
+    
+    void quickSelect(vector<vector<int>>& points, int k)
+    {
+        int left = 0;
+        int right = points.size() - 1;
+        int idx = -1;
+        do
+        {            
+            idx = partition(points, left, right);
+            //cout<<left<<" "<<right<<" | "<<idx<<endl;
+            if(idx < k) left = idx + 1;
+            else if(idx > k) right = idx - 1;
+        } while(idx != k);
+    }
+    
+    int partition(vector<vector<int>>& points, int left, int right)
+    {
+        int mid = left + (right-left)/2;
+        swap(points[right], points[mid]);
+        
+        int targetD = getDistance(points[right]);
+        int bigIdx = left;
+        for(int i=left; i<=right-1; ++i)
+        {
+            int curD = getDistance(points[i]);
+            if(curD < targetD) 
+            {
+                swap(points[bigIdx], points[i]);
+                ++bigIdx;
+            }
+        }
+        
+        /*
+        1 2 3 4 5
+        4 5 1 2 3
+        
+        1 2 4 5 3        
+        */        
+        swap(points[bigIdx], points[right]);
+        return bigIdx;
+    }
+    
+    int getDistance(vector<int>& point)
+    {
+        return point[0]*point[0] + point[1]*point[1];
+    }
+};
