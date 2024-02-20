bfc65d71dfc6a8598553233abd0857fdf49d124f
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-20 20:46:32 +0800

    Update Medium_57. Insert Interval.cpp

--- Medium_57. Insert Interval.cpp
+++ Medium_57. Insert Interval.cpp
@@ -253,3 +253,44 @@ public:
         for(auto i : arr) cout<<"("<<i[0]<<","<<i[1]<<") "; cout<<endl;
     }
 };
+
+//2024-02-20
+//time  : O(n)
+//space : O(n)
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    {
+        int n = intervals.size();
+        vector<vector<int>> res;
+        
+        int i = 0;
+        for (; i < n; ++i)
+        {
+            if (intervals[i][1] >= newInterval[0]) break;
+            res.push_back(intervals[i]);
+        }
+
+        res.push_back(newInterval);
+        for (; i < n; ++i)
+        {
+            if (!overlap(res.back()[0], res.back()[1], intervals[i][0], intervals[i][1])) break;
+            res.back()[0] = min(res.back()[0], intervals[i][0]);
+            res.back()[1] = max(res.back()[1], intervals[i][1]);
+        }
+
+        for (; i < n; ++i)
+        {
+            res.push_back(intervals[i]);
+        }
+        
+        return res;
+    }
+    
+    bool overlap(int a, int b, int c, int d)
+    {
+        if (a <= c && c <= b) return true;
+        if (c <= a && a <= d) return true;
+        return false;
+    }    
+};
