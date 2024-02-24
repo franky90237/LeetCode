3742d28e7bad41a18bf3718f30e0861cfbeecc30
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-02-24 08:22:16 +0800

    Update Hard_224. Basic Calculator.cpp

--- Hard_224. Basic Calculator.cpp
+++ Hard_224. Basic Calculator.cpp
@@ -73,3 +73,85 @@ public:
         return result;
     }     
 };
+
+//2024-02-24
+class Solution {
+public:
+    int calculate(string s) 
+    {        
+        int n = s.size();
+        vector<string> st;
+        for(int i=0; i<n; ++i)
+        {
+            if(s[i] == ' ') continue;
+            
+            if(s[i] == ')')
+            {
+                string val=st.back();
+                st.pop_back();
+                st.pop_back();
+                st.emplace_back(val);
+                
+                doOneOperation(st);
+            }
+            else if(isdigit(s[i]))
+            {
+                string num;
+                while(i < n && isdigit(s[i]))
+                {                
+                    num.push_back(s[i]);
+                    ++i;
+                }
+                --i;
+                
+                st.emplace_back(num);
+                doOneOperation(st);
+            }            
+            else
+            {
+                st.push_back({s[i]});
+            }                        
+        }
+                
+        return stoi(st.back());
+    }
+    
+    void doOneOperation(vector<string>& st)
+    {
+        //for(string& s : st) cout<<s<<" "; cout<<endl;
+        
+        if(st.size() <= 1) return;
+        if(st[st.size()-2] == "(") return;
+        if(st[st.size()-2] == "-" && (st.size() == 2 || st[st.size()-3].back() == '('))
+        {        
+            int val=stoi(st.back());
+            st.pop_back();
+            st.pop_back();
+            st.emplace_back(to_string(-val));
+            return;
+        }
+                
+        int second = stoi(st.back()); 
+        st.pop_back();
+        
+        string op = st.back();
+        st.pop_back();
+        
+        int first = 0;
+        if(!st.empty() && isdigit(st.back().back()))
+        {                      
+            first = stoi(st.back());
+            st.pop_back();
+        }
+        //cout<<first<<" "<<second<<endl;
+        
+        if(op == "+")
+        {
+            st.emplace_back(to_string(first + second));
+        }
+        else if(op == "-")
+        {
+            st.emplace_back(to_string(first - second));
+        }
+    }
+};
