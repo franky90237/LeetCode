7bbde719a8f4381ac2255cf066603a3a0480f6fb
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-03-02 17:03:37 +0800

    Update Medium_516. Longest Palindromic Subsequence.cpp

--- Medium_516. Longest Palindromic Subsequence.cpp
+++ Medium_516. Longest Palindromic Subsequence.cpp
@@ -26,3 +26,27 @@ public:
         return dp[i][j] = max(f(s, i+1, j), f(s, i, j-1));
     }
 };
+
+//2024-03-02
+//time  : O(n*n)
+//space : O(n*n)
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) 
+    {
+        int n = s.size();        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; ++i) dp[i][i]=1;
+        
+        for(int i = n - 2; i >= 0; --i)
+        {
+            for(int j = i + 1; j < n; ++j)
+            {
+                if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2;
+                else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[0][n-1];
+    }     
+};
