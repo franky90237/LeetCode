472ced96c645118d272bca86c0542242cc07f791
franky90237 <30579359+franky90237@users.noreply.github.com>
2024-03-08 14:07:55 +0800

    Update Hard_312. Burst Balloons.cpp

--- Hard_312. Burst Balloons.cpp
+++ Hard_312. Burst Balloons.cpp
@@ -31,3 +31,32 @@ public:
         return dp[i][j] = res;
     }
 };
+
+//2024-03-08
+//time  : O(n*n*n)
+//space : O(n*n)
+class Solution {      
+public:
+    int maxCoins(vector<int>& nums) 
+    {
+        nums.insert(nums.begin(), 1);
+        nums.insert(nums.end(), 1);
+        
+        int n=nums.size();
+         vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for(int i=n-3; i>=0; --i)
+        {
+            for(int j=i+2; j<n; ++j)
+            {
+                for(int k=i+1; k<j; ++k)
+                {
+                    int sum = dp[i][k] + nums[i]*nums[k]*nums[j] + dp[k][j];
+                    dp[i][j] = max(dp[i][j], sum);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+};
